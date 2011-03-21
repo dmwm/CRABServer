b88f05300581130f3fd85b08ab49cfcee4e5fe2a
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2011-03-21 23:27:04 +0000

    Improve CRAB API. Add config api which act as a proxy and do the upload to CouchDB. Fixes #1254
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABServer/trunk@12024 4525493e-7705-40b1-a816-d608a930855b

--- src/python/CRABRESTModel.py
+++ src/python/CRABRESTModel.py
@@ -8,13 +8,14 @@ from WMCore.RequestManager.RequestMaker.Registry import retrieveRequestMaker
 import WMCore.RequestManager.RequestDB.Connection as DBConnect
 import WMCore.Wrappers.JsonWrapper as JsonWrapper
 import WMCore.Lexicon
-import WMCore.RequestManager.RequestMaker.CheckIn as CheckIn
+from WMCore.RequestManager.RequestMaker import CheckIn
 import WMCore.RequestManager.RequestMaker.Processing.AnalysisRequest
-import WMCore.RequestManager.RequestDB.Interface.Request.ChangeState as ChangeState
-import WMCore.RequestManager.RequestDB.Interface.User.Registration as Registration
-import WMCore.RequestManager.RequestDB.Interface.Group.Information as GroupInfo
-import WMCore.RequestManager.RequestDB.Interface.Admin.GroupManagement as GroupAdmin
-import WMCore.RequestManager.RequestDB.Interface.Admin.ProdManagement as ProdManagement
+from WMCore.RequestManager.RequestDB.Interface.Request import ChangeState, GetRequest
+from WMCore.RequestManager.RequestDB.Interface.User import Registration
+from WMCore.RequestManager.RequestDB.Interface.Group import Information
+from WMCore.RequestManager.RequestDB.Interface.Admin import GroupManagement, ProdManagement
+from WMCore.Cache.WMConfigCache import ConfigCache
+import hashlib
 import cherrypy
 import threading
 import json
@@ -27,11 +28,12 @@ class CRABRESTModel(RESTModel):
         Initialise the RESTModel and add some methods to it.
         '''
         RESTModel.__init__(self, config)
-        
         #self.hostAddress = config.model.reqMgrHost
         self.couchUrl = config.model.couchUrl
         self.workloadCouchDB = config.model.workloadCouchDB
-        #/user 
+        self.configCacheCouchURL =  config.configCacheCouchURL
+        self.configCacheCouchDB = config.configCacheCouchDB
+        #/user
         self._addMethod('POST', 'user', self.addNewUser,
                         args=[],
                         validation=[self.isalnum])
@@ -50,9 +52,9 @@ class CRABRESTModel(RESTModel):
                         validation=[self.isalnum])
         #/config
         self._addMethod('POST', 'config', self.postUserConfig,
-                        args=['config'],
+                        args=[''],
                         validation=[self.checkConfig])
-        
+
         # Server status
         self._addMethod('GET', 'status', self.getServerStatus)
 
@@ -70,14 +72,29 @@ class CRABRESTModel(RESTModel):
         """ check user configuration """
         return pset
 
-    def postUserConfig(self, pset):
+    def postUserConfig(self):
         """ this must act as proxy for CouchDB. Upload user config and return DocID """
-        logging.info(pset) 
+
+        body = cherrypy.request.body.read()
+        params = unidecode(JsonWrapper.loads(body))
+
+        configCache = ConfigCache(self.confiCacheCouchURL, self.configCacheCouchDB)
+        configCache.createUserGroup(params['groupName'], params['userName'])
+
+        configMD5 = hashlib.md5(params['confFile']).hexdigest()
+        configCache.document['md5_hash'] = configMD5
+        configCache.document['pset_hash'] = params['psetHash']     #edmConfigHash
+        configCache.attachments['configFile'] = params['confFile']
+
+        configCache.setPSetTweaks(json.loads(params['psetTweaks']))
+        configCache.setLabel(params['label'])
+        configCache.setDescription(params['description'])
+        configCache.save()
+
         result = {}
-        result['DocID'] = '1234'
-        result['CouchURL'] = 'http://xyz'
-        result['CouchDBName'] = 'myconfigcache' 
-        return json.dump(result) 
+        result['DocID'] = configCache.id
+
+        return result
 
     def getServerStatus(self):
         result = {
@@ -89,42 +106,47 @@ class CRABRESTModel(RESTModel):
                 'in_drain': False,
                 'admin': "spiga"
                 }
-        return json.dump(result) 
+        return result
 
     def isalnum(self, index):
-        """ Validates that all input is alphanumeric, 
+        """ Validates that all input is alphanumeric,
             with spaces and underscores tolerated"""
         for v in index.values():
             WMCore.Lexicon.identifier(v)
         return index
-    
+
     def getTaskStatus(self, requestID):
-        """     """
-        return requestID 
- 
+        """ Return the whole task status  """
+        requestDetails = GetRequest.getRequestDetails(requestID)
+        return requestDetails
+
+
     def putTaskModifies(self, requestID):
         """ Modify the task in any possible field : B/W lists, stop/start automation... """
         return requestID
 
     def deleteRequest(self, requestID):
         """    """
-        return requestID  
+        return requestID
 
     def postRequest(self, requestName):
         """ Checks the request n the body with one arg, and changes the status with kwargs """
-        result = {} 
+        result = {}
         body = cherrypy.request.body.read()
         requestSchema = unidecode(JsonWrapper.loads(body))
         logging.error(requestSchema)
+
+        requestSchema["CouchUrl"] =  self.configCacheCouchURL
+        requestSchema["CouchDBName"] =  self.configCacheCouchDB
 
         #requestName must be unique. Unique name is the ID
         currentTime = time.strftime('%y%m%d_%H%M%S',
                                  time.localtime(time.time()))
 
-        requestSchema['RequestName'] = "%s_%s_%s" % (requestSchema["Username"], requestSchema['RequestName'],
+        requestSchema['RequestName'] = "%s_%s_%s" % (requestSchema["Requestor"], requestSchema['RequestName'],
                                                   currentTime)
         result['ID'] = requestSchema['RequestName']
-        
+
         maker = retrieveRequestMaker(requestSchema['RequestType'])
         specificSchema = maker.schemaClass()
         specificSchema.update(requestSchema)
@@ -132,7 +154,10 @@ class CRABRESTModel(RESTModel):
         # we only want the first part, before /task/
         url = url[0:url.find('/task')]
         specificSchema.reqMgrURL = url
-        specificSchema.validate()
+        try:
+            specificSchema.validate()
+        except Exception, ex:
+            raise cherrypy.HTTPError(400,ex.message)
 
         request = maker(specificSchema)
         helper = WMWorkloadHelper(request['WorkflowSpec'])
@@ -144,19 +169,25 @@ class CRABRESTModel(RESTModel):
         workloadUrl = helper.saveCouch(self.couchUrl, self.workloadCouchDB, metadata=metadata)
         request['RequestWorkflow'] = removePasswordFromUrl(workloadUrl)
 
-        CheckIn.checkIn(request)
-        # Auto Assign the requests     
-        ChangeState.changeRequestStatus(requestSchema['RequestName'], 'assignment-approved')
-        # Auto Assign the requests     
-        ### what is the meaning of the Team in the Analysis use case? 
+        try:
+            CheckIn.checkIn(request)
+        except Exception, ex:
+            raise cherrypy.HTTPError(500,ex.message)
+        # Auto Assign the requests
+        try:
+            ChangeState.changeRequestStatus(requestSchema['RequestName'], 'assignment-approved')
+        except Exception, ex:
+            raise cherrypy.HTTPError(500,ex.message)
+        # Auto Assign the requests
+        ### what is the meaning of the Team in the Analysis use case?
         ChangeState.assignRequest(requestSchema['RequestName'], requestSchema["Team"])
         #return  ID & status
-        return json.dumps(result)
+        return result
 
-    
+
     def addNewUser(self):
         """
-        The client must pass the user DN. 
+        The client must pass the user DN.
         The server get the username from sitedb (using userDN)
         The user DN must be propagated to WMBS wmbs_user.name
         """
@@ -164,21 +195,34 @@ class CRABRESTModel(RESTModel):
         body = cherrypy.request.body.read()
         requestorInfos = unidecode(JsonWrapper.loads(body))
 
-        userDN = requestorInfos["UserDN"]
-        user = requestorInfos["Username"]
-        group = requestorInfos["Group"]
-        team = requestorInfos["Team"]
-        email = requestorInfos["Email"] 
+        logging.info(requestorInfos)
+
+        userDN = requestorInfos.get("UserDN", None)
+        user = requestorInfos.get("Username", None)
+        group = requestorInfos.get("Group", None)
+        team = requestorInfos.get("Team", "Analysis")
+        email = requestorInfos.get("Email", None)
 
         result = {}
-        if group != None:
-            if not GroupInfo.groupExists(group):
-                GroupAdmin.addGroup(group)
-                result[group] = 'regitered' 
+        if userDN != None :
+            logging.info("userDN: To DO")
+            # get hnName from it.
+            # for the moment uses what the client pass..
+        else:
+            raise cherrypy.HTTPError(400, "Bad input userDN not defined")
         if user != None:
+            if email == None: raise cherrypy.HTTPError(400, "Bad input user email not defined")
             if not Registration.isRegistered(user):
-                Registration.registerUser(user, email)
-                result[user] = 'regitered' 
+                Registration.registerUser(user, email,userDN)
+                result['user'] = '%s registered'%user
+            else:
+                result['user'] = '%s already registered'%user
+        if group != None:
+            if not Information.groupExists(group):
+                GroupManagement.addGroup(group)
+                result['group'] = '% registered' %group
+            else:
+                result['group'] = '%s already registered'%group
         if group != None and user != None:
             factory = DBConnect.getConnection()
             idDAO = factory(classname = "Requestor.ID")
@@ -186,9 +230,14 @@ class CRABRESTModel(RESTModel):
             assocDAO = factory(classname = "Requestor.GetAssociation")
             assoc = assocDAO.execute(userId)
             if len(assoc) == 0:
-                GroupAdmin.addUserToGroup(user, group)
+                GroupManagement.addUserToGroup(user, group)
         if team != None:
-            ProdManagement.addTeam(team)
-            result[team] = 'regitered' 
-  
-        return json.dumps(result)
+            if not ProdManagement.teamExist(team):
+                ProdManagement.addTeam(team)
+                result['team'] = '%s registered' %team
+            else:
+                result['team'] = '%s already registered' %team
+        else:
+            raise cherrypy.HTTPError(400, "Bad input Team not defined")
+
+        return result
