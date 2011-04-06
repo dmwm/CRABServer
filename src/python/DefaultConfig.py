bc59c0450ffba209db984d2fb346a7ded79997a9
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2011-04-06 16:06:10 +0000

    update Config
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABServer/trunk@12196 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DefaultConfig.py
+++ src/python/DefaultConfig.py
@@ -13,14 +13,11 @@ CRABInterfacePort = 8888
 workDirectory = "/CRABInterface/worgkin/dir"
 
 couchURL = "http://user:passwd@host:5984"
-configCacheDBName = "configCacheName"
 workloadCouchDB = "workloadCouchDB"
 
 databaseUrl = "mysql://root@localhost/ReqMgrDB"
 databaseSocket = "/path/mysql.sock"
 
-
-# Root of the YUI javascript library.
 yuiRoot = "http://yui.yahooapis.com/2.8.0r4"
 
 config = Configuration()
@@ -36,10 +33,18 @@ CRABInterfaceUrl = "http://%s:%s" % (serverHostName, CRABInterfacePort)
 
 config.CRABInterface.componentDir = config.General.workDir + "/CRABInterface"
 config.CRABInterface.Webtools.host = serverHostName
-## User specific parameter
 config.CRABInterface.Webtools.port = CRABInterfacePort
 config.CRABInterface.templates =os.path.join(getWMBASE(),
                                        "src/templates/WMCore/WebTools")
+
+config.CRABInterface.configCacheCouchURL = "YourConfigCacheUrl"
+config.CRABInterface.configCacheCouchDB = "configCacheCouchDB-Name"
+
+config.CRABInterface.agentDN = "/Your/Agent/DN.here/"
+config.CRABInterface.SandBoxCache_endpoint = "USB-cache-endpoint"
+config.CRABInterface.SandBoxCache_port  = "PORT"
+config.CRABInterface.SandBoxCache_basepath ="/Path/if/Needed"
+
 
 config.CRABInterface.admin = "admin@mail.address"
 config.CRABInterface.title = "CRAB REST Interface"
@@ -47,26 +52,20 @@ config.CRABInterface.description = "rest interface for crab"
 config.CRABInterface.instance = "Analysis WMAGENT"
 config.CRABInterface.yuiroot = yuiRoot
 
-config.CRABInterface.couchURL = couchURL
-config.CRABInterface.configDBName = configCacheDBName
-
 config.CRABInterface.section_("security")
 config.CRABInterface.security.dangerously_insecure = True
 
 config.CRABInterface.section_('views')
 config.CRABInterface.views.section_('active')
-
 config.CRABInterface.views.active.section_('crab')
 config.CRABInterface.views.active.crab.section_('model')
 config.CRABInterface.views.active.crab.section_('formatter')
 config.CRABInterface.views.active.crab.object = 'WMCore.WebTools.RESTApi'
 config.CRABInterface.views.active.crab.templates =  os.path.join(getWMBASE(),
                                        "src/templates/WMCore/WebTools/")
-
 config.CRABInterface.views.active.crab.model.couchUrl = couchURL
 config.CRABInterface.views.active.crab.model.workloadCouchDB = workloadCouchDB
-# configCacheDBName
-config.CRABInterface.views.active.crab.model.object = 'WMCore.HTTPFrontEnd.CRAB.CRABRESTModel'
+config.CRABInterface.views.active.crab.model.object = 'CRABRESTModel'
 config.CRABInterface.views.active.crab.formatter.object = 'WMCore.WebTools.RESTFormatter'
 
 
