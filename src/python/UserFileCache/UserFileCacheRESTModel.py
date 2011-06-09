89eaa25578623252d61eee966d365dab4994285e
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2011-06-09 08:51:33 +0000

    fixes #1465
    From 35e63cbb3f5093e2fcd3a4bede90fff5b265bd46 Mon Sep 17 00:00:00 2001
    Subject: [PATCH] Add upload to CRABServer and exists() to UserFileCache
    
    From: Eric Vaandering <ewv@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABServer/branches/CRABServer_3_0_X@12965 4525493e-7705-40b1-a816-d608a930855b

--- src/python/UserFileCache/UserFileCacheRESTModel.py
+++ src/python/UserFileCache/UserFileCacheRESTModel.py
@@ -4,9 +4,10 @@
 REST Model portion for User File Cache.
 """
 
+import cherrypy
+import hashlib
 import os
 import shutil
-import hashlib
 
 from WMCore.WebTools.RESTModel import restexpose
 from WMCore.WebTools.RESTModel import RESTModel
@@ -16,13 +17,14 @@ class UserFileCacheRESTModel(RESTModel):
     A REST Model for User File Cache.
     """
     def __init__(self, config = None):
-
         if config is None:
             config = {}
         RESTModel.__init__(self, config)
 
         self.cacheDir = getattr(config, 'userCacheDir', '/tmp/UserFileCache')
 
+        self._addMethod('GET', 'exists', self.exists,
+                       args=['hashkey'], validation=[])
         self._addMethod('GET', 'status', self.status)
 
         # Manually add a file upload method because we don't want validation on the input arguments
@@ -34,7 +36,14 @@ class UserFileCacheRESTModel(RESTModel):
                                           'validation': [],
                                           'version':    1,
                                           'expires':    self.defaultExpires}
-
+        # These things are not in the config for unit tests
+        self.host = 'localhost'
+        self.port = 0
+        try:
+            self.host = self.config.Webtools.host
+            self.port = self.config.Webtools.port
+        except AttributeError:
+            pass
 
     def status(self):
         """
@@ -44,13 +53,33 @@ class UserFileCacheRESTModel(RESTModel):
         return {'up': True}
 
 
+    def exists(self, hashkey):
+        """
+        Does the file already exist on the server?
+        """
+        try:
+            if len(hashkey) < 16:
+                raise cherrypy.NotFound
+            int(hashkey, 16)
+        except ValueError:
+            raise cherrypy.NotFound
+
+        fileName = os.path.join(self.cacheDir, hashkey[0:2], hashkey)
+        if os.path.isfile(fileName):
+            self.touch(hashkey)
+            size = os.path.getsize(fileName)
+            url = 'http://%s:%s/userfilecache/download?hashkey=%s' % (self.host, self.port, hashkey)
+            return {'exists': True, 'size':size, 'hashkey':hashkey, 'url':url}
+
+        return {'exists': False}
+
+
     @restexpose
     def upload(self, userfile):
         """
         Upload the file, calculating the hash renaming it to the
         hash value. If the file already exists, just touch it
         """
-
         hasher = hashlib.sha256()
 
 
@@ -66,7 +95,7 @@ class UserFileCacheRESTModel(RESTModel):
         fileName = os.path.join(fileDir, digest)
 
         if os.path.isfile(fileName):
-            os.utime(fileName, None) # "touch" the file
+            self.touch(digest)
         else:
             if not os.path.isdir(fileDir):
                 os.makedirs(fileDir)
@@ -74,5 +103,17 @@ class UserFileCacheRESTModel(RESTModel):
             userfile.file.seek(0)
             shutil.copyfileobj(userfile.file, handle)
 
-        return {'size':size, 'name': userfile.filename, 'id' : hasher.hexdigest()}
+        url = 'http://%s:%s/userfilecache/download?hashkey=%s' % (self.host, self.port, digest)
 
+        return {'size':size, 'name':userfile.filename, 'hashkey':digest, 'url':url}
+
+    def touch(self, digest):
+        """
+        Touch the file to keep automated cleanup away
+        """
+
+        fileName = os.path.join(self.cacheDir, digest[0:2], digest)
+        #fileName = os.path.join(fileDir, digest)
+        if os.path.isfile(fileName):
+            os.utime(fileName, None)
+        return
