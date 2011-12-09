0b4b4b1f375839d024b0c3bef13560cb1959513b
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2011-12-09 13:33:42 +0000

    From a8d32daabba4475c32c1342aeba75b3a0b8d4ccc Mon Sep 17 00:00:00 2001 Subject: [PATCH] Fixes #2871
    From: Eric Vaandering <ewv@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABServer/trunk@14758 4525493e-7705-40b1-a816-d608a930855b

--- src/python/UserFileCache/UserFileCachePage.py
+++ src/python/UserFileCache/UserFileCachePage.py
@@ -10,6 +10,8 @@ import os
 from cherrypy.lib.static import serve_file
 from cherrypy import expose
 
+from WMCore.Lexicon import hnName
+from WMCore.Lexicon import check as regexCheck
 from WMCore.WebTools.Page import Page
 
 class UserFileCachePage(Page):
@@ -18,16 +20,24 @@ class UserFileCachePage(Page):
     """
 
     @expose
-    def index(self, hashkey):
+    def index(self, hashkey=None, subDir=None, name=None):
         """
         The download/ code
         """
-        try:
-            int(hashkey, 16)
-        except ValueError:
-            raise cherrypy.NotFound
 
-        fileName = os.path.join(self.config.userCacheDir, hashkey[0:2], hashkey)
+        if hashkey:
+            try:
+                int(hashkey, 16)
+            except ValueError:
+                raise cherrypy.NotFound
+            fileName = os.path.join(self.config.userCacheDir, hashkey[0:2], hashkey)
+
+        elif subDir and name:
+            hnName(subDir)
+            regexCheck(r'^[a-zA-Z0-9\-_\.]+$', name)
+            fileName = os.path.join(self.config.userCacheDir, subDir, name)
+        else:
+            raise cherrypy.NotFound
 
         if os.path.isfile(fileName):
             os.utime(fileName, None) # "touch" the file
--- src/python/UserFileCache/UserFileCacheRESTModel.py
+++ src/python/UserFileCache/UserFileCacheRESTModel.py
@@ -10,6 +10,8 @@ import os
 import shutil
 import tarfile
 
+from WMCore.Lexicon import hnName
+from WMCore.Lexicon import check as regexCheck
 from WMCore.WebTools.RESTModel import restexpose
 from WMCore.WebTools.RESTModel import RESTModel
 
@@ -25,18 +27,18 @@ class UserFileCacheRESTModel(RESTModel):
         self.cacheDir = getattr(config, 'userCacheDir', '/tmp/UserFileCache')
 
         self._addMethod('GET', 'exists', self.exists,
-                       args=['hashkey'], validation=[])
+                       args=['hashkey', 'subDir', 'name'], validation=[])
         self._addMethod('GET', 'status', self.status)
 
         # Manually add a file upload method because we don't want validation on the input arguments
-
         if not self.methods.has_key('POST'):
             self.methods['POST'] = {}
-        self.methods['POST']['upload'] = {'args':       ['userfile', 'checksum'],
+        self.methods['POST']['upload'] = {'args':       ['userfile', 'checksum', 'subDir', 'name'],
                                           'call':       self.upload,
                                           'validation': [],
                                           'version':    1,
                                           'expires':    self.defaultExpires}
+
         # These things are not in the config for unit tests
         self.host = 'localhost'
         self.port = 0
@@ -54,29 +56,40 @@ class UserFileCacheRESTModel(RESTModel):
         return {'up': True}
 
 
-    def exists(self, hashkey):
+    def exists(self, hashkey=None, subDir=None, name=None):
         """
         Does the file already exist on the server?
         """
-        try:
-            if len(hashkey) < 16:
+        # TODO: new API specifying name, dir (hn name)
+        if hashkey:
+            try:
+                if len(hashkey) < 16:
+                    raise cherrypy.NotFound
+                int(hashkey, 16)
+            except ValueError:
                 raise cherrypy.NotFound
-            int(hashkey, 16)
-        except ValueError:
-            raise cherrypy.NotFound
-
-        fileName = os.path.join(self.cacheDir, hashkey[0:2], hashkey)
-        if os.path.isfile(fileName):
-            self.touch(hashkey)
-            size = os.path.getsize(fileName)
+            fileName = os.path.join(self.cacheDir, hashkey[0:2], hashkey)
             url = 'http://%s:%s/userfilecache/download?hashkey=%s' % (self.host, self.port, hashkey)
+        else:
+            hnName(subDir)
+            regexCheck(r'^[a-zA-Z0-9\-_\.]+$', name)
+            fileName = os.path.join(self.cacheDir, subDir, name)
+            url = 'http://%s:%s/userfilecache/download?subDir=%s;name=%s' % (self.host, self.port, subDir, name)
+
+            tar = tarfile.open(fileName, mode='r')
+            lsl = [(x.name, int(x.size), int(x.mtime), x.uname) for x in tar.getmembers()]
+            haskey = hashlib.sha256(str(lsl)).hexdigest()
+
+        if os.path.isfile(fileName):
+            self.touch(fileName)
+            size = os.path.getsize(fileName)
             return {'exists': True, 'size':size, 'hashkey':hashkey, 'url':url}
 
         return {'exists': False}
 
 
     @restexpose
-    def upload(self, userfile, checksum):
+    def upload(self, userfile, checksum, subDir=None, name=None):
         """
         Upload the file, calculating the hash renaming it to the
         hash value. If the file already exists, just touch it
@@ -89,15 +102,25 @@ class UserFileCacheRESTModel(RESTModel):
         except tarfile.ReadError:
             raise cherrypy.HTTPError(400, 'File is not a .tgz file.')
 
-        fileDir = os.path.join(self.cacheDir, digest[0:2])
-        fileName = os.path.join(fileDir, digest)
+        if subDir and name:
+            hnName(subDir)
+            regexCheck(r'^[a-zA-Z0-9\-_\.]+$', name)
+            fileDir = os.path.join(self.cacheDir, subDir)
+            fileName = os.path.join(fileDir, name)
+            url = 'http://%s:%s/userfilecache/download?subDir=%s;name=%s' % (self.host, self.port, subDir, name)
+        elif checksum:
+            fileDir = os.path.join(self.cacheDir, digest[0:2])
+            fileName = os.path.join(fileDir, digest)
+            url = 'http://%s:%s/userfilecache/download?hashkey=%s' % (self.host, self.port, digest)
+        else:
+            raise cherrypy.HTTPError(400, 'Must supply name and directory or checksum.')
 
         # Basic preservation of the file integrity
-        if not (digest == checksum):
+        if digest != checksum:
             raise cherrypy.HTTPError(400, 'File transfer error: digest check failed.')
 
         if os.path.isfile(fileName):
-            self.touch(digest)
+            self.touch(fileName)
         else:
             if not os.path.isdir(fileDir):
                 os.makedirs(fileDir)
@@ -107,17 +130,12 @@ class UserFileCacheRESTModel(RESTModel):
             handle.close()
             size = os.path.getsize(fileName)
 
-        url = 'http://%s:%s/userfilecache/download?hashkey=%s' % (self.host, self.port, digest)
-
         return {'size':size, 'name':userfile.filename, 'hashkey':digest, 'url':url}
 
-    def touch(self, digest):
+    def touch(self, fileName):
         """
         Touch the file to keep automated cleanup away
         """
-
-        fileName = os.path.join(self.cacheDir, digest[0:2], digest)
-        #fileName = os.path.join(fileDir, digest)
         if os.path.isfile(fileName):
             os.utime(fileName, None)
 
