2a55d2fe5c973a12603df4c2ebcdcea09dbeca62
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2012-02-02 14:59:44 +0000

    Make LFN2PFNConverter work with SE names. Fixes #3191
    From: Marco Mascheroni <marco.mascheroni@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABServer/trunk@15073 4525493e-7705-40b1-a816-d608a930855b

--- src/python/CRABInterface/LFN2PFNConverter.py
+++ src/python/CRABInterface/LFN2PFNConverter.py
@@ -5,6 +5,8 @@ Created on Oct 19, 2011
 '''
 from WMCore.Storage.TrivialFileCatalog import readTFC
 from WMCore.Services.PhEDEx.PhEDEx import PhEDEx
+from WMCore.Services.SiteDB.SiteDB import SiteDBJSON
+from WMCore.Lexicon import cmsname
 
 class LFN2PFNConverter:
     '''
@@ -17,12 +19,16 @@ class LFN2PFNConverter:
         Constructor: create
         '''
         self.phedex = PhEDEx(responseType='xml', dict=dict)
+        self.sitedb = SiteDBJSON()
 
     def lfn2pfn(self, siteName, lfn):
         #Default: cache expires in 0.5 hours in Service
+        try:
+            cmsname(siteName)
+        except:
+            siteName = self.sitedb.seToCMSName( siteName )
         self.phedex.getNodeTFC(siteName)
-
         tfcCacheFile = self.phedex.cacheFileName('tfc', inputdata={'node': siteName})
         tfc = readTFC(tfcCacheFile)
-        return tfc.matchLFN('srmv2', lfn)
 
+        return tfc.matchLFN('srmv2', lfn)
