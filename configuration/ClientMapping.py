5a183c3cc2c10911c89b6b52fc0d159d29ae2fd1
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2012-03-08 22:35:17 +0000

    From 76c5179d8ad40ca3401d8452bb26eb9c85cc9a58 Mon Sep 17 00:00:00 2001 Subject: [PATCH] Fixes #3226
    From: Eric Vaandering <ewv@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABServer/branches/CRABServer_3_0_X@15367 4525493e-7705-40b1-a816-d608a930855b

--- configuration/ClientMapping.py
+++ configuration/ClientMapping.py
@@ -45,7 +45,7 @@ defaulturi = {
                                            "General.serverUrl", "General.ufccacheUrl", "General.requestName", "General.workArea",
                                            "JobType.pluginName", "JobType.externalPluginFile", "JobType.psetName",
                                            "JobType.inputFiles", "JobType.pyCfgParams",
-                                           "Data.unitsPerJob", "Data.splitting", "Data.inputDataset", "Data.lumiMask",
+                                           "Data.unitsPerJob", "Data.splitting", "Data.inputDataset", "Data.lumiMask", "Data.runRange",
                                            "User.email", "Data.publishDbsUrl", "Site.removeT1Blacklisting"
                                           ]
                 },
