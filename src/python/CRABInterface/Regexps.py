717cba08cc57ee608827e34cf9d1f4c52f317915
Marco Mascheroni <marco.mascheroni@cern.ch>
2012-07-31 16:50:43 +0200

    Ignore the acdcdoc and pass lumis and runs in the spec. Fixes #3947

--- src/python/CRABInterface/Regexps.py
+++ src/python/CRABInterface/Regexps.py
@@ -19,6 +19,8 @@ RX_DBSURL    = re.compile(r"^https?://cmsweb([-\w\.]*)\.cern\.ch+(:\d+)?(/([\w/_
 RX_PUBDBSURL = re.compile(r"^https?://([-\w\.]*)\.cern\.ch+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?$")
 RX_ACDCDOC   = re.compile(r"^[A-Za-z0-9]*$")
 RX_GLOBALTAG   = re.compile(r"[a-zA-Z0-9\s\.\-_:]{1,100}$")
+RX_RUNS      = re.compile(r"^\d+$")
+RX_LUMILIST  = re.compile(r"^\d+,\d+(,\d+,\d+)*$")
 
 RX_SUBRESTAT = re.compile(r"^errors|report|logs|data|schema|configcache|type|fwjr$")
 #subresources of the ServerInfo (/info) resource
