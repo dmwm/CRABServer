63be8ddf3d891295a60dc9bb602e04cbd0f1fceb
Brian Bockelman <bbockelm@cse.unl.edu>
2013-11-06 19:49:26 -0600

    Use the uuid module if uuidgen fails.

--- src/python/PandaServerInterface.py
+++ src/python/PandaServerInterface.py
@@ -558,12 +558,13 @@ def wrappedUuidGen():
     tmpSt,tmpOut = commands.getstatusoutput('which uuidgen')
     if tmpSt == 0:
         # use uuidgen
-        return commands.getoutput('uuidgen 2>/dev/null')
-    else:
-        # use python uuidgen
-        try:
-            import uuid
-        except:
-            raise ImportError,'uuidgen and uuid.py are unavailable on your system. Please install one of them'
-        return str(uuid.uuid4())
+        st, output = commands.getoutput('uuidgen 2>/dev/null')
+        if st:
+            return output
+    # use python uuidgen
+    try:
+        import uuid
+    except:
+        raise ImportError,'uuidgen and uuid.py are unavailable on your system. Please install one of them'
+    return str(uuid.uuid4())
 
