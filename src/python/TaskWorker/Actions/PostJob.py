024977a9340f1e7453fb052b61c6f10beeaa7a0b
Hassen Riahi <Hassen.Riahi@cern.ch>
2014-02-03 19:47:33 +0100

    Try to remove files from the source for resubmitted jobs.

--- src/python/TaskWorker/Actions/PostJob.py
+++ src/python/TaskWorker/Actions/PostJob.py
@@ -129,7 +129,7 @@ def getUserFromLFN(lfn):
         if lfn.split('/')[2] == 'temp':
             # /store/temp/user/$USER.$HASH/foo
             user = lfn.split('/')[4].rsplit(".", 1)[0]
-        else: 
+        else:
             # /store/user/$USER/foo
             user = lfn.split('/')[3]
     else:
@@ -240,8 +240,7 @@ class ASOServerJob(object):
                             "start_time": now,
                             "end_time": '',
                             "job_end_time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),
-                            "retry_count": [],
-                            "failure_reason": [],
+                            "failure_reason": []
                           }
             try:
                 doc = self.couchDatabase.document( doc_id )
@@ -259,6 +258,7 @@ class ASOServerJob(object):
                         "user": user,
                         "role": role,
                         "dbSource_url": "gWMS",
+                        "retry_count": [],
                         "publish_dbs_url": publish_dbs_url,
                         "dbs_url": dbs_url,
                         "workflow": self.reqname,
