8fb8f76dc699450daba9e264d6a35ef09da86e5f
Brian Bockelman <bbockelm@cse.unl.edu>
2014-03-13 16:55:05 +0100

    Weight schedd selection based on memory.

--- src/python/HTCondorLocator.py
+++ src/python/HTCondorLocator.py
@@ -1,10 +1,24 @@
 
+import types
+import bisect
 import random
 
 import classad
 import htcondor
 
 import HTCondorUtils
+
+# From http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
+def weighted_choice(choices):
+    values, weights = zip(*choices)
+    total = 0
+    cum_weights = []
+    for w in weights:
+        total += w
+        cum_weights.append(total)
+    x = random.random() * total
+    i = bisect.bisect(cum_weights, x)
+    return values[i]
 
 class HTCondorLocator(object):
 
@@ -17,9 +31,18 @@ class HTCondorLocator(object):
         """
         collector = self.getCollector()
         schedd = "localhost"
+
+        htcondor.param['COLLECTOR_HOST'] = collector
+        coll = htcondor.Collector()
+        schedds = coll.query(htcondor.AdTypes.Schedd, 'StartSchedulerUniverse =?= true', ['Name', 'DetectedMemory'])
+        schedds_dict = {}
+        for schedd in schedds:
+            if 'DetectedMemory' in schedd and 'Name' in schedd:
+                schedds_dict[schedd['Name']] = schedd['DetectedMemory']
+
         if self.config and "htcondorSchedds" in self.config:
-            random.shuffle(self.config["htcondorSchedds"])
-            schedd = self.config["htcondorSchedds"][0]
+            choices = [(i, schedds_dict.get(i, 24*1024)) for i in self.config["htcondorSchedds"]]
+            schedd = weighted_choice(choices)
         if collector:
             return "%s:%s" % (schedd, collector)
         return schedd
