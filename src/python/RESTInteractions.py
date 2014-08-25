5fcbf83aab3251e6ac10b51e91d58a0dce28ade9
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-08-25 13:51:55 -0500

    Use WMCore's REST API
    The old RESTInteractions, while nice, missed some pretty major
    functionality, the least of which is the ability to cache requests.
    Because of the number of round-trips to cmsweb (times transcontinental
    latency), each CRABClient command takes much longer than it needs to
    because it doesn't cache even simple requests like:
    
    {'result': [{'services': ['/DC=ch/DC=cern/OU=computers/CN=vocms(24[345]|3[136]|21).cern.ch']}]} 200 OK False
    {'result': [['3.3.7', '3.3.7.patch1', '3.3.7.patch2', '3.3.8', '3.3.8.rc10']]} 200 OK False
    
    This wraps the WMCore api with a simple bit of argument-smashing to get
    it to look like what the old API looked like and enables caching.

--- src/python/RESTInteractions.py
+++ src/python/RESTInteractions.py
@@ -1,151 +1,34 @@
 """
-Handles client interactions with remote REST interface
-""" 
-
-import urllib
-import os
-from urlparse import urlunparse
-from httplib import HTTPException
-
+    RESTInteractions.py - Wrap WMCore's request API. Needed to support caching 
+                     values.
+"""
+import os.path
 from WMCore.Services.Requests import JSONRequests
-from WMCore.Services.pycurl_manager import RequestHandler
 
-__version__= '0.0.0'
-EnvironmentException = Exception
-
-
-class HTTPRequests(dict):
-    """
-    This code is a simplified version of WMCore.Services.Requests - we don't
-    need all the bells and whistles here since data is always sent via json we
-    also move the encoding of data out of the makeRequest.
-
-    HTTPRequests does no logging or exception handling, these are managed by the
-    Client class that instantiates it.
-
-    NOTE: This class should be replaced by the WMCore.Services.JSONRequests if WMCore
-    is used more in the client.
-    """
-
+class RESTWrapper(JSONRequests):
     def __init__(self, url='localhost', localcert=None, localkey=None, version=None):
-        """
-        Initialise an HTTP handler
-        """
-        #set up defaults
-        self.setdefault("accept_type", 'text/html')
-        self.setdefault("content_type", 'application/x-www-form-urlencoded')
-        self.setdefault("host", url)
-        #self.setdefault("proxyfilename", proxyfilename)
-        self.setdefault("cert", localcert)
-        self.setdefault("key", localkey)
-        # get the URL opener
-        self.setdefault("conn", self.getUrlOpener())
+        if not url.startswith('https://'):
+            url = "https://%s" % url
+        cachePath = os.path.expanduser("~/.crab3_cache")
+        extraDict = {'cachepath' : cachePath,
+                'serice_name' : 'https://github.com/dmwm/CRABServer'}
+        JSONRequests.__init__(self, url, idict = extraDict)
         if not version:
-            version = __version__
-        self.setdefault("version", version)
+            version = "CRABServer/v001"
+        self.additionalHeaders = {"User-agent": 
+                                        version}
+        if localcert:
+            self['cert'] = localcert
+        if localkey:
+            self['key'] = localkey
+    
+    def makeRequest(self, uri = None, data = {}, verb = 'GET', incoming_headers={},
+                            encoder = True, decoder = True, contentType = None):
 
-    def getUrlOpener(self):
-        """
-        method getting an HTTPConnection, it is used by the constructor such
-        that a sub class can override it to have different type of connection
-        i.e. - if it needs authentication, or some fancy handler
-        """
-        return RequestHandler(config={'timeout': 300, 'connecttimeout' : 300})
+        data, responseStatus, responseReason, responseFromCache = \
+                JSONRequests.makeRequest(self, uri, data, 
+                                         verb, incoming_headers,
+                                         encoder, decoder,
+                                         contentType)
 
-    def get(self, uri = None, data = {}):
-        """
-        GET some data
-        """
-        return self.makeRequest(uri = uri, data = data, verb = 'GET')
-
-    def post(self, uri = None, data = {}):
-        """
-        POST some data
-        """
-        return self.makeRequest(uri = uri, data = data, verb = 'POST')
-
-    def put(self, uri = None, data = {}):
-        """
-        PUT some data
-        """
-        return self.makeRequest(uri = uri, data = data, verb = 'PUT')
-
-    def delete(self, uri = None, data = {}):
-        """
-        DELETE some data
-        """
-        return self.makeRequest(uri = uri, data = data, verb = 'DELETE')
-
-    def makeRequest(self, uri = None, data = {}, verb = 'GET',
-                     encoder = True, decoder = True, contentType = None):
-        """
-        Make a request to the remote database. for a give URI. The type of
-        request will determine the action take by the server (be careful with
-        DELETE!). Data should be a dictionary of {dataname: datavalue}.
-
-        Returns a tuple of the data from the server, decoded using the
-        appropriate method the response status and the response reason, to be
-        used in error handling.
-
-        You can override the method to encode/decode your data by passing in an
-        encoding/decoding function to this method. Your encoded data must end up
-        as a string.
-
-        """
-        headers = {
-                   "User-agent": "CRABClient/%s" % self['version'],
-                   "Accept": "*/*",
-                  }
-
-        #Quoting the uri since it can contain the request name, and therefore spaces (see #2557)
-        uri = urllib.quote(uri)
-        caCertPath = self.getCACertPath()
-        url = 'https://' + self['host'] + uri
-        response, datares = self['conn'].request(url, data, headers, verb=verb, doseq = True, ckey=self['key'], cert=self['cert'], \
-                            capath=caCertPath)#, verbose=True)# for debug
-        if response.status >= 400:
-            e = HTTPException()
-            setattr(e, 'req_data', data)
-            setattr(e, 'req_headers', headers)
-            setattr(e, 'url', self.buildUrl(uri))
-            setattr(e, 'result', datares)
-            setattr(e, 'status', response.status)
-            setattr(e, 'reason', response.reason)
-            setattr(e, 'headers', response.getheaders())
-            raise e
-
-        #result = json.loads(result)
-        return self.decodeJson(datares), response.status, response.reason
-
-    def decodeJson(self, result):
-        """
-        decodeJson
-
-        decode the response result reveiced from the server
-        """
-        encoder = JSONRequests()
-        return encoder.decode(result)
-
-
-    def buildUrl(self, uri):
-        """
-        Prepares the remote URL
-        """
-        scheme = 'https'
-        netloc = '%s:%s' % (self['conn'].host, self['conn'].port)
-        return urlunparse([scheme, netloc, uri, '', '', ''])
-
-    @staticmethod
-    def getCACertPath():
-        """ Get the CA certificate path. It looks for it in the X509_CERT_DIR variable if present
-            or return /etc/grid-security/certificates/ instead (if it exists)
-            If a CA certificate path cannot be found throws a EnvironmentException exception
-        """
-        caDefault = '/etc/grid-security/certificates/'
-        if os.environ.has_key("X509_CERT_DIR"):
-            return os.environ["X509_CERT_DIR"]
-        elif os.path.isdir(caDefault):
-            return caDefault
-        else:
-            raise EnvironmentException("The X509_CERT_DIR variable is not set and the %s directory cannot be found.\n" % caDefault +
-                                        "Cannot find the CA certificate path to ahuthenticate the server.")
+        return data, responseStatus, responseReason
