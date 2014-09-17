7718b49a27aee89a820d25fa11dbad3d911cfb93
Justas Balčas <juztas@gmail.com>
2014-09-17 15:00:00 +0200

    Nothing to repeat in regular expr fix

--- src/python/CRABInterface/Regexps.py
+++ src/python/CRABInterface/Regexps.py
@@ -43,7 +43,7 @@ RX_FILESTATE  = re.compile(r'^TRANSFERRING|FINISHED|FAILED|COOLOFF$')
 RX_LFNPATH   = re.compile(r"^(?=.{0,500}$)%(subdir)s(/%(subdir)s)*/?$" % lfnParts)
 RX_HOURS   = re.compile(r"^\d{0,6}$") #should be able to erase the last 100 years with 6 digits
 RX_ASOURL = RX_DBSURL
-RX_URL = re.compile(r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w :\.-]*)*$")
+RX_URL = re.compile(r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w :\.-])*$")
 #TODO!
 RX_OUT_DATASET = re.compile(r"^.*$")
 
