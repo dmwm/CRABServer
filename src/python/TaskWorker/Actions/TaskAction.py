4715a91341a7555d3974d4b953297e1c7dfd4205
Marco Mascheroni <marco.mascheroni@cern.ch>
2014-12-27 19:08:19 +0100

    Improve the logging of the TaskWorker

--- src/python/TaskWorker/Actions/TaskAction.py
+++ src/python/TaskWorker/Actions/TaskAction.py
@@ -4,8 +4,8 @@ import logging
 class TaskAction(object):
     """The ABC of all actions"""
 
-    def __init__(self, config, server = None, resturi = None):
-        self.logger = logging.getLogger(type(self).__name__)
+    def __init__(self, config, server = None, resturi = None, procnum = -1):
+        self.logger = logging.getLogger(str(procnum))
         self.config = config
         self.jobtypeMapper = {'Analysis'  : 'Processing',
                               'PrivateMC' : 'Production',
