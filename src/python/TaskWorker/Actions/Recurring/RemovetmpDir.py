27a1f5a8663992350134c67679001af7be53fc64
Marco Mascheroni <marco.mascheroni@cern.ch>
2015-01-18 18:19:22 +0100

    RemovetmpDir: take scratchdir from config, better exception handling, move now definition

--- src/python/TaskWorker/Actions/Recurring/RemovetmpDir.py
+++ src/python/TaskWorker/Actions/Recurring/RemovetmpDir.py
@@ -7,25 +7,22 @@ from datetime import date
 from TaskWorker.Actions.Recurring.BaseRecurringAction import BaseRecurringAction
 
 days = 86400*30
-now = time.time()
 
 class RemovetmpDir(BaseRecurringAction):
     pollingTime = 60*24 #minutes
 
     def _execute(self, resthost, resturi, config, task):
         self.logger.info('Checking for directory older than 30 days..')
-        for dirpath, dirnames, filenames in os.walk('/data/srv/tmp'):
+        now = time.time()
+        for dirpath, dirnames, filenames in os.walk(config.TaskWorker.scratchDir):
             for dir in dirnames:
                 timestamp = os.path.getmtime(os.path.join(dirpath,dir))
                 if now-days > timestamp:
-                    try: 
+                    try:
                         self.logger.info('Removing:')
                         self.logger.info(os.path.join(dirpath,dir))
                         shutil.rmtree(os.path.join(dirpath,dir))
-                    except Exception,e:
-                        print e
-                        pass
-                    else: 
+                    except Exception, e:
+                        self.logger.exception(e)
+                    else:
                         self.logger.info('Directory removed.')
-            
-        
