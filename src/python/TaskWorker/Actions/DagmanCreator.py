dfa008274bd616b76881273719e0e59f279fd0a5
Anna Elizabeth Woodard <awoodard@nd.edu>
2015-03-17 18:29:08 +0100

    Make changes implemented in 410a1d14577140d1573ab20c6577f803074db610 backwards-compatible with earlier client versions.
    Also, remove something I added for testing, but am not using.

--- src/python/TaskWorker/Actions/DagmanCreator.py
+++ src/python/TaskWorker/Actions/DagmanCreator.py
@@ -709,7 +709,6 @@ class DagmanCreator(TaskAction.TaskAction):
                 self.logger.error("Failed to record the number of jobs.")
                 return 1
 
-
         return info, splitterResult
 
 
