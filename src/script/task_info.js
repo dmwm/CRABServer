0feb1332371bda9d3dc14ff8efc85d229b256e82
Emilis <phonixreal@gmail.com>
2015-09-07 15:53:10 +0200

    Ops mon: Working links, no more sandbox. Fixes #4983

--- src/script/task_info.js
+++ src/script/task_info.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     // Task name that was entered by the user, is set on form submission
     var inputTaskName = "";
 
@@ -11,18 +12,17 @@ $(document).ready(function() {
         taskInfoUrl = "",
         taskStatusUrl = "",
         cacheUrl = "",
-        sandboxApiUrl = "",
+        webDirProxyApiUrl = "",
         username = "",
         userWebDir = "",
         scriptExe = "",
         inputDataset = "";
-
-    // In most cases the user won't want to override the default database
-    // setDefaultDbVersionSelector();
+        proxiedWebDirUrl = "";
 
     // If a parameter "task" exists, tries to load task info the same way a form submit loads it.
     processPageUrl();
 
+    // Prevent all future ajax calls from being cached
 
     /**
      * Task search form listener - the starting point of control flow.
@@ -39,19 +39,21 @@ $(document).ready(function() {
         // Change the URL so that it can be copied/pasted more easily
         var temp = window.location.href.split("/ui")[0] + "/ui/task/" + inputTaskName;
 
-        // If default db versin is overriden, add it to url parameters
+        // If default db version is overriden, add it to url parameters
         if (getDbVersionSelector() !== getDefaultDbVersion()) {
             temp += "/dbver/" + getDbVersionSelector();
         }
 
         window.history.pushState("", "", temp);
-        console.log("test");
 
         clearPreviousContent();
-        displayTaskInfo(handleTaskInfoErr);;
+        displayTaskInfo(handleTaskInfoErr);
     });
 
-    // Has to be run after displayTaskInfo
+    /**
+     * Saves necessary information from task info into global variables
+     * Has to be run after displayTaskInfo
+     */
     function loadGlobalDataFromTaskInfo() {
         userWebDir = "", username = "", cacheUrl = "", scriptExe = "", inputDataset = "";
 
@@ -81,21 +83,16 @@ $(document).ready(function() {
 
     /**
      * Called on task info search form submission. It then fetches JSON data
-     * and inserts it into the task_info.html #task-info-table.
+     * and inserts it into the #task-info-table.
      */
     function displayTaskInfo(errHandler) {
         var xmlhttp = new XMLHttpRequest();
         // var url = "https://mmascher-mon.cern.ch/crabserver/dev/task?subresource=search&workflow=";
         var url = taskInfoUrl + inputTaskName;
-        console.log(url);
-
-
 
         function queryApi(url) {
             $.ajax(url)
                 .done(function(data) {
-                    console.log("TW info:");
-                    console.log(data);
                     // Storing the data for the use of other display functions
                     taskInfo = data;
 
@@ -104,14 +101,12 @@ $(document).ready(function() {
                         $("#task-info-table tbody")
                             .append("<tr><td>" + data.desc.columns[i] + "</td><td>" + data.result[i] + "</td></tr>");
                     }
-
                     loadOtherData();
 
                 })
                 .fail(function(xhr) {
                     var headers = xhr.getAllResponseHeaders().toLowerCase();
                     errHandler(new ServerError(headers));
-                    console.log("xhr status:" + xhr.status);
                     loadOtherData();
                 })
         };
@@ -121,12 +116,10 @@ $(document).ready(function() {
     /**
      * Fetches and displays the config/ PSet files for given task.
      * It first querys an api which either returns a proxied url (which is needed to get around firewalls)
-     * or returns nothing, in which case no proxying is needed.
+     * or returns nothing, in which case the files cannot be retrieved.
      *
-     * For getting the files out of the sandbox.tar.gz archive, it uses a tar.gz library.
      */
     function displayConfigAndPSet(errHandler) {
-        // var userWebDir = "";
         if (userWebDir === "") {
             errHandler(new TaskInfoUndefinedError());
             return;
@@ -134,45 +127,43 @@ $(document).ready(function() {
             // If user webdir wasn't created at all
             errHandler(new UserWebDirUndefinedError());
             return;
-        } else if (sandboxUrl === "") {
+        } else if (proxiedWebDirUrl === "") {
             // In case proxy api returned empty or failed
-            errHandler(new SandboxNotLoadedError);
+            // Set links, show error and don't load anything else.
+            $("#task-config-link").attr("href", userWebDir + urlEnd);
+            $("#task-pset-link").attr("href", userWebDir + urlEnd);
+            errHandler(new ProxyNotFoundErrorError);
             return;
         }
+        
+        $.ajax(proxiedWebDirUrl + "/debug/crabConfig.py")
+            .done(function(data) {
+                $("#task-config-paragraph").text(data);
+            });
+        
+        $.ajax(proxiedWebDirUrl + "/debug/originalPSet.py")
+            .done(function(data) {
+                $("#task-pset-paragraph").text(data);
+            });
 
-        var urlEnd = "/sandbox.tar.gz";
-
-        // Goes through all the files and picks out the ones that are needed
-        var tgz = TarGZ.stream(sandboxUrl + urlEnd, function(f, h) {
-            if (f.filename == "debug/crabConfig.py") {
-                $("#task-config-paragraph").text(f.data);
-            }
-
-            if (f.filename == "debug/originalPSet.py") {
-                $("#task-pset-paragraph").text(f.data);
-            }
-        }, null, handleTarGZCallbackErr);
-        $("#task-config-link").attr("href", userWebDir + urlEnd);
-        $("#task-pset-link").attr("href", userWebDir + urlEnd);
+        $("#task-config-link").attr("href", userWebDir + "/debug/crabConfig.py");
+        $("#task-pset-link").attr("href", userWebDir + "/debug/originalPSet.py");
     }
 
-    function querySandboxApi() {
-        console.log("sandbox url " + sandboxApiUrl + inputTaskName);
-        $.ajax(sandboxApiUrl + inputTaskName)
+    function queryWebDirProxyApi() {
+        $.ajax(webDirProxyApiUrl + inputTaskName)
             .done(function(data) {
-                sandboxUrl = data.result[0];
+                proxiedWebDirUrl = data.result[0];
 
-                if (sandboxUrl === undefined || sandboxUrl == "None") {
-                    sandboxUrl = "";
+                if (proxiedWebDirUrl === undefined || proxiedWebDirUrl == "None") {
+                    proxiedWebDirUrl = "";
                 }
 
                 displayConfigAndPSet(handleConfigPSetErr);
                 displayScriptExe(handleScriptExeErr);
             })
             .fail(function(xhr) {
-                // TODO process error?
-                console.log("error querying sandbox api");
-                sandboxUrl = "";
+                proxiedWebDirUrl = "";
                 displayConfigAndPSet(handleConfigPSetErr);
                 displayScriptExe(handleScriptExeErr);
             });
@@ -201,10 +192,8 @@ $(document).ready(function() {
                     errHandler(new ServerError(headers));
                 });
         }
-
         $("#taskworker-log-link").attr("href", url);
         queryApi(url);
-
     }
 
     /**
@@ -230,8 +219,6 @@ $(document).ready(function() {
                     errHandler(new ServerError(headers));
                 });
         }
-
-        console.log("ulog link: " + url);
         $("#upload-log-link").attr("href", url);
         queryApi(url);
     }
@@ -251,27 +238,26 @@ $(document).ready(function() {
             // If user webdir wasn't created at all
             errHandler(new UserWebDirUndefinedError());
             return;
-        } else if (sandboxUrl === "") {
+        } else if (proxiedWebDirUrl === "") {
             // In case proxy api returned empty or failed
-            errHandler(new SandboxNotLoadedError);
+            // Set links, show error and don't load anything else.
+            $("#script-exe-link").attr("href", userWebDir + urlEnd);
+            errHandler(new ProxyNotFoundErrorError);
             return;
         }
 
+        $.ajax(proxiedWebDirUrl + "/debug/originalPSet.py")
+            .done(function(data) {
+                $("#script-exe-paragraph").text(data);
+            });
 
-        var urlEnd = "/sandbox.tar.gz";
-
-
-
-        var tgz = TarGZ.stream(sandboxUrl + urlEnd, function(f, h) {
-            if (f.filename == scriptExe) {
-                $("#script-exe-paragraph").text(f.data);
-                console.log(f.data);
-            }
-        }, null, handleScriptExeCallbackErr);
-        $("#script-exe-link").attr("href", userWebDir + urlEnd);
-
+        $("#script-exe-link").attr("href", userWebDir + "/debug/" + scriptExe);
     }
 
+
+    /**
+     * Displays main page information - sets correct links and loads task status.
+     */
     function displayMainPage(errHandler) {
         if (userWebDir !== "" && inputTaskName !== "" && inputTaskName !== undefined) {
 
@@ -300,9 +286,6 @@ $(document).ready(function() {
                             }
                         }
                     })
-                    .fail(function(xhr) {
-                        // TODO handle some errors?
-                    });
             }
             queryApi(url);
         } else {
@@ -340,16 +323,13 @@ $(document).ready(function() {
 
             if (headers != undefined && headers != "") {
                 var headerArray = processErrorHeaders(headers);
-                console.log(headerArray);
                 for (var i = 0; i < headerArray.length; i++) {
                     var colonIndex = headerArray[i].search(":");
-                    // console.log(headerArray[i].substr(0, colonIndex + 1) + " " + headerArray[i].substr(colonIndex + 1));
                     $("#upload-log-error-box").append("<span id=\"spaced-span\">" + headerArray[i].substr(0, colonIndex + 1) + "</span><span>" + headerArray[i].substr(colonIndex + 1) + "</span><br/>");
                 }
             } else {
-                $("#upload-log-error-box").css("display", "inherit").text("Couldn't load UploadLog, you can download it from the link below.");
+                $("#upload-log-error-box").css("display", "inherit").text("Couldn't load UploadLog, please try the link below.");
             }
-
         }
     }
 
@@ -358,7 +338,6 @@ $(document).ready(function() {
 
         var headers = err.headers;
         var headerArray = processErrorHeaders(headers);
-        console.log(headers);
         for (var i = 0; i < headerArray.length; i++) {
             var colonIndex = headerArray[i].search(":");
             $("#task-info-error-box").append("<span id=\"spaced-span\">" + headerArray[i].substr(0, colonIndex + 1) + "</span><span>" + headerArray[i].substr(colonIndex + 1) + "</span><br/>");
@@ -385,7 +364,7 @@ $(document).ready(function() {
                     $("#taskworker-log-error-box").append("<span id=\"spaced-span\">" + headerArray[i].substr(0, colonIndex + 1) + "</span><span>" + headerArray[i].substr(colonIndex + 1) + "</span>\n");
                 }
             } else {
-                $("#taskworker-log-error-box").css("display", "inherit").text("Couldn't load TaskWorker log, you can download it from the link below.");
+                $("#taskworker-log-error-box").css("display", "inherit").text("Couldn't load TaskWorker log, please try the link below.");
             }
         } else if (err instanceof TaskInfoUndefinedError) {
             $("#taskworker-log-error-box").empty().css("display", "inherit").text("Task info not loaded");
@@ -393,11 +372,9 @@ $(document).ready(function() {
     }
 
     function handleConfigPSetErr(err) {
-        if (err instanceof SandboxNotLoadedError) {
-            $("#task-config-error-box").css("display", "inherit").text("Couldn't load config, please open sandbox.tar.gz " + 
-                "from the link below and look for it under debug/originalConfig.py");
-            $("#task-pset-error-box").css("display", "inherit").text("Couldn't load PSet, please open sandbox.tar.gz " + 
-                "from the link below and look for it under debug/originalPset.py");
+        if (err instanceof ProxyNotFoundErrorError) {
+            $("#task-config-error-box").css("display", "inherit").text("Couldn't load config, please try the link below.");
+            $("#task-pset-error-box").css("display", "inherit").text("Couldn't load PSet, please try the link below.");
         } else if (err instanceof TaskInfoUndefinedError) {
             $("#task-config-error-box").css("display", "inherit").text("Task Info not loaded, can't get config");
             $("#task-pset-error-box").css("display", "inherit").text("Task Info not loaded, can't get PSet")
@@ -410,9 +387,8 @@ $(document).ready(function() {
     function handleScriptExeErr(err) {
         if (err instanceof ScriptExeNotUsedError) {
             $("#script-exe-error-box").css("display", "inherit").text("ScriptExe was not used");
-        } else if (err instanceof SandboxNotLoadedError) {
-            $("#script-exe-error-box").css("display", "inherit").text("Couldn't load ScriptExe, please open sandbox.tar.gz " +
-                "from the link below and look for " + scriptExe);
+        } else if (err instanceof ProxyNotFoundErrorError) {
+            $("#script-exe-error-box").css("display", "inherit").text("Couldn't load ScriptExe, please try the link below.");
         } else if (err instanceof TaskInfoUndefinedError) {
             $("#script-exe-error-box").css("display", "inherit").text("Task info not loaded");
         } else if (err instanceof UserWebDirUndefinedError) {
@@ -426,19 +402,6 @@ $(document).ready(function() {
 
     function handleMainErr(err) {
         $("#main-error-box").css("display", "inherit").text("Task info not loaded");
-    }
-
-    /**
-     * Callback function for handling tar file related problems. (404 not found for example)
-     * Not as verbose as html headers.
-     */
-    function handleTarGZCallbackErr(xhr, err) {
-        // Workaround for a possible bug in GZip library - if redirected 
-        // to tar.gz file it throws an error but still processes it correctly
-        if (err === undefined || err.search("Not a GZip file") === -1) {
-            $("#task-config-error-box").css("display", "inherit").text(err ? err : xhr.status);
-            $("#task-pset-error-box").css("display", "inherit").text(err ? err : xhr.status);
-        }
     }
 
     function ServerError(headers) {
@@ -459,8 +422,8 @@ $(document).ready(function() {
         this.name = "ScriptExeNotUsedError";
     }
 
-    function SandboxNotLoadedError() {
-        this.name = "SandboxNotLoadedError";
+    function ProxyNotFoundErrorError() {
+        this.name = "ProxyNotFoundErrorError";
     }
 
     function UserWebDirUndefinedError() {
@@ -472,17 +435,17 @@ $(document).ready(function() {
             case "prod":
                 taskInfoUrl = "https://" + document.domain + "/crabserver/prod/task?subresource=search&workflow=";
                 taskStatusUrl = "https://" + document.domain + "/crabserver/prod/workflow?workflow=";
-                sandboxApiUrl = "https://" + document.domain + "/crabserver/prod/task?subresource=webdirprx&workflow="
+                webDirProxyApiUrl = "https://" + document.domain + "/crabserver/prod/task?subresource=webdirprx&workflow="
                 break;
             case "preprod":
                 taskInfoUrl = "https://" + document.domain + "/crabserver/preprod/task?subresource=search&workflow=";
                 taskStatusUrl = "https://" + document.domain + "/crabserver/preprod/workflow?workflow=";
-                sandboxApiUrl = "https://" + document.domain + "/crabserver/preprod/task?subresource=webdirprx&workflow="
+                webDirProxyApiUrl = "https://" + document.domain + "/crabserver/preprod/task?subresource=webdirprx&workflow="
                 break;
             case "dev":
                 taskInfoUrl = "https://" + document.domain + "/crabserver/dev/task?subresource=search&workflow=";
                 taskStatusUrl = "https://" + document.domain + "/crabserver/dev/workflow?workflow=";
-                sandboxApiUrl = "https://" + document.domain + "/crabserver/dev/task?subresource=webdirprx&workflow="
+                webDirProxyApiUrl = "https://" + document.domain + "/crabserver/dev/task?subresource=webdirprx&workflow="
                 break;
             default:
                 break;
@@ -500,7 +463,6 @@ $(document).ready(function() {
             default:
                 $("#db-selector-box").val("dev")
                 break;
-
         }
     }
 
@@ -526,7 +488,6 @@ $(document).ready(function() {
      */
     function processPageUrl() {
         var urlArray = window.location.href.split("\/");
-        console.log(urlArray);
         var taskIndex = $.inArray("task", urlArray);
 
         var dbVersionIndex = $.inArray("dbver", urlArray);
@@ -551,13 +512,11 @@ $(document).ready(function() {
             dbVersion = getDbVersionSelector();
             setUrls(dbVersion);
         }   
-
-        
     }
 
     function loadOtherData() {
         loadGlobalDataFromTaskInfo();
-        querySandboxApi();
+        queryWebDirProxyApi();
         // displayConfigAndPSet(handleConfigPSetErr);
         displayTaskWorkerLog(handleTaskWorkerLogErr);
         displayUploadLog(handleUploadLogErr);
@@ -573,21 +532,3 @@ $(document).ready(function() {
         $(".dynamic-content").empty();
     }
 });
-
-// var t = function() {
-//         $.ajax("https://mmasher-mon.cern.ch/scheddmon/059/cms1425/150805_100838:erupeika_crab_tutorial_May2015_MC_analysis",
-//             function(data, status, xhr) {
-//                 console.log("success");
-//                 console.log(data);
-//                 console.log(xhr.status);
-
-//             })
-//             .fail(function(xhr) {
-//                 console.log(xhr.getAllResponseHeaders());
-//                 console.log(xhr.status);
-//             })
-//             .onerror(function() {
-//                 alert("w");
-//             });
-//     }
-    // t();
