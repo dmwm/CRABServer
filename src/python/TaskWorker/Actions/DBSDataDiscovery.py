f73384983e9b08458e599cecdfd5e7e95414cdf1
Marco Mascheroni <marco.mascheroni@cern.ch>
2015-12-16 13:56:33 +0100

    pylint

--- src/python/TaskWorker/Actions/DBSDataDiscovery.py
+++ src/python/TaskWorker/Actions/DBSDataDiscovery.py
@@ -129,7 +129,7 @@ class DBSDataDiscovery(DataDiscovery):
                     secinfos['lumiobj'] = LumiList(runsAndLumis=secinfos['Lumis'])
 
                 self.logger.info("Beginning to match files from secondary dataset")
-                for filename, infos in filedetails.items():
+                for dummyFilename, infos in filedetails.items():
                     infos['Parents'] = []
                     lumis = LumiList(runsAndLumis=infos['Lumis'])
                     for secfilename, secinfos in moredetails.items():
