49cadc3dcd2905dc68d9508218a1e6da03a0f4c1
Marco Mascheroni <marco.mascheroni@cern.ch>
2015-12-17 17:10:46 +0100

    Remove T1 when we make the list of sites for PrivateMC

--- src/python/TaskWorker/Actions/MakeFakeFileSet.py
+++ src/python/TaskWorker/Actions/MakeFakeFileSet.py
@@ -9,13 +9,32 @@ from WMCore.Services.SiteDB.SiteDB import SiteDBJSON
 
 
 class MakeFakeFileSet(TaskAction):
-    """This is needed to make WMCore.JobSplitting lib working... 
-       do not like very much. Given that all is fake here I am 
-       quite sure we only need total number of events said that I 
-       set all the other parmas to dummy values. We may want to set 
+    """This is needed to make WMCore.JobSplitting lib working...
+       do not like very much. Given that all is fake here I am
+       quite sure we only need total number of events said that I
+       set all the other parmas to dummy values. We may want to set
        them in the future"""
- 
-    def execute(self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
+        TaskAction.__init__(self, *args, **kwargs)
+        self.sbj = SiteDBJSON({"key":self.config.TaskWorker.cmskey,
+                          "cert":self.config.TaskWorker.cmscert})
+
+
+    def getListOfSites(self):
+        """ Get the list of sites to use for PrivateMC workflows.
+            For the moment we are filtering out T1_ since they are precious resources
+            and don't want to overtake production (WMAgent) jobs there. In the
+            future we would like to take this list from the SSB.
+        """
+        sites = self.sbj.getAllCMSNames()
+        filteredSites = [site for site in sites if not site.startswith("T1_")]
+
+        return filteredSites
+
+
+    #even though args is not used we call "handler.actionWork(args, kwargs)" in Handler
+    def execute(self, *args, **kwargs): #pylint: disable=unused-argument
 
         totalevents = kwargs['task']['tm_totalunits']
         firstEvent = 1
@@ -32,9 +51,7 @@ class MakeFakeFileSet(TaskAction):
         #MC comes with only one MCFakeFile
         singleMCFileset = Fileset(name = "MCFakeFileSet")
         newFile = File("MCFakeFile", size = 1000, events = totalevents)
-        sbj = SiteDBJSON({"key":self.config.TaskWorker.cmskey,
-                          "cert":self.config.TaskWorker.cmscert})
-        newFile.setLocation(sbj.getAllCMSNames())
+        newFile.setLocation(self.getListOfSites())
         newFile.addRun(Run(1, *range(firstLumi, lastLumi + 1)))
         newFile["block"] = 'MCFakeBlock'
         newFile["first_event"] = firstEvent
