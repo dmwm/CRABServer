d1512436056851cc3f869fbd2ec7e89ac9e3bf89
Emilis Rupeika <phonixreal@gmail.com>
2016-03-21 17:23:59 +0100

    Adding Utils to CRAB3.zip

--- bin/htcondor_make_runtime.sh
+++ bin/htcondor_make_runtime.sh
@@ -57,7 +57,7 @@ if [[ "x$RPM_RELEASE" != "x" ]]; then
 
     pushd $ORIGDIR/../WMCore-$WMCOREVER/build/lib/
     zip -r $STARTDIR/WMCore.zip *
-    zip -rq $STARTDIR/CRAB3.zip WMCore PSetTweaks -x \*.pyc || exit 3
+    zip -rq $STARTDIR/CRAB3.zip WMCore PSetTweaks Utils -x \*.pyc || exit 3
     popd
 
     pushd $ORIGDIR/build/lib
@@ -135,7 +135,7 @@ else
 
     pushd $WMCORE_PATH/src/python
     zip -rq $STARTDIR/WMCore.zip * || exit 3
-    zip -rq $STARTDIR/CRAB3.zip WMCore PSetTweaks -x \*.pyc || exit 3
+    zip -rq $STARTDIR/CRAB3.zip WMCore PSetTweaks Utils -x \*.pyc || exit 3
     popd
 
     pushd $CRABSERVER_PATH/src/python
