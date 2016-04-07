1b1bcf587866fb6509e9847f8a0b7d077596c275
Matthias Wolf <matthias.wolf@cern.ch>
2016-04-07 11:21:14 +0200

    Don't transfer and publish output for the probe job.

--- src/python/TaskWorker/Actions/DagmanCreator.py
+++ src/python/TaskWorker/Actions/DagmanCreator.py
@@ -382,7 +382,7 @@ class DagmanCreator(TaskAction.TaskAction):
         return self.task['tm_activity']
 
 
-    def makeJobSubmit(self, task):
+    def makeJobSubmit(self, task, realjob):
         """
         Create the submit file.  This is reused by all jobs in the task; differences
         between the jobs are taken care of in the makeDagSpecs.
@@ -415,7 +415,7 @@ class DagmanCreator(TaskAction.TaskAction):
         info['asyncdest'] = info['tm_asyncdest']
         info['dbsurl'] = info['tm_dbs_url']
         info['publishdbsurl'] = info['tm_publish_dbs_url']
-        info['publication'] = 1 if info['tm_publication'] == 'T' else 0
+        info['publication'] = 1 if realjob and info['tm_publication'] == 'T' else 0
         info['userdn'] = info['tm_user_dn']
         info['requestname'] = string.replace(task['tm_taskname'], '"', '')
         info['savelogsflag'] = 1 if info['tm_save_logs'] == 'T' else 0
@@ -439,7 +439,7 @@ class DagmanCreator(TaskAction.TaskAction):
         # TODO: pass through these correctly.
         info['runs'] = []
         info['lumis'] = []
-        info['saveoutput'] = 1 if info['tm_transfer_outputs'] == 'T' else 0
+        info['saveoutput'] = 1 if realjob and info['tm_transfer_outputs'] == 'T' else 0
         info['accounting_group'] = 'analysis.%s' % info['userhn']
         info = transform_strings(info)
         info['faillimit'] = task['tm_fail_limit']
@@ -578,7 +578,8 @@ class DagmanCreator(TaskAction.TaskAction):
         ## file and we would take it from the Task DB.
         kwargs['task']['numautomjobretries'] = getattr(self.config.TaskWorker, 'numAutomJobRetries', 2)
 
-        info = self.makeJobSubmit(kwargs['task'])
+        realjob = stage is not 'probe'
+        info = self.makeJobSubmit(kwargs['task'], realjob)
 
         outfiles = kwargs['task']['tm_outfiles'] + kwargs['task']['tm_tfile_outfiles'] + kwargs['task']['tm_edm_outfiles']
 
