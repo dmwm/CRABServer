c6ac584c4fe7e5ab9fda03c175b5edc9b5fb9b05
Matthias Wolf <matthias.wolf@cern.ch>
2016-04-07 10:04:37 +0200

    Save the node state per dag.

--- src/python/TaskWorker/Actions/DagmanCreator.py
+++ src/python/TaskWorker/Actions/DagmanCreator.py
@@ -39,7 +39,7 @@ from ApmonIf import ApmonIf
 
 DAG_HEADER = """
 
-NODE_STATUS_FILE node_state 30 ALWAYS-UPDATE
+NODE_STATUS_FILE node_state{nodestate} 30 ALWAYS-UPDATE
 
 # NOTE: a file must be present, but 'noop' makes it not be read.
 #FINAL FinalCleanup Job.1.submit NOOP
@@ -749,7 +749,10 @@ class DagmanCreator(TaskAction.TaskAction):
             self.logger.warning(msg)
 
         ## Write down the DAG as needed by DAGMan.
-        dag = DAG_HEADER.format(resthost=kwargs['task']['resthost'], resturiwfdb=kwargs['task']['resturinoapi'] + '/workflowdb')
+        dag = DAG_HEADER.format(
+                nodestate='' if subjob is None else '.{0}'.format(startjobid),
+                resthost=kwargs['task']['resthost'],
+                resturiwfdb=kwargs['task']['resturinoapi'] + '/workflowdb')
         for dagSpec in dagSpecs:
             dag += DAG_FRAGMENT.format(**dagSpec)
             if subjob is None:
