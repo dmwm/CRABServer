a11baec4cdfcb13abbe2a70e17b1d2989fbc1961
Marco Mascheroni <m.masche@gmail.com>
2017-01-24 10:05:57 +0100

    Add the possibility to use egroups for localuser. Fixes #5276 (#5352)

--- src/python/CMSGroupMapper.py
+++ src/python/CMSGroupMapper.py
@@ -2,10 +2,32 @@ from __future__ import print_function
 
 import os
 import re
+import ldap
 import time
 
 g_cache = {}
 g_expire_time = 0
+
+
+def get_egroup_users(egroup_name):
+    """ Given an egroup name it returns the whole list of user contained in this egroup
+    """
+    res = set()
+    searchFilter = "memberOf=CN=%s,OU=e-groups,OU=Workgroups,DC=cern,DC=ch" % egroup_name
+    searchAttribute = ["sAMAccountName"]
+    l = ldap.initialize("ldaps://xldap.cern.ch:636")
+    basedn = "DC=cern,DC=ch"
+    searchScope = ldap.SCOPE_SUBTREE
+
+    ldap_result_id = l.search(basedn, searchScope, searchFilter, searchAttribute)
+
+    result_type, result_data = l.result(ldap_result_id, timeout=100)
+    for item in result_data:
+        user = item[1]['sAMAccountName'][0]
+        res.add(user)
+
+    return res
+
 
 def cache_users(logFunction=print):
     """ Cache the entries in the variuos local-users.txt files
@@ -34,20 +56,39 @@ def cache_users(logFunction=print):
         g_expire_time = time.time() + 60
         return
     for entry in sites:
-        full_path = os.path.join(base_dir, entry, 'GlideinConfig', 'local-users.txt')
-        if (entry == 'local') or (not os.path.isfile(full_path)):
+        if (entry == 'local'):
             continue
-        try:
-            fd = open(full_path)
-            for line in fd:
-                line = line.strip()
-                if user_re.match(line):
-                    group_set = cache.setdefault(line, set())
-                    group_set.add(entry)
-        except OSError as ose:
-            logFunction("Cannot list SITECONF directories in cvmfs:" + str(ose))
-            raise
-
+        #first take care of users that are directly in local-users.txt
+        full_path = os.path.join(base_dir, entry, 'GlideinConfig', 'local-users.txt')
+        if os.path.isfile(full_path):
+            try:
+                fd = open(full_path)
+                for user in fd:
+                    user = user.strip()
+                    if user_re.match(user):
+                        group_set = cache.setdefault(user, set())
+                        group_set.add(entry)
+            except OSError as ose:
+                logFunction("Cannot list SITECONF directories in cvmfs:" + str(ose))
+                raise
+        #then do the same for users in local-egroups.txt
+        full_path = os.path.join(base_dir, entry, 'GlideinConfig', 'local-egroups.txt')
+        if os.path.isfile(full_path):
+            egroup = ''
+            try:
+                fd = open(full_path)
+                for egroup in fd:
+                    egroup = egroup.strip()
+                    users = get_egroup_users(egroup)
+                    for user in users:
+                        if user_re.match(user):
+                            group_set = cache.setdefault(user, set())
+                            group_set.add(entry)
+            except OSError as ose:
+                logFunction("Cannot list SITECONF egroups in cvmfs:" + str(ose))
+                raise
+            except ldap.LDAPError as le:
+                logFunction("Cannot get user list from egroup: %s. Error: %s\n\t" % (egroup, str(le)))
 
     g_cache = cache
     g_expire_time = time.time() + 15*60
