3eced2a4ab3b0d910ee2229d92ba8e574747d1db
Emilis Rupeika <phonixreal@gmail.com>
2017-03-07 17:17:22 +0100

    Invert logic in TP when checking when to exit

--- scripts/task_process/task_proc_wrapper.sh
+++ scripts/task_process/task_proc_wrapper.sh
@@ -18,10 +18,11 @@ echo "CLUSTER_ID: $CLUSTER_ID"
 sleep 60s
 TIME_OF_LAST_QUERY=$(date +"%s")
 
-# Loop will exit when condor_q returns 5 or 6, meaning that the main dag has been stopped and will no longer be updated.
-# For the initial query period, set CONDOR_RES to something other than empty. Since querying condor immediatly after
+# Loop will exit when condor_q does not return 1 (idle) or 2 (running), meaning that the dag reached a final state
+# and no more changes should happen to the status.
+# For the initial query period, set DAG_STATUS to something other than empty. Since querying condor immediatly after
 # submission is most likely pointless and relatively expensive, the script will run normally and perform the query later.
-CONDOR_RES="init"
+DAG_STATUS="init"
 
 echo "Starting task daemon wrapper"
 while true
@@ -32,20 +33,20 @@ do
 
     # Calculate how much time has passed since the last condor_q and perform it again if it has been long enough.
     if [ $(($(date +"%s") - $TIME_OF_LAST_QUERY)) -gt $(($HOURS_BETWEEN_QUERIES * 3600)) ]; then
-        CONDOR_RES=$(condor_q $CLUSTER_ID -af JobStatus)
+        DAG_STATUS=$(condor_q $CLUSTER_ID -af JobStatus)
         TIME_OF_LAST_QUERY=$(date +"%s")
 
         echo "Query done on $(date)"
     fi
 
-    # Once the dag reaches one of the final states (4, 5 or 6), no further updates are expected to the log files
-    # and the task_process can exit.
+    # Once the dag's status changes from 1 (idle) or 2 (running) to something else,
+    # no further updates are expected to the log files and the task_process can exit.
     # In a rare case (if this script does condor_q right after the the dag changes states),
     # it is actually possible that task_process would exit before the changes contributing to the status are propagated.
     # Adding a short sleep would solve this, however, that then presents another problem of task_process not starting
     # at all if the task is resubmitted during this short sleep period.
-    echo "Dag status code: $CONDOR_RES"
-    if [[ "$CONDOR_RES" == 4 || "$CONDOR_RES" == 5 || "$CONDOR_RES" == 6 ]]; then
+    echo "Dag status code: $DAG_STATUS"
+    if [[ "$DAG_STATUS" != 1 && "$DAG_STATUS" != 2 && "$DAG_STATUS" != "init" ]]; then
         echo "Dag is in one of the final states. Removing the task_process/task_process_running file and exiting."
         rm task_process/task_process_running
 
