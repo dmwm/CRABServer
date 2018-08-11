5346d57909caff077e6ea9dcd9a648107523af04
belforte <stefano.belforte@cern.ch>
2018-08-11 23:43:14 +0200

    do not use handleException if there's no exception, fix #5713

--- scripts/CMSRunAnalysis.py
+++ scripts/CMSRunAnalysis.py
@@ -570,8 +570,15 @@ def extractUserSandbox(archiveJob, cmsswVersion):
 def getProv(filename, scram):
     ret = scram("edmProvDump %s" % filename, runtimeDir=os.getcwd(), logName="edmProvDumpOutput.log")
     if ret > 0:
-        msg = scram.diagnostic()
-        handleException("FAILED", EC_CMSRunWrapper, 'Error getting pset hash from file.\n\tScram Env %s\n\tCommand:edmProvDump %s' % (msg, filename))
+        scramMsg = scram.diagnostic()
+        if os.path.isfile("edmProvDumpOutput.log"):
+            with open("edmProvDumpOutput.log", "r") as fd:
+                output = fd.read()
+            scramMsg += "\n" + output
+
+        msg = "FAILED (%s)\n" % EC_CMSRunWrapper
+        msg += "Error getting pset hash from file.\n\tCommand:edmProvDump %s\n\tScram Diagnostic %s" % (filename, scramMsg)
+        print(msg)
         mintime()
         sys.exit(EC_CMSRunWrapper)
     output = ''
@@ -966,7 +973,7 @@ if __name__ == "__main__":
         # allow us to use a different LFN on job failure.
         report['jobExitCode'] = jobExitCode
         AddChecksums(report)
-        if not 'CRAB3_RUNTIME_DEBUG' in os.environ:
+        if not 'CRAB3_RUNTIME_DEBUG' in os.environ or True: 
             try:
                 AddPsetHash(report, scram)
             except Exception as ex:
