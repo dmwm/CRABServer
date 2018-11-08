46e7a9294bf2a4966fa3e6d938efe2f9f3eb9594
belforte <stefano.belforte@cern.ch>
2018-11-08 11:06:03 -0600

    Revert "reapply https://github.com/belforte/CRABServer/commit/60b1a021165a07505523c62b54d1f188ac48b8cc  use context handler to temporary change logLevel. Fix #5873"
    This reverts commit ceb2b12d0dc2aa93387f74b5a1804d043d9fd95e.

--- src/python/TaskWorker/Actions/DataDiscovery.py
+++ src/python/TaskWorker/Actions/DataDiscovery.py
@@ -12,7 +12,6 @@ from WMCore.Services.CRIC.CRIC import CRIC
 from TaskWorker.Actions.TaskAction import TaskAction
 from TaskWorker.DataObjects.Result import Result
 from TaskWorker.WorkerExceptions import TaskWorkerException
-from ServerUtilities import tempSetLogLevel
 
 class DataDiscovery(TaskAction):
     """
@@ -35,59 +34,54 @@ class DataDiscovery(TaskAction):
         uniquelumis = set()
         datasetLumis = {}
         ## Loop over the sorted list of files.
-        configDict = {"cacheduration": 1, "pycurl": True} # cache duration is in hours
-        resourceCatalog = CRIC(logger=self.logger, configDict=configDict)
         # can't affort one message from CRIC per file, unless critical !
-        with tempSetLogLevel(logger=self.logger, level=logging.ERROR):
-            for lfn, infos in datasetfiles.iteritems():
-                ## Skip the file if it is not in VALID state.
-                if not infos.get('ValidFile', True):
-                    self.logger.warning("Skipping invalid file %s", lfn)
-                    continue
-                ## Skip the file if the block has not been found or has no locations.
-                if not infos['BlockName'] in locations or not locations[infos['BlockName']]:
-                    self.logger.warning("Skipping %s because its block (%s) has no locations", lfn, infos['BlockName'])
-                    blocksWithNoLocations.add(infos['BlockName'])
-                    continue
+        previousLogLevel=self.logger.getEffectiveLevel()
+        resourceCatalog = CRIC(logger=self.logger)
+        self.logger.setLevel(logging.ERROR)
+        for lfn, infos in datasetfiles.iteritems():
+            ## Skip the file if the block has not been found or has no locations.
+            if not infos['BlockName'] in locations or not locations[infos['BlockName']]:
+                self.logger.warning("Skipping %s because its block (%s) has no locations" % (lfn, infos['BlockName']))
+                continue
+            ## Skip the file if it is not in VALID state.
+            if not infos.get('ValidFile', True):
+                self.logger.warning("Skipping invalid file %s" % lfn)
+                continue
 
-                if task['tm_use_parent'] == 1 and len(infos['Parents']) == 0:
-                    raise TaskWorkerException(
-                            "The CRAB3 server backend refuses to submit jobs to the Grid scheduler\n" +
-                            "because you specified useParents=True but some your files have no" +
-                            "parents.\nExample: " + lfn)
-                ## Create a WMCore File object.
-                try:
-                    size = infos['FileSize']
-                    checksums = {'Checksum': infos['Checksum'], 'Adler32': infos['Adler32'], 'Md5': infos['Md5']}
-                except:
-                    #This is so that the task worker does not crash if an old version of WMCore is used (the interface of an API suddenly changed).
-                    # We may want to remove the try/except and the following two lines eventually, but keeping them for the moment so other devels won't be affected
-                    #See this WMCore commit: https://github.com/dmwm/WMCore/commit/2afc01ae571390f5fa009dd258be757adac89c28#diff-374b7a6640288184175057234e393e1cL204
-                    size = infos['Size']
-                    checksums = infos['Checksums']
-                wmfile = File(lfn = lfn, events = infos['NumberOfEvents'], size = size, checksums = checksums, parents = infos['Parents'])
-                wmfile['block'] = infos['BlockName']
-                try:
-                    wmfile['locations'] = resourceCatalog.PNNstoPSNs(locations[wmfile['block']])
-                except Exception as ex:
-                    self.logger.error("Impossible translating %s to a CMS name through CMS Resource Catalog", locations[wmfile['block']] )
-                    self.logger.error("got this exception:\n %s", ex)
-                    raise
-                wmfile['workflow'] = requestname
-                event_counter += infos['NumberOfEvents']
-                for run, lumis in infos['Lumis'].iteritems():
-                    datasetLumis.setdefault(run, []).extend(lumis)
-                    wmfile.addRun(Run(run, *lumis))
-                    for lumi in lumis:
-                        uniquelumis.add((run, lumi))
-                    lumi_counter += len(lumis)
-                wmfiles.append(wmfile)
+            if task['tm_use_parent'] == 1 and len(infos['Parents']) == 0:
+                raise TaskWorkerException(
+                        "The CRAB3 server backend refuses to submit jobs to the Grid scheduler\n" +
+                        "because you specified useParents=True but some your files have no" +
+                        "parents.\nExample: " + lfn)
+            ## Create a WMCore File object.
+            try:
+                size = infos['FileSize']
+                checksums = {'Checksum': infos['Checksum'], 'Adler32': infos['Adler32'], 'Md5': infos['Md5']}
+            except:
+                #This is so that the task worker does not crash if an old version of WMCore is used (the interface of an API suddenly changed).
+                # We may want to remove the try/except and the following two lines eventually, but keeping them for the moment so other devels won't be affected
+                #See this WMCore commit: https://github.com/dmwm/WMCore/commit/2afc01ae571390f5fa009dd258be757adac89c28#diff-374b7a6640288184175057234e393e1cL204
+                size = infos['Size']
+                checksums = infos['Checksums']
+            wmfile = File(lfn = lfn, events = infos['NumberOfEvents'], size = size, checksums = checksums, parents = infos['Parents'])
+            wmfile['block'] = infos['BlockName']
+            try:
+                wmfile['locations'] = resourceCatalog.PNNstoPSNs(locations[wmfile['block']])
+            except Exception as ex:
+                self.logger.error("Impossible translating %s to a CMS name through CMS Resource Catalog" % locations[wmfile['block']] )
+                self.logger.error("got this exception:\n %s" % ex)
+                raise
+            wmfile['workflow'] = requestname
+            event_counter += infos['NumberOfEvents']
+            for run, lumis in infos['Lumis'].iteritems():
+                datasetLumis.setdefault(run, []).extend(lumis)
+                wmfile.addRun(Run(run, *lumis))
+                for lumi in lumis:
+                    uniquelumis.add((run, lumi))
+                lumi_counter += len(lumis)
+            wmfiles.append(wmfile)
 
-        if blocksWithNoLocations:
-            msg = "The locations of some blocks (%d) have not been found: %s" % (len(blocksWithNoLocations), list(blocksWithNoLocations))
-            self.logger.warning(msg)
-        self.uploadWarning(msg, task['user_proxy'], task['tm_taskname'])
-
+        self.logger.setLevel(previousLogLevel)
         uniquelumis = len(uniquelumis)
         self.logger.debug('Tot events found: %d' % event_counter)
         self.logger.debug('Tot lumis found: %d' % uniquelumis)
