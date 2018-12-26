d7e839f31b34a6fe0f0d10422bfc8ec07f671dca
Leonardo Cristella <leonardo.cristella@cern.ch>
2018-12-26 12:23:09 +0100

    Add TAPERECALL tasks to monitoring

--- scripts/Monitor/GenerateMONIT.py
+++ scripts/Monitor/GenerateMONIT.py
@@ -131,7 +131,7 @@ class CRAB3CreateJson(object):
         twStatus = self.getCountTasksByStatusAbs()
 
         states_filter = ['KILL', 'RESUBMIT', 'NEW', 'QUEUED', 'KILLFAILED', 'RESUBMITFAILED', 'SUBMITFAILED',
-                         'UPLOADED']
+                         'UPLOADED', 'TAPERECALL']
         if len(twStatus) > 0:
             for state in twStatus.keys():
                 if state in states_filter:
