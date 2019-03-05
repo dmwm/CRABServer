bd668f040b5e015e9689297bdefcd85fb5e0fef8
dciangot <dciangot@cern.ch>
2019-03-05 14:16:35 +0100

    Mitigate task process stuck by transfers.py #5833

--- scripts/task_process/task_proc_wrapper.sh
+++ scripts/task_process/task_proc_wrapper.sh
@@ -11,7 +11,7 @@ function cache_status {
 
 function manage_transfers {
     log "Running transfers.py"
-    python task_process/transfers.py 
+    python task_process/transfers.py &
 }
 
 function exit_now {
