7a8c969b3932878402b525893f63c212c9df8562
belforte <stefano.belforte@cern.ch>
2019-03-29 20:12:09 -0500

    proper call to killtransfersbyId. Fix #5853

--- src/python/TaskWorker/Actions/PostJob.py
+++ src/python/TaskWorker/Actions/PostJob.py
@@ -1136,23 +1136,31 @@ class ASOServerJob(object):
                 else:
                     transfersToKill.append(doc_id)
 
-        if not isCouchDBURL(self.aso_db_url):
-            # Now this means that we have a list of ids which needs to be killed
-            # First try to kill ALL in one API call
-            newDoc = {'listOfIds': transfersToKill,
-                      'publish': 0,
-                      'subresource': 'killTransfersById'}
-            try:
-                killedFiles = self.server.post(self.rest_uri_file_user_transfers, data=encodeRequest(newDoc, ['listOfIds', 'publish']))
-                not_cancelled = killedFiles[0]['result'][0]['failedKill']
-                cancelled = killedFiles[0]['result'][0]['killed']
-            except HTTPException as hte:
-                msg  = "Error setting KILL status in database."
-                msg += " Transfer KILL failed."
-                msg += "\n%s" % (str(hte.headers))
-                self.logger.warning(msg)
-                not_cancelled = transfersToKill
-                cancelled = []
+            if not isCouchDBURL(self.aso_db_url):
+                # Now this means that we have a list of ids which needs to be killed
+                # First try to kill ALL in one API call
+                newDoc = {'listOfIds': transfersToKill,
+                          'username': self.job_ad['CRAB_UserHN'],
+                          'subresource': 'killTransfersById'}
+                try:
+                    killedFiles = self.server.post(self.rest_uri_file_user_transfers, data=encodeRequest(newDoc, ['listOfIds']))
+                    not_cancelled = killedFiles[0]['result'][0]['failedKill']
+                    cancelled = killedFiles[0]['result'][0]['killed']
+                except HTTPException as hte:
+                    msg  = "Error setting KILL status in database."
+                    msg += " Transfer KILL failed."
+                    msg += "\n%s" % (str(hte.headers))
+                    self.logger.warning(msg)
+                    not_cancelled = transfersToKill
+                    cancelled = []
+                except Exception as ex:
+                    msg  = "Unknown error setting KILL status in database."
+                    msg += " Transfer KILL failed."
+                    msg += "\n%s" % (str(ex))
+                    self.logger.error(msg)
+                    not_cancelled = transfersToKill
+                    cancelled = []
+
             # Ok Now lets do a double check on doc_ids which failed to update one by one.
             # It is just to make proof concept to cover KILL to RDBMS
             self.logger.info("Failed to kill %s and succeeded to kill %s", not_cancelled, cancelled)
@@ -1161,12 +1169,12 @@ class ASOServerJob(object):
             # and also kill if status is not in KILL or KILLED
             for docIdKill in transfersToKill:
                 newDoc = {'listOfIds': [docIdKill],
-                          'publish': 0,
+                          'username': self.job_ad['CRAB_UserHN'],
                           'subresource': 'killTransfersById'}
                 try:
                     doc_out = self.getDocByID(docIdKill)
                     if doc_out['transfer_state'] not in ['kill', 'killed']:
-                        killedFiles = self.server.post(self.rest_uri_file_user_transfers, data=encodeRequest(newDoc, ['listOfIds', 'publish']))
+                        killedFiles = self.server.post(self.rest_uri_file_user_transfers, data=encodeRequest(newDoc, ['listOfIds']))
                         failedFiles = killedFiles[0]['result'][0]['failedKill']
                         notfailedFiles = killedFiles[0]['result'][0]['killed']
                         if failedFiles:
