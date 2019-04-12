37b80da5e7e8a896509c8d636e30cd42bc4fe982
todor-ivanov <todor.trendafilov.ivanov@cern.ch>
2019-04-13 00:44:36 +0200

    Link libcurl from cvmfs. Solve #5528 (#5873)
    * Link libcurl from cvmfs. Solve #5528

--- scripts/dag_bootstrap_startup.sh
+++ scripts/dag_bootstrap_startup.sh
@@ -11,11 +11,17 @@ for FILE in $1.dagman.out RunJobs.dag.dagman.out dbs_discovery.err dbs_discovery
     touch $FILE
 done
 
-export PATH="/opt/glidecondor/bin:/opt/glidecondor/sbin:/usr/local/bin:/bin:/usr/bin:/usr/bin:$PATH"
 export PATH="/data/srv/glidecondor/bin:/data/srv/glidecondor/sbin:/usr/local/bin:/bin:/usr/bin:/usr/bin:$PATH"
-export PYTHONPATH=/opt/glidecondor/lib/python:$PYTHONPATH
-export LD_LIBRARY_PATH=/opt/glidecondor/lib:/opt/glidecondor/lib/condor:.:$LD_LIBRARY_PATH
+export PYTHONPATH=/data/srv/glidecondor/lib/python:$PYTHONPATH
 export LD_LIBRARY_PATH=/data/srv/glidecondor/lib:/data/srv/glidecondor/lib/condor:.:$LD_LIBRARY_PATH
+
+os_ver=$(lsb_release -rs|cut -d. -f1)
+curl_path="/cvmfs/cms.cern.ch/slc${os_ver}_amd64_gcc700/external/curl/7.59.0"
+libcurl_path="${curl_path}/lib"
+source ${curl_path}/etc/profile.d/init.sh
+
+srcname=$0
+env > ${srcname%.sh}.env
 
 #Sourcing Remote Condor setup
 source_script=`grep '^RemoteCondorSetup =' $_CONDOR_JOB_AD | tr -d '"' | awk '{print $NF;}'`
