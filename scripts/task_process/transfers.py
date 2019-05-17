0d675822bd77e778b949ee2f79df5b32df692fe2
Leonardo Cristella <leonardo.cristella@cern.ch>
2019-05-17 02:13:01 +0200

    use pilot FTS

--- scripts/task_process/transfers.py
+++ scripts/task_process/transfers.py
@@ -298,7 +298,7 @@ class submit_thread(threading.Thread):
         fileDoc['subresource'] = 'updateTransfers'
         fileDoc['list_of_ids'] = [x[2] for x in self.files]
         fileDoc['list_of_transfer_state'] = ["SUBMITTED" for _ in self.files]
-        fileDoc['list_of_fts_instance'] = ['https://fts3.cern.ch:8446/' for _ in self.files]
+        fileDoc['list_of_fts_instance'] = ['https://fts3-pilot.cern.ch:8446/' for _ in self.files]
         fileDoc['list_of_fts_id'] = [jobid for _ in self.files]
 
         self.log.info("Marking submitted %s files" % (len(fileDoc['list_of_ids'])))
@@ -415,7 +415,7 @@ def perform_transfers(inputFile, lastLine, _lastFile, ftsContext, phedex):
             jobids = submit(phedex, ftsContext, transfers)
 
             for jobid in jobids:
-                logging.info("Monitor link: https://fts3.cern.ch:8449/fts3/ftsmon/#/job/"+jobid)
+                logging.info("Monitor link: https://fts3-pilot.cern.ch:8449/fts3/ftsmon/#/job/"+jobid)
 
             # TODO: send to dashboard
 
@@ -527,11 +527,11 @@ def algorithm():
     """
 
     # TODO: pass by configuration
-    fts = HTTPRequests('fts3.cern.ch:8446/',
+    fts = HTTPRequests('fts3-pilot.cern.ch:8446/',
                        proxy,
                        proxy)
 
-    ftsContext = fts3.Context('https://fts3.cern.ch:8446', proxy, proxy, verify=True)
+    ftsContext = fts3.Context('https://fts3-pilot.cern.ch:8446', proxy, proxy, verify=True)
     logging.debug("Delegating proxy to FTS: "+fts3.delegate(ftsContext, lifetime=timedelta(hours=48), force=False))
 
     try:
