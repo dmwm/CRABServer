fc6863f627be5d31d4fff260977fcfec8e82abac
belforte <stefano.belforte@cern.ch>
2020-01-28 16:43:17 -0600

    remove junk lines. Aling with preprod-02

--- src/script/Deployment/Publisher/start-Publisher.sh
+++ src/script/Deployment/Publisher/start-Publisher.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# if PUBLISHER_HOME is already defined, use it
-if [ -v PUBLISHER_HOME ]
-then
-  echo "PUBLISHER_HOME already set to $PUBLISHER_HOME. Will use that"
-else
-  export PUBLISHER_HOME=/data/srv/Publisher  # where we run the Publisher and where Config is
-  echo "Define environment for Publisher in $PUBLISHER_HOME"
-fi
-
-# we only run from 'current' which points to current taskworker installation
-
-source ${PUBLISHER_HOME}/current/*/cms/crabtaskworker/*/etc/profile.d/init.sh
-
-export PUBLISHER_ROOT=${CRABTASKWORKER_ROOT}
-export PUBLISHER_VERSION=${CRABTASKWORKER_VERSION} 
-
-export CONDOR_CONFIG=/data/srv/condor_config
-belforte@crab-preprod-tw02/Publisher> cat start.sh 
-#!/bin/bash
-
 unset X509_USER_PROXY
 unset X509_USER_CERT
 unset X509_USER_KEY
