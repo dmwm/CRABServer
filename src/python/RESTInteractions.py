bea23b3bfe91f0d01c8d7069bdf44559b0b39b0d
Stefano Belforte <stefano.belforte@cern.ch>
2020-02-19 23:26:42 -0600

    log URL when HTTP call returns bad JSON (#6021)
    * log URL when HTTP call returns bad JSON
    
    * log also full url and subresource

--- src/python/RESTInteractions.py
+++ src/python/RESTInteractions.py
@@ -130,7 +130,7 @@ class HTTPRequests(dict):
             except Exception as ex:
                 #add here other temporary errors we need to retry
                 if (not retriableError(ex)) or (i == self['retry']):
-                    msg = "Fatal error trying to connect to %s" % url
+                    msg = "Fatal error trying to connect to %s using %s" % (url, data)
                     self.logger.error(msg)
                     raise #really exit and raise exception if this was the last retry or the exit code is not among the list of the one we retry
                 sleeptime = 20 * (i + 1)
@@ -143,7 +143,12 @@ class HTTPRequests(dict):
                 time.sleep(sleeptime) #sleeps 20s the first time, 40s the second time and so on
             else:
                 break
-        result = JSONRequests(idict={"pycurl" : True}).decode(datares)
+        try:
+            result = JSONRequests(idict={"pycurl" : True}).decode(datares)
+        except Exception as ex:
+            msg = "Fatal error reading data from %s using %s" % (url, data)
+            self.logger.error(msg)
+            raise #really exit and raise exception
         return result, response.status, response.reason
 
     @staticmethod
