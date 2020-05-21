cba494b1bdd8ba3aefabfca0704eea27acfb80cb
belforte <stefano.belforte@cern.ch>
2020-05-21 11:41:03 -0500

    initial version of TaskWorkerConfig.py template

--- src/script/Deployment/TaskWorker/TaskWorkerConfig.py
+++ src/script/Deployment/TaskWorker/TaskWorkerConfig.py
@@ -13,53 +13,51 @@ config.Services.Rucio_account = 'crab_server'
 config.Services.Rucio_authUrl = 'https://cms-rucio-auth.cern.ch'
 config.Services.Rucio_caPath = '/etc/grid-security/certificates/'
 
-
 config.section_("TaskWorker")
+############################################################################
+## Following parameters NEED to be customized for every TW installation
+## values here default to a generic development instance, values
+## REMEMBER TO UPDATE THIS WHEN USING THIS FILE
+## currently used in production are marked with ##PROD
+############################################################################
+
+## This TaskWorker identity:
+config.TaskWorker.name = 'DevelopmentTaskWorker'
+##PROD config.TaskWorker.name = 'prod-tw01'
+
+## How much work can be done in parallel
+config.TaskWorker.nslaves = 1
+##PROD config.TaskWorker.nslaves = 6
+
+## recurring actions: only the simplest one for minimal testing
+## other make no sense (or do not work) other than in Production
+## anyhow Recurring Actions can be tested as standalone scripts
+config.TaskWorker.recurringActions = ['RemovetmpDir']
+##PROD config.TaskWorker.recurringActions = ['RenewRemoteProxies', 'RemovetmpDir',
+##PROD                                       'BanDestinationSites', 'TapeRecallStatus']
+
+## Which CRAB SERVICE instance is this TW part of
+## Possible values for instance are:
+#   - preprod   : uses cmsweb-testbed RESt with preprod DB
+#   - prod      : uses cmsweb,cern.ch production REST with production DB
+#   - K8s       : uses cmsweb-k8s-testbed REST with preprod DB
+#   - other     : allows to use any restHost with andy dbInstance, those needs to be
+#                  specified in the two following parameters
+config.TaskWorker.instance = 'other'
+config.TaskWorker.restHost = 'cmsweb-test.cern.ch'
+config.TaskWorker.dbInstance = 'dev'
+
+
+############################################################################
+## Following parameters are good defaults for any TW and should not be changed
+## lightly nor without a godd reason
+############################################################################
+
 config.TaskWorker.polling = 30 #seconds
-#config.TaskWorker.polling = 60 #MM - increased to 60 after TW has been off one day
-# we can add one worker per core, plus some spare ones since most of actions wait for I/O
-
-<% if !@tw_nslaves.nil? && !@tw_nslaves.to_s.empty? -%>
-config.TaskWorker.nslaves = <%= @tw_nslaves.to_s %>
-<% end %>
-
-
-<% if !@tw_name.nil? && !@tw_name.to_s.empty? -%>
-config.TaskWorker.name = '<%= @tw_name.to_s %>' #Remember to update this!
-<% end %>
-
-<% if !@tw_recurring_actions.nil? && !@tw_recurring_actions.to_s.empty? -%>
-config.TaskWorker.recurringActions = <%= @tw_recurring_actions.to_s %>
-# config.TaskWorker.recurringActions = ['RenewRemoteProxies', 'RemovetmpDir', 'BanDestinationSites']
-<% end %>
-
 
 config.TaskWorker.scratchDir = '/data/srv/tmp' #make sure this directory exists
 config.TaskWorker.logsDir = './logs'
 
-## SB START - simple config for testing
-#config.TaskWorker.nslaves = 1
-#config.TaskWorker.recurringActions = []
-## SB END
-
-# Setting the list of users for the highprio accounting group.
-# config.TaskWorker.highPrioUsers = ['sethzenz']
-
-
-## Possible values for mode are:
-#   - cmsweb-dev       : not really used
-#   - cmsweb-preprod   : uses cmsweb-testbed RESt with preprod DB
-#   - cmsweb-prod      : uses cmsweb,cern.ch production REST with production DB 
-#   - test             : allows to use any test REST with preprod DB
-#   - private          : allows to use any REST with a private DB instance
-<% if !@tw_mode.nil? && !@tw_mode.to_s.empty? -%>
-config.TaskWorker.mode = '<%= @tw_mode.to_s %>'
-config.TaskWorker.restURInoAPI = '/crabserver/<%= @tw_mode.to_s.sub("cmsweb-","").sub("private","dev") %>/'
-<% end %>
-## If 'private' or 'test' mode then a REST host name is needded here. It is called url by mistake
-<% if !@tw_resturl.nil? && !@tw_resturl.to_s.empty? -%>
-config.TaskWorker.resturl = '<%= @tw_resturl.to_s %>'
-<% end %>
 ## the parameters here below are used to contact cmsweb services for the REST-DB interactions
 config.TaskWorker.cmscert = '/data/certs/servicecert.pem'
 config.TaskWorker.cmskey = '/data/certs/servicekey.pem'
@@ -86,8 +84,6 @@ config.TaskWorker.ASOTimeout = 86400
 config.TaskWorker.stageoutPolicy = ["local", "remote"]
 config.TaskWorker.dashboardTaskType = 'analysis'
 
-# 0 - number of post jobs = max( (# jobs)*.1, 20)
-# -1 - no limit
 # This is needed for Site Metrics
 # It should not block any site for Site Metrics and if needed for other activities
 config.TaskWorker.ActivitiesToRunEverywhere = ['hctest', 'hcdev']
@@ -95,35 +91,29 @@ config.TaskWorker.ActivitiesToRunEverywhere = ['hctest', 'hcdev']
 config.TaskWorker.maxIdle = 1000
 config.TaskWorker.maxPost = 20
 
-# new schedd chooser
-#config.TaskWorker.scheddPickerFunction = HTCondorLocator.memoryBasedChoices
-#config.TaskWorker.scheddPickerFunction = tunedScheddSubmission
-#config.TaskWorker.scheddPickerFunction = totalRandom
-#config.TaskWorker.scheddPickerFunction = newScheddPicker
 config.TaskWorker.scheddPickerFunction = HTCondorLocator.capacityMetricsChoicesHybrid
 
 config.TaskWorker.DDMServer = 'dynamo.mit.edu'
 
 config.section_("Sites")
-#config.Sites.DashboardURL = "https://cmst1.web.cern.ch/CMST1/SST/analysis/usableSites.json"
 config.Sites.DashboardURL = "https://cmssst.web.cern.ch/cmssst/analysis/usableSites.json"
-
-# config.Sites.available = []
-
 
 config.section_("MyProxy")
 config.MyProxy.serverhostcert = '/data/certs/hostcert.pem'
 config.MyProxy.serverhostkey = '/data/certs/hostkey.pem'
-#config.MyProxy.uisource = '/afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh'
 config.MyProxy.cleanEnvironment = True
 config.MyProxy.credpath = '/data/certs/creds' #make sure this directory exists
-<% if !@tw_serverdn.nil? && !@tw_serverdn.to_s.empty? -%>
-config.MyProxy.serverdn = '<%= @tw_serverdn.to_s %>'
-<% end %>
 
 # Setting the minimum runtime requirements in minutes for automatic splitting
 config.TaskWorker.minAutomaticRuntimeMins = 60
-config.TaskWorker.highPrioEgroups = ['cms-crab-HighPrioUsers']
 
+config.TaskWorker.highPrioEgroups = ['cms-crab-HighPrioUsers']
 config.TaskWorker.bannedUsernames = ['mickeymouse','donaldduck']
+
+config.MyProxy.serverdn = 'dummy'  # this is a WMcore/Credential config param which is not actually used in CRAB
+##PROD config.MyProxy.serverdn ='/../tw/DN=crab-prod-tw01/'
+
+# Setting the list of users for the highprio accounting group
+# not usually needed since the list if automatically populated from e-group
+# config.TaskWorker.highPrioUsers = ['sethzenz']
 
