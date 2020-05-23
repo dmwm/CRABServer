868f695b0c7234206f769538d942f9b4489da345
belforte <stefano.belforte@cern.ch>
2020-05-23 18:59:21 -0500

    add predefined SERVICE_INSTANCES for stefanovm[2]

--- src/python/ServerUtilities.py
+++ src/python/ServerUtilities.py
@@ -38,6 +38,8 @@ SERVICE_INSTANCES = {'prod': {'restHost':'cmsweb.cern.ch', 'dbInstance':'prod'},
                      'test4': {'restHost':'cmsweb-test4.cern.ch', 'dbInstance':'dev'},
                      'test5': {'restHost':'cmsweb-test5.cern.ch', 'dbInstance':'dev'},
                      'test6': {'restHost':'cmsweb-test6.cern.ch', 'dbInstance':'dev'},
+                     'stefanovm': {'resthost':'stefanovm.cern.ch', 'dbInstace':'dev'},
+                     'stefanovm2': {'resthost':'stefanovm2.cern.ch', 'dbInstace':'dev'},
                      'other': {'restHost':None, 'dbInstance':None},
                      }
 
