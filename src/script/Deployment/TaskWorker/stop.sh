9c545f37b0be5ca365fb3a0ca233d6d04c424c9d
Stefano Belforte <stefano.belforte@cern.ch>
2020-06-10 16:45:11 -0500

    avoid traceback

--- src/script/Deployment/TaskWorker/stop.sh
+++ src/script/Deployment/TaskWorker/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # gracefully stop TaskWorker by issueing SIGTERM to the Master
-TaskMasterPid=`ps exf|grep MasterWorker|grep -v grep|head -1|awk '{print $1}'`
+TaskMasterPid=`ps exfww|grep MasterWorker|grep -v grep|head -1|awk '{print $1}'`
 kill $TaskMasterPid
