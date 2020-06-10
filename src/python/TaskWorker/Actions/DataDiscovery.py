4cfe2b74c0ab9a637f1de32ed0206bcee69f9ab5
Stefano Belforte <stefano.belforte@cern.ch>
2020-06-10 15:27:24 -0500

    imporove message for blocks not on disk

--- src/python/TaskWorker/Actions/DataDiscovery.py
+++ src/python/TaskWorker/Actions/DataDiscovery.py
@@ -86,7 +86,7 @@ class DataDiscovery(TaskAction):
                 wmfiles.append(wmfile)
 
         if blocksWithNoLocations:
-            msg = "%d blocks will be skipped because are not located on DISK: %s" % (len(blocksWithNoLocations), list(blocksWithNoLocations))
+            msg = "%d blocks will be skipped because are not completely replicated on DISK: %s" % (len(blocksWithNoLocations), list(blocksWithNoLocations))
             self.logger.warning(msg)
             self.uploadWarning(msg, task['user_proxy'], task['tm_taskname'])
 
