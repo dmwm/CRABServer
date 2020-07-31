06a6a8fe1f32be55f48a95be92f2e66ba8c607e5
Stefano Belforte <stefano.belforte@cern.ch>
2020-07-31 08:38:11 -0500

    make sure user_groups is defined. Fix #6106 (#6107)
    * make sure user_groups is defined. Fix #6106
    
    * fix pylint complains
    
    * fix more pylint complains

--- src/python/TaskWorker/Actions/MyProxyLogon.py
+++ src/python/TaskWorker/Actions/MyProxyLogon.py
@@ -1,9 +1,9 @@
 
+import logging
 from TaskWorker.DataObjects.Result import Result
 from TaskWorker.Actions.TaskAction import TaskAction
 from TaskWorker.WorkerExceptions import TaskWorkerException
 from ServerUtilities import tempSetLogLevel
-import logging
 
 # We won't do anything if the proxy is shorted then 1 hour
 # NB: in the PoC we had 24 hours, but does that make sense
@@ -18,7 +18,7 @@ class MyProxyLogon(TaskAction):
         TaskAction.__init__(self, config, server, resturi, procnum)
         self.myproxylen = myproxylen
 
-    def tryProxyLogon (self, proxycfg=None):
+    def tryProxyLogon(self, proxycfg=None):
         """
         Utility function to allow trying with diffenent myproxy configurations.
         It tries to retrieve a valid proxy from myproxy using the configuration
@@ -70,13 +70,16 @@ class MyProxyLogon(TaskAction):
                    }
         try:
             # try first to retrieve credential with login name = <username>_CRAB
-            (userproxy,usergroups) = self.tryProxyLogon(proxycfg=proxycfg)
-        except TaskWorkerException as twe:
+            (userproxy, usergroups) = self.tryProxyLogon(proxycfg=proxycfg)
+        except TaskWorkerException:
             self.logger.error("proxy retrieval from %s failed with login name %s.",
                               proxycfg['myProxySvr'], proxycfg['userName'])
             self.logger.error("will try with old-style DN hash")
             del proxycfg['userName']
             (userproxy, usergroups) = self.tryProxyLogon(proxycfg=proxycfg)
+        #  minimal sanity check. Submission will fail if there's no group
+        if not usergroups:
+            raise TaskWorkerException('Could not retrieve VOMS groups list from %s' % userproxy)
         kwargs['task']['user_proxy'] = userproxy
         kwargs['task']['user_groups'] = usergroups
         self.logger.debug("Valid proxy for %s now in %s", proxycfg['userDN'], userproxy)
