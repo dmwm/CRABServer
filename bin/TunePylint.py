38727fd32738aadd6c8f6b2290bf7f90d9808f87
Daina <60326190+ddaina@users.noreply.github.com>
2020-08-04 14:27:40 +0200

    Change variable name

--- bin/TunePylint.py
+++ bin/TunePylint.py
@@ -9,8 +9,8 @@ import configparser
 directories = ['/home/dmwm/crabclient_test/', '/home/dmwm/crabserver_test/', '/home/dmwm/aso_test/']
 
 config = configparser.ConfigParser()
-for dir in directories:
-    oldCfg = config.read(dir + '.default_pylintrc')
+for directory in directories:
+    oldCfg = config.read(directory + '.default_pylintrc')
     config['MESSAGES CONTROL']['disable'] += ', C0103, W0703, R0912, R0914, R0915'
-    with open(dir + '.pylintrc', 'w') as newCfg:
+    with open(directory + '.pylintrc', 'w') as newCfg:
         config.write(newCfg)
