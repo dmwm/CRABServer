53eacb0fd5fd80c8f19e510daa604b4a742159c2
Daina <60326190+ddaina@users.noreply.github.com>
2020-08-04 14:12:30 +0200

    Refactor code: pylint, python3, change variable name

--- bin/TunePylint.py
+++ bin/TunePylint.py
@@ -1,11 +1,16 @@
+"""
+Script creates new .pylintrc based on a default .pylintrc defined in WMCore/standards
+by appending messages which should be disabled for CRABClient/CRABServer testing.
+"""
+
+from __future__ import division
 import configparser
 
-directories = ['/home/dmwm/crabclient_test/', '/home/dmwm/crabclient_test/', '/home/dmwm/aso_test/']
+directories = ['/home/dmwm/crabclient_test/', '/home/dmwm/crabserver_test/', '/home/dmwm/aso_test/']
 
 config = configparser.ConfigParser()
-for i in directories:
-    oldCfg = config.read(i + '.default_pylintrc')
+for dir in directories:
+    oldCfg = config.read(dir + '.default_pylintrc')
     config['MESSAGES CONTROL']['disable'] += ', C0103, W0703, R0912, R0914, R0915'
-    with open(i + '.pylintrc', 'w') as newCfg:
+    with open(dir + '.pylintrc', 'w') as newCfg:
         config.write(newCfg)
-
