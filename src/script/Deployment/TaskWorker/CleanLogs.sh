86b6127fe1580632115cf0f92b23133b0a6c00f8
Stefano Belforte <stefano.belforte@cern.ch>
2020-08-16 04:06:13 -0500

    Update CleanLogs.sh since subdirectories are now re-created as needed

--- src/script/Deployment/TaskWorker/CleanLogs.sh
+++ src/script/Deployment/TaskWorker/CleanLogs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # remove ALL old logs (mostly useful on test machines)
-rm -rf /data/srv/TaskManager/logs/tasks/*
-rm -rf /data/srv/TaskManager/logs/processes/*
-rm -f /data/srv/TaskManager/logs/*txt*
-rm -f /data/srv/TaskManager/logs/recurring.log
+rm -rf /data/srv/TaskManager/logs/*
 rm /data/srv/TaskManager/nohup.out
