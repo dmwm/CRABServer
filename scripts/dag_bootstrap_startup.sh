e454dd43e786f448da824ba7a5db5fe8cb46d499
belforte <stefano.belforte@cern.ch>
2020-09-25 23:32:32 +0200

    allow to force use of NewPublisher via extraJDL

--- scripts/dag_bootstrap_startup.sh
+++ scripts/dag_bootstrap_startup.sh
@@ -167,6 +167,18 @@ then
     touch USE_NEW_PUBLISHER
 fi
 
+if [ "X$USE_NEWPUBLISHER" != "X" ] ;
+then
+    echo "\$USE_NEW_PUBLISHER defined. Set this task to use New Publisher"
+    touch USE_NEW_PUBLISHER
+fi
+
+UseNewPublisher=`grep '^CRAB_USE_NEW_PUBLISHER =' $_CONDOR_JOB_AD | tr -d '"' | awk '{print $NF;}'`
+if [ "$UseNewPublisher" = "True" ];
+then
+    echo "+CRAB_USE_NEW_PUBLISHER classAd is True. Set this task to use New Publisher"
+    touch USE_NEW_PUBLISHER
+fi
 
 export _CONDOR_DAGMAN_LOG=$PWD/$1.dagman.out
 export _CONDOR_DAGMAN_GENERATE_SUBDAG_SUBMITS=False
