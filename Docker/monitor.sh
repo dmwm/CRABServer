7602d8adcb9ab1d08aa4df73e8e10b74e6b698d5
ddaina <daina.dirmaite@cern.ch>
2020-11-18 19:50:16 +0100

    Update location where filebeat.yaml is stored (fix #6260)

--- Docker/monitor.sh
+++ Docker/monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if  [ -f /data/srv/cfg/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
+if  [ -f /data/hostdisk/${SERVICE}/cfg/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
     ldir=/tmp/filebeat
     mkdir -p $ldir/data
     nohup /usr/bin/filebeat \
-        -c /data/srv/cfg/filebeat.yaml \
+        -c /data/hostdisk/${SERVICE}/cfg/filebeat.yaml \
         --path.data $ldir/data --path.logs $ldir -e 2>&1 1>& $ldir/log < /dev/null &
 fi
 
