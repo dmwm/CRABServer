9d49748fd8bd4860eb20dfc3245a1d7375a287d3
ddaina <daina.dirmaite@cern.ch>
2020-11-19 15:12:13 +0100

    move filebeat to parent directory for all services use

--- Docker/monitor.sh
+++ Docker/monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if  [ -f /data/hostdisk/${SERVICE}/cfg/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
+if  [ -f /data/hostdisk/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
     ldir=/tmp/filebeat
     mkdir -p $ldir/data
     nohup /usr/bin/filebeat \
-        -c /data/hostdisk/${SERVICE}/cfg/filebeat.yaml \
+        -c /data/hostdisk/filebeat.yaml \
         --path.data $ldir/data --path.logs $ldir -e 2>&1 1>& $ldir/log < /dev/null &
 fi
 
