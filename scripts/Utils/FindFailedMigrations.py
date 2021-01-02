8505677ffd0e12e07e1b02733592a1ed073785c9
Stefano Belforte <stefano.belforte@cern.ch>
2021-01-02 13:12:20 +0100

    fix typos in FindFailedMigrations and please pylint (#6315)
    * add FindFailedMigrations.py script
    
    * add RemoveFailedMigration.py to scripts/Utils
    
    * fix typos in FindFailedMigrations and please pylint

--- scripts/Utils/FindFailedMigrations.py
+++ scripts/Utils/FindFailedMigrations.py
@@ -2,13 +2,10 @@
 # coding: utf-8
 
 import os
-import sys
-import pprint
-import time
 from  datetime import datetime
 import argparse
 
-import CRABClient
+import CRABClient # this is needed to make it possible for the following import to work
 from dbs.apis.dbsClient import DbsApi
 
 
@@ -22,38 +19,38 @@ def readAndParse(csvFile, apiMig):
     returns: a list of dictionaries with keys
      migrId, status, created, block
     """
-# 
-    failedMigrations=[]
+
+    failedMigrations = []
     ids = set()
-    with open(csvFile) as _list:
-      lines = _list.readlines()
+    with open(csvFile) as fp:
+        lines = fp.readlines()
 
     for line in lines:
         items = line.strip().split(',')
         ids.add(int(items[0]))
     uniqueIDs = list(ids)
     print("Found %d unique migration IDs logged as terminally failed" % len(uniqueIDs))
-    for id in uniqueIDs:
-        print(" %d" % id)
+    for migId in uniqueIDs:
+        print(" %d" % migId)
     print("Check current status")
-    for id in uniqueIDs:
-        status = apiMig.statusMigration(migration_rqst_id=id)
+    for migId in uniqueIDs:
+        status = apiMig.statusMigration(migration_rqst_id=migId)
         if not status:
-            print(" %d has been removed" % id)
+            print(" %d has been removed" % migId)
             continue
         state = status[0].get("migration_status")
         # values for state:
         # 0-request created; 1-in process; 2-succeeded;
         # 3-failed, but has three chances to try; 9-Permanently failed
-        if not state==9:
-            print("%id is in state %d, not 9" % (id, state))
+        if not state == 9:
+            print("%id is in state %d, not 9" % (migId, state))
             continue
         tFromEpoch = status[0].get("creation_date")
         created = datetime.fromtimestamp(tFromEpoch).strftime('%Y-%m-%d %H:%M:%S')
         block = status[0].get("migration_input") # CRAB migrations are always one block at a time
-        migDict = {'id':id, 'status':state, 'created':created, 'block':block}
+        migDict = {'id':migId, 'status':state, 'created':created, 'block':block}
         failedMigrations.append(migDict)
-        
+
     return failedMigrations
 
 
@@ -63,16 +60,17 @@ def main():
                         default='TerminallyFailedLog.txt')
     args = parser.parse_args()
     logFile = os.path.abspath(args.file)
-    
-    migUrl='https://cmsweb.cern.ch/dbs/prod/phys03/DBSMigrate'
+
+    migUrl = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSMigrate'
     apiMig = DbsApi(url=migUrl)
-                   
+
     failedMigrations = readAndParse(logFile, apiMig)
-    
+
     print("Found %d terminally failed migrations" % len(failedMigrations))
-    for id in failedMigrations:
-        print(" %d" % id) 
-        
+    if failedMigrations:
+        print("   ID\t\tcreated\t\t\tblock")
+        for migDict in failedMigrations:
+            print(" %d\t%s\t%s" % (migDict['id'], migDict['created'], migDict['block']))
+
 if __name__ == '__main__':
     main()
-
