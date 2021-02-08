c98da4633c52f2cc0f33d3d363b45161ad3cfa4a
belforte <stefano.belforte@cern.ch>
2021-02-08 16:28:39 +0100

    user validate_ustr instead of validate_str

--- src/python/CRABInterface/RESTFileMetadata.py
+++ src/python/CRABInterface/RESTFileMetadata.py
@@ -1,7 +1,7 @@
 # WMCore dependecies here
 from WMCore.REST.Error import InvalidParameter
 from WMCore.REST.Server import RESTEntity, restcall
-from WMCore.REST.Validation import validate_str, validate_strlist, validate_num, validate_numlist
+from WMCore.REST.Validation import validate_ustr, validate_ustrlist, validate_num, validate_numlist
 
 # CRABServer dependecies here
 from CRABInterface.RESTExtensions import authz_login_valid, authz_operator
@@ -26,14 +26,14 @@ class RESTFileMetadata(RESTEntity):
         if method in ['PUT']:
             #TODO check optional parameter
             #TODO check all the regexp
-            validate_str("taskname", param, safe, RX_TASKNAME, optional=False)
-            validate_strlist("outfilelumis", param, safe, RX_LUMILIST)
+            validate_ustr("taskname", param, safe, RX_TASKNAME, optional=False)
+            validate_ustrlist("outfilelumis", param, safe, RX_LUMILIST)
             validate_numlist("outfileruns", param, safe)
             if len(safe.kwargs["outfileruns"]) != len(safe.kwargs["outfilelumis"]):
                 raise InvalidParameter("The number of runs and the number of lumis lists are different")
-            validate_strlist("inparentlfns", param, safe, RX_PARENTLFN)
-            validate_str("globalTag", param, safe, RX_GLOBALTAG, optional=True)
-            validate_str("jobid", param, safe, RX_JOBID, optional=True)
+            validate_ustrlist("inparentlfns", param, safe, RX_PARENTLFN)
+            validate_ustr("globalTag", param, safe, RX_GLOBALTAG, optional=True)
+            validate_ustr("jobid", param, safe, RX_JOBID, optional=True)
             #TODO: for backward compatibility. Get rid of the pandajobid once all jobs using it are done (one month after this gets released)
             #(The following five lines can be deleted)
             validate_num("pandajobid", param, safe, optional=True)
@@ -43,35 +43,35 @@ class RESTFileMetadata(RESTEntity):
             if safe.kwargs["pandajobid"] == None:
                 safe.kwargs["pandajobid"] =  0
             validate_num("outsize", param, safe, optional=False)
-            validate_str("publishdataname", param, safe, RX_PUBLISH, optional=False)
-            validate_str("appver", param, safe, RX_CMSSW, optional=False)
-            validate_str("outtype", param, safe, RX_OUTTYPES, optional=False)
-            validate_str("checksummd5", param, safe, RX_CHECKSUM, optional=False)
-            validate_str("checksumcksum", param, safe, RX_CHECKSUM, optional=False)
-            validate_str("checksumadler32", param, safe, RX_CHECKSUM, optional=False)
-            validate_str("outlocation", param, safe, RX_CMSSITE, optional=False)
-            validate_str("outtmplocation", param, safe, RX_CMSSITE, optional=False)
-            validate_str("acquisitionera", param, safe, RX_TASKNAME, optional=False)#TODO Do we really need this?
-            validate_str("outdatasetname", param, safe, RX_OUTDSLFN, optional=False)#TODO temporary, need to come up with a regex
-            validate_str("outlfn", param, safe, RX_PARENTLFN, optional=False)
-            validate_str("outtmplfn", param, safe, RX_PARENTLFN, optional=True)
+            validate_ustr("publishdataname", param, safe, RX_PUBLISH, optional=False)
+            validate_ustr("appver", param, safe, RX_CMSSW, optional=False)
+            validate_ustr("outtype", param, safe, RX_OUTTYPES, optional=False)
+            validate_ustr("checksummd5", param, safe, RX_CHECKSUM, optional=False)
+            validate_ustr("checksumcksum", param, safe, RX_CHECKSUM, optional=False)
+            validate_ustr("checksumadler32", param, safe, RX_CHECKSUM, optional=False)
+            validate_ustr("outlocation", param, safe, RX_CMSSITE, optional=False)
+            validate_ustr("outtmplocation", param, safe, RX_CMSSITE, optional=False)
+            validate_ustr("acquisitionera", param, safe, RX_TASKNAME, optional=False)#TODO Do we really need this?
+            validate_ustr("outdatasetname", param, safe, RX_OUTDSLFN, optional=False)#TODO temporary, need to come up with a regex
+            validate_ustr("outlfn", param, safe, RX_PARENTLFN, optional=False)
+            validate_ustr("outtmplfn", param, safe, RX_PARENTLFN, optional=True)
             validate_num("events", param, safe, optional=False)
-            validate_str("filestate", param, safe, RX_FILESTATE, optional=True)
+            validate_ustr("filestate", param, safe, RX_FILESTATE, optional=True)
             validate_num("directstageout", param, safe, optional=True)
             safe.kwargs["directstageout"] = 'T' if safe.kwargs["directstageout"] else 'F' #'F' if not provided
         elif method in ['POST']:
-            validate_str("taskname", param, safe, RX_TASKNAME, optional=False)
-            validate_str("outlfn", param, safe, RX_LFN, optional=False)
-            validate_str("filestate", param, safe, RX_FILESTATE, optional=False)
+            validate_ustr("taskname", param, safe, RX_TASKNAME, optional=False)
+            validate_ustr("outlfn", param, safe, RX_LFN, optional=False)
+            validate_ustr("filestate", param, safe, RX_FILESTATE, optional=False)
         elif method in ['GET']:
-            validate_str("taskname", param, safe, RX_TASKNAME, optional=False)
-            validate_str("filetype", param, safe, RX_OUTTYPES, optional=False)
+            validate_ustr("taskname", param, safe, RX_TASKNAME, optional=False)
+            validate_ustr("filetype", param, safe, RX_OUTTYPES, optional=False)
             validate_num("howmany", param, safe, optional=True)
-            validate_strlist("lfn", param, safe, RX_LFN)
+            validate_ustrlist("lfn", param, safe, RX_LFN)
         elif method in ['DELETE']:
             authz_operator()
-            validate_str("taskname", param, safe, RX_TASKNAME, optional=True)
-            validate_str("hours", param, safe, RX_HOURS, optional=True)
+            validate_ustr("taskname", param, safe, RX_TASKNAME, optional=True)
+            validate_ustr("hours", param, safe, RX_HOURS, optional=True)
             if bool(safe.kwargs["taskname"]) == bool(safe.kwargs["hours"]):
                raise InvalidParameter("You have to specify a taskname or a number of hours. Files of this task or created before the number of hours"+\
                                         " will be deleted. Only one of the two parameters can be specified.")
--- src/python/CRABInterface/RESTTask.py
+++ src/python/CRABInterface/RESTTask.py
@@ -1,6 +1,6 @@
 # WMCore dependecies here
 from WMCore.REST.Server import RESTEntity, restcall
-from WMCore.REST.Validation import validate_str, validate_strlist, validate_num
+from WMCore.REST.Validation import validate_ustr, validate_ustrlist, validate_num
 from WMCore.REST.Error import InvalidParameter, ExecutionError
 
 from CRABInterface.Utils import conn_handler
@@ -33,20 +33,20 @@ class RESTTask(RESTEntity):
         """Validating all the input parameter as enforced by the WMCore.REST module"""
         authz_login_valid()
         if method in ['POST']:
-            validate_str('subresource', param, safe, RX_SUBRES_TASK, optional=False)
-            validate_str("workflow", param, safe, RX_TASKNAME, optional=True)
-            validate_str("warning", param, safe, RX_TEXT_FAIL, optional=True)
-            validate_str("webdirurl", param, safe, RX_URL, optional=True)
-            validate_str("scheddname", param, safe, RX_SCHEDD_NAME, optional=True)
-            validate_strlist("outputdatasets", param, safe, RX_OUT_DATASET)
-            validate_str("taskstatus", param, safe, RX_STATUS, optional=True)
-            validate_str("ddmreqid", param, safe, RX_RUCIORULE, optional=True)
+            validate_ustr('subresource', param, safe, RX_SUBRES_TASK, optional=False)
+            validate_ustr("workflow", param, safe, RX_TASKNAME, optional=True)
+            validate_ustr("warning", param, safe, RX_TEXT_FAIL, optional=True)
+            validate_ustr("webdirurl", param, safe, RX_URL, optional=True)
+            validate_ustr("scheddname", param, safe, RX_SCHEDD_NAME, optional=True)
+            validate_ustrlist("outputdatasets", param, safe, RX_OUT_DATASET)
+            validate_ustr("taskstatus", param, safe, RX_STATUS, optional=True)
+            validate_ustr("ddmreqid", param, safe, RX_RUCIORULE, optional=True)
         elif method in ['GET']:
-            validate_str('subresource', param, safe, RX_SUBRES_TASK, optional=False)
-            validate_str("workflow", param, safe, RX_TASKNAME, optional=True)
-            validate_str('taskstatus', param, safe, RX_STATUS, optional=True)
-            validate_str('username', param, safe, RX_USERNAME, optional=True)
-            validate_str('minutes', param, safe, RX_RUNS, optional=True)
+            validate_ustr('subresource', param, safe, RX_SUBRES_TASK, optional=False)
+            validate_ustr("workflow", param, safe, RX_TASKNAME, optional=True)
+            validate_ustr('taskstatus', param, safe, RX_STATUS, optional=True)
+            validate_ustr('username', param, safe, RX_USERNAME, optional=True)
+            validate_ustr('minutes', param, safe, RX_RUNS, optional=True)
 
     @restcall
     def get(self, subresource, **kwargs):
