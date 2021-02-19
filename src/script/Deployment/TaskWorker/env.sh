4077bc4676f3ae9308d4cd061c5188fef56c5ba0
belforte <stefano.belforte@cern.ch>
2021-02-19 14:18:11 +0100

    cleanup TW/env.sh make it more readable like Publisher/env.sh

--- src/script/Deployment/TaskWorker/env.sh
+++ src/script/Deployment/TaskWorker/env.sh
@@ -1,9 +1,19 @@
-scram_arch=slc7_amd64_gcc630
+# if TASKWORKER_HOME is already defined, use it
+if [ -v TASKWORKER_HOME ]
+then
+  echo "TASKWORKER_HOME already set to $TASKWORKER_HOME. Will use that"
+else
+  export TASKWORKER_HOME=/data/srv/TaskManager  # where we run the TASKWORKER and where Config is
+  echo "Define environment for TASKWORKER in $TASKWORKER_HOME"
+fi
 
+# is the following really needed ?
+#export scram_arch=slc7_amd64_gcc630
 
-export MYTESTAREA=/data/srv/TaskManager/current
-source $MYTESTAREA/${scram_arch}/cms/crabtaskworker/*/etc/profile.d/init.sh
+# we only run from 'current' which points to current taskworker installation
+source ${TASKWORKER_HOME}/current/*/cms/crabtaskworker/*/etc/profile.d/init.sh
 
 export CRABTASKWORKER_ROOT
 export CRABTASKWORKER_VERSION
 export CONDOR_CONFIG=/data/srv/condor_config
+export GHrepoDir=/data/hostdisk/repos
