962b3a87fd6577fecbe4d8edd3984dc60f7e5d5d
belforte <stefano.belforte@cern.ch>
2021-02-23 17:40:31 +0100

    unify run.sh across TW and Publ. as per https://github.com/dmwm/CRABServer/pull/6421#discussion_r580271424

--- src/script/Deployment/TaskWorker/run.sh
+++ src/script/Deployment/TaskWorker/run.sh
@@ -1,54 +1,57 @@
 #!/bin/bash
+# This script prepare the host environment for running one CRAB service based on CRAB
+# TaskWorker container image and runs it
+# Currently those services include TaskWorker and Publisher_*
+
+# This script is meant to be called in the Dockerfile CMD, and whoever issues the docker run command
+# must pass the SERVICE name as an environmental variable  via the '-e SERVICE=name' option
+# The script expects to be run in the /data/srv/$SERVICE directory and to in find there the start.sh
+# script appropriate for the $SERVICE being run
+
 # cleanup environment
 unset X509_USER_PROXY
 unset X509_USER_CERT
 unset X509_USER_KEY
 
-# if $SERVICE is defined, I am running in a container
-if [ -v SERVICE ]; then
-  # ensure container has needed mounts
-  check_link(){
-  # function checks if symbolic links required to start service exists and if they are not broken
+# ensure container has needed mounts
+check_link(){
+# function checks if symbolic links required to start service exists and if they are not broken
 
-    if [ -L $1 ] ; then
-      if [ -e $1 ] ; then
-         return 0
-      else
-         unlink $1
-         return 1
-      fi
+  if [ -L $1 ] ; then
+    if [ -e $1 ] ; then
+       return 0
     else
-      return 1
+       unlink $1
+       return 1
     fi
-  }
+  else
+    return 1
+  fi
+}
 
-  #directories/files that should be created before starting the container (SERVICE will be set to 'TaskWorker'):
+# directories/files that should be created before starting the container :
+# note a trick to do partial string match from https://unix.stackexchange.com/a/465906
+if [[ $SERVICE == TaskWorker ]]; then
   # -/data/hostdisk/${SERVICE}/cfg/TaskWorkerConfig.py
   # -/data/hostdisk/${SERVICE}/logs
   declare -A links=( ["current/TaskWorkerConfig.py"]="/data/hostdisk/${SERVICE}/cfg/TaskWorkerConfig.py" ["logs"]="/data/hostdisk/${SERVICE}/logs" ["nohup.out"]="/data/hostdisk/${SERVICE}/nohup.out")
-
-  for name in "${!links[@]}";
-  do
-    check_link "${name}" || ln -s "${links[$name]}" "$name"
-  done
+elif [[ $SERVICE == Publisher* ]]; then
+  # -/data/hostdisk/${SERVICE}/cfg/PublisherConfig.py
+  # -/data/hostdisk/${SERVICE}/logs
+  # -/data/hostdisk/${SERVICE}/PublisherFiles
+  declare -A links=( ["PublisherConfig.py"]="/data/hostdisk/${SERVICE}/cfg/PublisherConfig.py" ["logs"]="/data/hostdisk/${SERVICE}/logs" ["/data/srv/Publisher_files"]="/data/hostdisk/${SERVICE}/PublisherFiles" ["nohup.out"]="/data/hostdisk/${SERVICE}/nohup.out")
 fi
 
-# if TASKWORKER_HOME is already defined, use it
-if [ -v TASKWORKER_HOME ]
+for name in "${!links[@]}";
+do
+  check_link "${name}" || ln -s "${links[$name]}" "$name"
+done
+
+# if GH repositories location is not already defined, set a default
+if ! [ -v GHrepoDir ]
 then
-  echo "TASKWORKER_HOME already set to $TASKWORKER_HOME. Will use that"
-else
-  thisScript=`realpath $0`
-  myDir=`dirname ${thisScript}`
-  export TASKWORKER_HOME=${myDir}  # where we run the TASKWORKER and where Config is
-  echo "Define environment for TASKWORKER in $TASKWORKER_HOME"
+  GHrepoDir='/data/hostdisk/repos'
 fi
-
-source ${TASKWORKER_HOME}/env.sh
-# env.sh defines $CRABTASKWORKER_ROOT and $CRABTASKWORKER_VERSION
-# those horribly long names derive from names of spec files in CMSDIST, too much history and
-# dependencies to think of changing to remove the CRAB string or other simplifications
 
 # run current instance
-rm -f ${TASKWORKER_HOME}/nohup.out
-nohup python $CRABTASKWORKER_ROOT/lib/python2.7/site-packages/TaskWorker/MasterWorker.py --config $TASKWORKER_HOME/TaskWorkerConfig.py --logDebug &
+sh start.sh -c
