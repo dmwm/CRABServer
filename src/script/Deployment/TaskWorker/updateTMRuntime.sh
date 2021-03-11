794088bd3d2a6663febaae7fcdf944adf44def48
belforte <stefano.belforte@cern.ch>
2021-03-11 11:04:31 +0100

    make updateTMruntime more readable

--- src/script/Deployment/TaskWorker/updateTMRuntime.sh
+++ src/script/Deployment/TaskWorker/updateTMRuntime.sh
@@ -18,7 +18,7 @@ CRABTASKWORKER_ROOT=${TW_CURRENT}/${TW_ARCH}/cms/crabtaskworker/${TW_RELEASE}
 # if GH repositories location is not already defined, set a default
 if ! [ -v GHrepoDir ]
 then
-  GHrepoDir='/data/hostdisk/repos'
+  GHrepoDir='/data/repos'
 fi
 
 logFile=/tmp/updateRuntimeLog.txt
@@ -30,12 +30,34 @@ sh bin/htcondor_make_runtime.sh > ${logFile} 2>&1
 mv TaskManagerRun-$CRAB3_DUMMY_VERSION.tar.gz TaskManagerRun.tar.gz >> ${logFile} 2>&1
 mv CMSRunAnalysis-$CRAB3_DUMMY_VERSION.tar.gz CMSRunAnalysis.tar.gz >> ${logFile} 2>&1
 
-cp -v CMSRunAnalysis.tar.gz CRAB3-externals.zip TaskManagerRun.tar.gz $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/AdjustSites.py $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/dag_bootstrap_startup.sh $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/dag_bootstrap.sh $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/gWMS-CMSRunAnalysis.sh $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/CMSRunAnalysis.sh $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/cmscp.py $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/DashboardFailure.sh $CRABTASKWORKER_ROOT/data  >> ${logFile} 2>&1
+# files to be placed in $CRABTASKWORKER_ROOT/data
+filesToCopy=CMSRunAnalysis.tar.gz
+filesToCopy="$filesToCopy CRAB3-externals.zip"
+filesToCopy="$filesToCopy TaskManagerRun.tar.gz"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/AdjustSites.py"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/dag_bootstrap_startup.sh"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/dag_bootstrap.sh"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/gWMS-CMSRunAnalysis.sh"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/CMSRunAnalysis.sh"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/cmscp.py"
+filesToCopy="$filesToCopy $CRAB_OVERRIDE_SOURCE/CRABServer/scripts/DashboardFailure.sh"
+
+# clean backup of previous Runtime files
+rm -rf  $CRABTASKWORKER_ROOT/data/PreviousRuntime/*
+mkdir -p $CRABTASKWORKER_ROOT/data/PreviousRuntime
+
+# move current files to backup are and then update current TW with new ones
+for file in $filesToCopy; do
+  mv $CRABTASKWORKER_ROOT/data/`basename $file` $CRABTASKWORKER_ROOT/data/PreviousRuntime/
+  cp -v $file $CRABTASKWORKER_ROOT/data  >> ${logFile} 2>&1
+done
 
 if [ $? -eq 0 ]
 then
   echo ""
   echo "OK. New tarballs created and placed inside directory tree: $TW_CURRENT"
+  echo "Previous files have been saved in $CRABTASKWORKER_ROOT/data/PreviousRuntime/"
+  echo "BEWARE: Safest way to revert to original configuration is to re-deploy container"
 else
   echo "*** ERROR ** ERROR ** ERROR ** ERROR ***"
   echo "Tarball creation or copy failed."
