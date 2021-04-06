98e5920eee22ad5ff0ee06700371132246a65509
belforte <stefano.belforte@cern.ch>
2021-04-06 22:01:55 +0200

    pylint

--- src/python/CRABInterface/RESTBaseAPI.py
+++ src/python/CRABInterface/RESTBaseAPI.py
@@ -80,7 +80,7 @@ class RESTBaseAPI(DatabaseRESTApi):
             kwbinds = self.bindmap(**kwbinds)
             c, _ = self.executemany(sql, kwbinds, *binds)
         trace = cherrypy.request.db["handle"]["trace"]
-        trace and cherrypy.log("%s commit" % trace)
+        trace and cherrypy.log("%s commit" % trace)  # pylint: disable=expression-not-assigned
         cherrypy.request.db["handle"]["connection"].commit()
         return rows([{ "modified": c.rowcount }])
 
