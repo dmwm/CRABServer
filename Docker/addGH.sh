41171cc8845268e43fe426d4e3aad27d61d3ab38
Stefano Belforte <stefano.belforte@cern.ch>
2021-04-12 09:21:06 +0200

    add stefano and daina GH repos as remote

--- Docker/addGH.sh
+++ Docker/addGH.sh
@@ -11,15 +11,22 @@ CRABServerDir=`realpath /data/srv/TaskManager/current/*/cms/crabtaskworker`
 export PYTHONPATH=$CRABServerDir/$CRABServerTag/lib/python2.7/site-packages
 WMCoreTag=`python -c "from WMCore import __version__; print __version__"`
 
-# 2. create directories for repositories and clone
+# 2. create directories for repositories and clone 
 mkdir /data/repos
-cd /data/repos
+pushd /data/repos
 git clone https://github.com/dmwm/CRABServer.git
 git clone https://github.com/dmwm/WMCore.git
 
-# 3. checkout the installed tags
+# 3. checkout the installed tags and add ptrs to developers repos
 cd CRABServer
 git checkout $CRABServerTag
+git remote add stefano https://github.com/belforte/CRABServer.git
+git remote add daina https://github.com/ddaina/CRABServer.git
 cd ..
 cd WMCore
 git checkout $WMCoreTag
+git remote add stefano https://github.com/belforte/WMCore.git
+git remote add daina https://github.com/ddaina/WMCore.git
+
+# 4. all done, reset cwd and exit
+popd
