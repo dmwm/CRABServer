2154f98e118da84d5e2cb2f44adb03a610d22d92
Stefano Belforte <stefano.belforte@cern.ch>
2021-05-04 09:35:31 +0200

    user () around list of possible values. Fix #6579 (#6580)

--- src/python/CRABInterface/Regexps.py
+++ src/python/CRABInterface/Regexps.py
@@ -23,7 +23,7 @@ lfnParts.update( {'publishname' : pNameRE,
 RX_TASKNAME_LEN = 232
 RX_TASKNAME  = re.compile(r"^[a-zA-Z0-9\-_:]{1,%s}$" % RX_TASKNAME_LEN)
 #analysis-crab3=prod jobs; analysistest=preprod jobs; analysis-crab3-hc=special HC tests (CSA14, AAA, ...); hctest=site readiness; test=middlewere validation
-RX_ACTIVITY  = re.compile(r'^analysis(test|-crab3(-hc)?)|hc(test|xrootd)|test|integration$')
+RX_ACTIVITY  = re.compile(r'^(analysis(test|-crab3(-hc)?)|hc(test|xrootd)|test|integration)$')
 RX_PUBLISH   = re.compile('^'+pNameRE+'$')
 #RX_LFN       = re.compile(r'^(?=.{0,500}$)/store/(temp/)?(user|group)/%(hnName)s/%(primDS)s/%(publishname)s/%(psethash)s/%(counter)s/(log/)?%(filename)s+$' % lfnParts)
 RX_LFN       = re.compile(r'^(?=[a-zA-Z0-9\-\._/]{0,500}$)/store/(temp/)?(user/%(hnName)s|group|local|test)/?' % lfnParts)
@@ -38,7 +38,7 @@ RX_ARCH      = re.compile(r"^(?=.{0,255}$)slc[0-9]{1}_[a-z0-9]+_gcc[a-z0-9]+(_[a
 RX_DATASET   = re.compile(DATASET_RE) #See https://github.com/dmwm/WMCore/issues/6054#issuecomment-135475550
 RX_LFNPRIMDS = re.compile(r"^%(primDS)s$" % lfnParts)
 RX_BLOCK     = re.compile(r"^(/[a-zA-Z0-9\.\-_]{1,100}){3}#[a-zA-Z0-9\.\-_]{1,100}$")
-RX_SPLIT     = re.compile(r"^Automatic|FileBased|EventBased|LumiBased|EventAwareLumiBased$")
+RX_SPLIT     = re.compile(r"^(Automatic|FileBased|EventBased|LumiBased|EventAwareLumiBased)$")
 RX_CACHEURL  = re.compile(r"^https?://([-\w\.]*)\.cern\.ch+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?$")
 RX_ADDFILE   = re.compile(r"^(?=.{0,255}$)([a-zA-Z0-9\-\._]+)$")
 # Can be a LFN or PFN (anything CMSSW accepts is fine here)
@@ -60,9 +60,9 @@ RX_RUCIORULE = re.compile(r"^(?=.{32}$)[A-Za-z0-9]*$")
 # '1:20,4:20,6:21,8:20,9:20,10:21...' (new format with events per each lumi)
 RX_LUMILIST = re.compile(r"^(\d+(,\d+)*)$|^((\d+:(\d+|None))(,\d+:(\d+|None))*)$")
 RX_GLOBALTAG = re.compile(r'^[a-zA-Z0-9\s\.\-_:]{1,100}$')
-RX_OUTTYPES  = re.compile(r'^EDM|LOG|TFILE|FAKE|POOLIN|DQM$')
+RX_OUTTYPES  = re.compile(r'^(EDM|LOG|TFILE|FAKE|POOLIN|DQM)$')
 RX_CHECKSUM  = re.compile(r'^[A-Za-z0-9\-]+$')
-RX_FILESTATE  = re.compile(r'^TRANSFERRING|FINISHED|FAILED|COOLOFF$')
+RX_FILESTATE  = re.compile(r'^(TRANSFERRING|FINISHED|FAILED|COOLOFF)$')
 RX_LFNPATH   = re.compile(r"^(?=.{0,500}$)%(subdir)s(/%(subdir)s)*/?$" % lfnParts)
 RX_HOURS   = re.compile(r"^\d{0,6}$") #should be able to erase the last 100 years with 6 digits
 RX_URL = re.compile(r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w :\.-])*$")
@@ -76,15 +76,15 @@ RX_CLUSTERID = re.compile(r"^[0-9.]+$")
 RX_CERT = re.compile(r'^[-]{5}BEGIN CERTIFICATE[-]{5}[\w\W]+[-]{5}END CERTIFICATE[-]{5}\n$')
 
 #subresourced of DataUserWorkflow (/workflow) resource
-RX_SUBRESTAT = re.compile(r"^errors|report|logs|data|logs2|data2|resubmit|resubmit2|proceed|publicationstatus|taskads$")
+RX_SUBRESTAT = re.compile(r"^(errors|report|logs|data|logs2|data2|resubmit|resubmit2|proceed|publicationstatus|taskads)$")
 
 #subresources of the ServerInfo (/info) and Task (/task) resources
-RX_SUBRES_SI = re.compile(r"^delegatedn|backendurls|version|bannedoutdest|scheddaddress|ignlocalityblacklist|$")
-RX_SUBRES_TASK = re.compile(r"^allinfo|allusers|summary|search|taskbystatus|getpublishurl|addwarning|deletewarnings|addwebdir|addoutputdatasets|addddmreqid|webdir|counttasksbystatus|lastfailures|updateschedd|updatepublicationtime$")
+RX_SUBRES_SI = re.compile(r"^(delegatedn|backendurls|version|bannedoutdest|scheddaddress|ignlocalityblacklist)$")
+RX_SUBRES_TASK = re.compile(r"^(allinfo|allusers|summary|search|taskbystatus|getpublishurl|addwarning|deletewarnings|addwebdir|addoutputdatasets|addddmreqid|webdir|counttasksbystatus|lastfailures|updateschedd|updatepublicationtime)$")
 
 #subresources of Cache resource
-RX_SUBRES_CACHE = re.compile(r"^upload|download|retrieve|list|used$")
-RX_CACHE_OBJECTTYPE = re.compile(r"^clientlog|twlog|sandbox|debugfiles|runtimefiles")
+RX_SUBRES_CACHE = re.compile(r"^(upload|download|retrieve|list|used)$")
+RX_CACHE_OBJECTTYPE = re.compile(r"^(clientlog|twlog|sandbox|debugfiles|runtimefiles)$")
 
 #worker workflow
 RX_WORKER_NAME = re.compile(r"^[A-Za-z0-9\-\._%]{1,100}$")
@@ -102,19 +102,19 @@ RX_TEXT_FAIL = re.compile(r"^[A-Za-z0-9\-\._\s\=\+/]{0,10000}$")
 ## user dn
 RX_DN = re.compile(r"^/(?:C|O|DC)=.*/CN=.")
 ## worker subresources
-RX_SUBPOSTWORKER = re.compile(r"^state|start|failure|success|process|lumimask$")
+RX_SUBPOSTWORKER = re.compile(r"^(state|start|failure|success|process|lumimask)$")
 RX_SUBGETWORKER = re.compile(r"jobgroup")
 
 # Schedulers
-RX_SCHEDULER = re.compile(r"^panda|condor$")
+RX_SCHEDULER = re.compile(r"^(panda|condor)$")
 
 
 
 
 
 # File Transfers api
-RX_SUBPOSTTRANSFER = re.compile(r"^acquireTransfers|acquirePublication|updateTransfers|updatePublication|retryPublication|retryTransfers|killTransfers$")
-RX_SUBGETTRANSFER = re.compile(r"^acquiredTransfers|acquiredPublication|getVOMSAttributesForTask|groupedTransferStatistics|groupedPublishStatistics|activeUsers|getTransfersToKill$")
+RX_SUBPOSTTRANSFER = re.compile(r"^(acquireTransfers|acquirePublication|updateTransfers|updatePublication|retryPublication|retryTransfers|killTransfers)$")
+RX_SUBGETTRANSFER = re.compile(r"^(acquiredTransfers|acquiredPublication|getVOMSAttributesForTask|groupedTransferStatistics|groupedPublishStatistics|activeUsers|getTransfersToKill)$")
 
 RX_USERGROUP = RX_ANYTHING
 RX_USERROLE = RX_ANYTHING
@@ -138,7 +138,5 @@ RX_TRANSFER_STATE = re.compile(r"^[01234567]")
 RX_PUBLISH_STATE = re.compile(r"^[01234]")
 RX_ASO_WORKERNAME = RX_WORKER_NAME
 
-RX_SUBGETUSERTRANSFER = re.compile(r"^getById|getTransferStatus|getPublicationStatus$")
-RX_SUBPOSTUSERTRANSFER = re.compile(r"^killTransfers|retryPublication|retryTransfers|killTransfersById|updateDoc$")
-
-
+RX_SUBGETUSERTRANSFER = re.compile(r"^(getById|getTransferStatus|getPublicationStatus)$")
+RX_SUBPOSTUSERTRANSFER = re.compile(r"^(killTransfers|retryPublication|retryTransfers|killTransfersById|updateDoc)$")
