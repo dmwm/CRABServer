ba8b97358e6cdbe85071a7d3c8763a0bba9dfd9d
belforte <stefano.belforte@cern.ch>
2021-05-15 14:57:13 +0200

    Do length check after calling Lexicon

--- src/python/TaskWorker/Actions/DagmanCreator.py
+++ src/python/TaskWorker/Actions/DagmanCreator.py
@@ -243,12 +243,14 @@ def validateLFNs(path, outputFiles):
         else:
             fileName = "%s_%s" % (origFile, jobId)
         testLfn = os.path.join(path, dirCounter, fileName)
-        if len(testLfn) > 495 :  # max LNF length is 500, allow for addition of '/temp' for local stageout
+        Lexicon.lfn(testLfn)  # will raise if testLfn is not a valid lfn
+        # since Lexicon does not have lenght check, do it manually here
+        # max LNF length is 500, allow for addition of '/temp' for local stageout
+        if len(testLfn) > 495 :
             msg = "\nYour task speficies an output LFN %d-char long " % len(testLfn)
             msg += "\n which exceeds maximum length of 495"
             msg += "\n and therefore can not be published in DBS"
             raise TaskWorker.WorkerExceptions.TaskWorkerException(msg)
-        Lexicon.lfn(testLfn)  # will raise if testLfn is not a valid lfn
     return
 
 def transform_strings(data):
