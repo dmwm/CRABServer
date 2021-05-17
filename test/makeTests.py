4cd0ef27cd23ea440e3c219f3447964bf28ac122
belforte <stefano.belforte@cern.ch>
2021-05-17 14:24:00 +0200

    add list of test tasks

---
+++ test/makeTests.py
@@ -0,0 +1,205 @@
+"""
+An almost standalone python script which creates a set of files for CRAB validation
+Requires the testUtils.py file to be placed in same directory
+The script output is a set of files written to current directory
+ A simple PSET.py file to be used in all tests
+ A list of crab configuration files, one for each test, named: <testName>.py
+ A list of checking script to be executed to validate each test, named:  <testName>.sh
+
+ Tasks needs to be submitted from the directory where they have been
+  created, since they may need additional external files created by this script.
+
+ Checking scripts take a task name as only argument, and exit with code
+    0 if all OK
+    1 if FAIL
+    2 to main "wait and run me again later"
+   there is no validation of the task name argument, a bad one results in exit code 1
+   These script need to be executed like:
+     bash <scriptName> <taskName>     or
+     bash -x <scriptName> <taskName>
+
+"""
+from testUtils import writePset, changeInConf, writeConfigFile, writeValidationScript
+
+writePset()
+
+#
+# test CRAB Configuration file parameters
+#
+
+#=============================
+# SECTION GENERAL
+#=============================
+
+# transferOutputs
+name ='transferOutputs'
+changeDict = {'param': name, 'value': 'False', 'section': 'General'}
+confChangesList = [changeDict]
+validationScript = """
+checkStatus ${taskName} COMPLETED
+crabCommand getlog "--short --jobids=1"
+lookFor "Retrieved job_out.1.*.txt" commandLog.txt
+lookFor "disabled" ${workDir}/results/job_out.1.*.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# transferLogs
+name ='transferLogs'
+changeDict = {'param': name, 'value': 'False', 'section': 'General'}
+confChangesList = [changeDict]
+validationScript = """
+checkStatus ${taskName} COMPLETED
+crabCommand getlog "--short --jobids=1"
+lookFor "Retrieved job_out.1.*.txt" commandLog.txt
+lookFor "disabled" ${workDir}/results/job_out.1.*.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+#=============================
+# SECTION JOB TYPE
+#=============================
+
+# inputFiles
+# to be filled
+
+# disableAutomaticOutputCollection
+name = 'disableAutomaticOutputCollection'
+changeDict = {'param': name, 'value': 'True', 'section': 'JobType'}
+confChangesList = [changeDict]
+validationScript = """
+checkStatus ${taskName} COMPLETED
+crabCommand getlog "--short --jobids=1"
+lookFor "Retrieved job_out.1.*.txt" commandLog.txt
+lookFor "^Output file : \$" ${workDir}/results/job_out.1.*.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# outputFiles
+name ='outputFiles'
+confChangesList = []
+changeDict = {'param': 'disableAutomaticOutputCollection', 'value': 'True', 'section': 'JobType'}
+confChangesList.append(changeDict)
+changeDict = {'param': 'outputFiles', 'value': '"output.root"', 'section': 'JobType'}
+confChangesList.append(changeDict)
+validationScript = """
+checkStatus ${taskName} COMPLETED
+crabCommand getoutput --jobids=1"
+lookFor "Success in retrieving output_1.root " commandLog.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# allowUndistributedCMSSW
+name ='allowUndistributedCMSSW'
+changeDict = {'param': name, 'value': 'True', 'section': 'JobType'}
+confChangesList = [changeDict]
+validationScript = """
+checkStatus ${taskName} SUBMITTED
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# maxMemoryMB
+name = 'maxMemoryMB'
+confChangesList = []
+changeDict = {'param': name, 'value': '2500', 'section': 'JobType'}
+confChangesList.append(changeDict)
+validationScript = """
+checkStatus ${taskName} COMPLETED
+crabCommand getlog "--short --jobids=1"
+lookFor "Retrieved job_out.1.*.txt" commandLog.txt
+lookFor "JOB AD: RequestMemory = 2500" ${workDir}/results/job_out.1.*.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# maxJobRuntimeMin
+name = 'maxJobRuntimeMin'
+changeDict = {'param': name, 'value': '100', 'section': 'JobType'}
+confChangesList = [changeDict]
+validationScript = """
+checkStatus ${taskName} COMPLETED
+crabCommand getlog "--short --jobids=1"
+lookFor "Retrieved job_out.1.*.txt" commandLog.txt
+lookFor "JOB AD: MaxWallTimeMins_RAW = 100" ${workDir}/results/job_out.1.*.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# numCores
+# to be filled
+
+# scriptExe
+# to be filled
+
+# scriptArgs
+# to be filled (used ony with scriptExe)
+
+# sendPythonFolder
+name = 'sendPythonFolder'
+changeDict = {'param': name, 'value': 'True', 'section': 'JobType'}
+confChangesList = [changeDict]
+#TODO need a way to check sandbox content
+validationScript = """
+checkStatus ${taskName} SUBMITTED
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+
+# sendExternalFolder
+name = 'sendExternalFolder'
+changeDict = {'param': name, 'value': 'True', 'section': 'JobType'}
+confChangesList = [changeDict]
+#TODO need a way to check sandbox content
+validationScript = """
+checkStatus ${taskName} SUBMITTED
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+#=============================
+# SECTION DATA
+#=============================
+
+
+#=============================
+# SECTION SITE
+#=============================
+
+
+#=============================
+# SECTION DEBUG
+#=============================
+
+# extraJDL
+# to be filled
+
+# scheddName
+name = 'scheddName'
+confChangesList = []
+changeDict = {'param': name, 'value': '"crab3@vocms059.cern.ch"', 'section': 'Debug'}
+confChangesList.append(changeDict)
+validationScript = """
+checkStatus ${taskName} SUBMITTED
+lookFor "crab3@vocms059.cern.ch" statusLog.txt
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
+# collector
+name = 'collector'
+confChangesList = []
+changeDict = {'param': name, 'value': '"cmsgwms-collector-itb.cern.ch"', 'section': 'Debug'}
+confChangesList.append(changeDict)
+changeDict = {'param': 'scheddName', 'value': '"crab3@vocms068.cern.ch"', 'section': 'Debug'}
+confChangesList.append(changeDict)
+validationScript = """
+checkStatus ${taskName} SUBMITTED
+"""
+writeConfigFile(testName=name, listOfDicts=confChangesList)
+writeValidationScript(testName=name, validationScript=validationScript)
+
---
+++ test/testUtils.py
@@ -0,0 +1,143 @@
+def writePset():
+    psetFileContent = """
+    import FWCore.ParameterSet.Config as cms
+    
+    process = cms.Process('NoSplit')
+    
+    process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch///store/mc/HC/GenericTTbar/GEN-SIM-RECO/CMSSW_5_3_1_START53_V5-v1/0010/00CE4E7C-DAAD-E111-BA36-0025B32034EA.root'))
+    process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+    process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+    process.output = cms.OutputModule("PoolOutputModule",
+        outputCommands = cms.untracked.vstring("drop *", "keep recoTracks_globalMuons_*_*"),
+        fileName = cms.untracked.string('output.root'),
+    )
+    process.out = cms.EndPath(process.output)
+    """
+
+    with open('PSET.py', 'w') as fp:
+        fp.write(psetFileContent)
+    return
+
+def changeInConf(configuration=None, paramName=None, paramValue=None, configSection=None):
+    newParamLine = 'config.%s.%s = %s\n' % (configSection, paramName, paramValue)
+    # is this param already present in the configuration ?
+    if paramName in configuration:
+        # override:
+        for line in configuration.split('\n'):
+            if paramName in line :
+                existingLine = line
+                break
+        configuration = configuration.replace(existingLine, newParamLine)
+    else:
+        # add below section header
+        cfgSectionHeader = "config.section_('%s')\n" % configSection
+        newCfgLines = cfgSectionHeader + newParamLine
+        configuration = configuration.replace(cfgSectionHeader, newCfgLines)
+    return configuration
+
+def writeConfigFile(testName=None, listOfDicts=[]):
+    standardConfig = """
+# a simple configuration which is customized for each test
+#
+import time
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_('General')
+config.General.instance = 'prod'
+config.General.workArea = '/tmp/crabTestConfig'
+config.General.requestName = REQUESTNAME
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSET.py'
+config.JobType.maxJobRuntimeMin = 60
+
+config.section_('Data')
+config.Data.inputDataset = '/GenericTTbar/HC-CMSSW_9_2_6_91X_mcRun1_realistic_v2-v2/AODSIM'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = 1
+config.Data.publication = False
+config.Data.outputDatasetTag = 'CrabAutoTest_' + config.General.requestName
+
+config.section_('User')
+
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+
+config.section_('Debug')
+"""
+    conf = standardConfig
+    for d in listOfDicts:
+        param = d['param']
+        value = d['value']
+        section = d['section']
+        conf = changeInConf(configuration=conf, paramName=param, paramValue=value, configSection=section)
+    # also set the requestName (do it now to avoid confusing changeInConf)
+    conf = conf.replace('REQUESTNAME', '"'+testName+'"')
+    with open(testName + '.py', 'w') as fp:
+        fp.write(conf)
+    return
+
+def writeValidationScript(testName=None, validationScript=None):
+    validationScriptHeader = """#!/bin/bash
+function checkStatus {
+  # check that taskName has reached targetStatus and writes statusLog.txt
+  # if target = SUBMITTED, accepts status COMPLETED as well
+  # if target = COMPLETED and status is SUBMITTED, ask for retry after delay
+  # Fail test if command fails or status is not good
+  local taskName="$1"
+  local targetStatus="$2"
+
+  crab remake --task ${taskName} 2>&1 | tee remakeLog.txt 
+  [ $? -ne 0 ] && exit -1  # if remake fails, abort
+  grep -q Success remakeLog.txt || exit -1  # if log does not contain "Success" string, abort
+  workDir=`grep Success remakeLog.txt | awk '{print $NF}'`
+  crab status -d $workDir 2>&1 | tee  statusLog.txt
+  [ $? -ne 0 ] && exit -1  # if crab status fails, abort
+
+  local isSub=0
+  local isDone=0
+  grep -q "Status on the scheduler:.*SUBMITTED" statusLog.txt 2>&1 && isSub=1
+  grep -q "Status on the scheduler:.*COMPLETED" statusLog.txt 2>&1 && isDone=1
+
+  case $targetStatus in
+    NONE)
+      # no check is needed
+      ;;
+    SUBMITTED)
+      # both SUBMITTED or COMPLETED are OK
+      [ ${isSub} -eq 0 ] && [ ${isDone} -eq 0 ] && exit 2
+      ;;
+    COMPLETED)
+      [ ${isSub} -eq 1 ] && exit 1  # ask for a check later on
+      [ ${isDone} -eq 0 ] && exit 2
+  esac
+  return 0
+}
+
+function lookFor {
+  # looks for string in file. Fail test if not found
+  local string="$1"
+  local file="$2"
+  grep -q "${string}" ${file}
+  [ $? -ne 0 ] && exit 2
+  return 0
+}
+
+function crabCommand() {
+  # execute crab command and write commandLog.txt.
+  # Fails test if command exit code is non 0
+  local cmd="$1"
+  local params="$2"
+  crab $cmd $params 2>&1 | tee commandLog.txt
+  [ $? -ne 0 ] && exit 2
+  return 0
+}
+taskName="$1"
+"""
+    with open(testName + '.sh', 'w') as fp:
+        fp.write(validationScriptHeader)
+        fp.write(validationScript)
+    return
