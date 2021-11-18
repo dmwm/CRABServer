a00531190822452d21275d184692a359a43d0602
Dario Mapelli <mapelli.dario@gmail.com>
2021-11-18 15:11:01 +0100

    HTCondorWorkflow: decode to str before parsing

--- src/python/CRABInterface/HTCondorDataWorkflow.py
+++ src/python/CRABInterface/HTCondorDataWorkflow.py
@@ -316,7 +316,7 @@ class HTCondorDataWorkflow(DataWorkflow):
                 else:
                     self.logger.info("Node state file is too old or does not have an update time. Stale info is shown")
             except Exception as ee:
-                addStatusAndFailure(result, status = 'UNKNOWN', failure = ee.info)
+                addStatusAndFailure(result, status = 'UNKNOWN', failure = str(ee))
                 return [result]
 
         if 'DagStatus' in taskStatus:
@@ -571,7 +571,7 @@ class HTCondorDataWorkflow(DataWorkflow):
         if first_char == "[":
             return self.parseNodeStateV2(fp, nodes)
         for line in fp.readlines():
-            m = self.job_re.match(line)
+            m = self.job_re.match(line.decode("utf8") if isinstance(line, bytes) else line)
             if not m:
                 continue
             nodeid, status, msg = m.groups()
