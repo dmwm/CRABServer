11ec3bc9c2251c8290e131626146551c5ad24391
belforte <stefano.belforte@cern.ch>
2021-11-30 16:58:54 +0100

    improve log

--- src/script/Deployment/TaskWorker/updateTMRuntime.sh
+++ src/script/Deployment/TaskWorker/updateTMRuntime.sh
@@ -1,18 +1,12 @@
 # Run the script inside CRABServer
 # this is a hardcoded name in htcondor_make_runtime.sh with no relation
 # with actual CRAB version
-
-set -x
-
 CRAB3_DUMMY_VERSION=3.3.0-pre1
 
-#
 # Will replace the tarball in whatever is current TW
 
 TW_HOME=/data/srv/TaskManager
-
 TW_CURRENT=${TW_HOME}/current
-
 TW_RELEASE=`ls -l ${TW_CURRENT}|awk '{print $NF}'|tr -d '/'`
 TW_ARCH=$(basename $(ls -d ${TW_CURRENT}/sl*))
 CRABTASKWORKER_ROOT=${TW_CURRENT}/${TW_ARCH}/cms/crabtaskworker/${TW_RELEASE}
@@ -26,10 +20,19 @@ fi
 
 logFile=/tmp/updateRuntimeLog.txt
 
+echo "working environment:" > ${logFile}
+echo TW_HOME = $TW_HOME >> ${logFile}
+echo TW_CURRENT = $TW_CURRENT >> ${logFile}
+echo TW_RELEASE = $TW_RELEASE >> ${logFile}
+echo TW_ARCH = $TW_ARCH >> ${logFile}
+echo CRABTASKWORKER_ROOT = $CRABTASKWORKER_ROOT >> ${logFile}
+
 # CRAB_OVERRIDE_SOURCE tells htcondor_make_runtime.sh where to find the CRABServer repository
 export CRAB_OVERRIDE_SOURCE=${GHrepoDir}
+echo CRAB_OVERRIDE_SOURCE = $CRAB_OVERRIDE_SOURCE >> ${logFile}
 pushd $CRAB_OVERRIDE_SOURCE/CRABServer > /dev/null
-sh bin/htcondor_make_runtime.sh > ${logFile} 2>&1
+echo "current working directory is: " `pwd` >> ${logFile}
+sh bin/htcondor_make_runtime.sh >> ${logFile} 2>&1
 mv TaskManagerRun-$CRAB3_DUMMY_VERSION.tar.gz TaskManagerRun.tar.gz >> ${logFile} 2>&1
 mv CMSRunAnalysis-$CRAB3_DUMMY_VERSION.tar.gz CMSRunAnalysis.tar.gz >> ${logFile} 2>&1
 
