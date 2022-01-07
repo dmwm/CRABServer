23d2bf4ada6f6004cfdcfb4c02dce4c707e36070
Stefano Belforte <stefano.belforte@cern.ch>
2022-01-07 23:04:50 +0100

    pass $XrdSecGSISRVNAMES to cmsRun. Fix #6953 (#6955) (#6956)

--- scripts/CMSRunAnalysis.py
+++ scripts/CMSRunAnalysis.py
@@ -639,6 +639,9 @@ if __name__ == "__main__":
         preCmd = 'export X509_USER_PROXY=%s; ' % os.getenv('X509_USER_PROXY')
         # needed for root problem with $HOME/.root.mimes, #6801
         preCmd += 'export HOME=${HOME:-$PWD}; '
+        # needed for accessing EOS at RAL (Echo). See https://ggus.eu/index.php?mode=ticket_info&ticket_id=155272
+        if os.getenv('XrdSecGSISRVNAMES'):
+            preCmd += 'export XrdSecGSISRVNAMES=%s; ' % os.getenv('XrdSecGSISRVNAMES')
         print("==== %s Execution started at %s ====" % (applicationName, time.asctime(time.gmtime())))
         if not options.scriptExe :
             cmd = 'cmsRun -p PSet.py -j FrameworkJobReport.xml'
