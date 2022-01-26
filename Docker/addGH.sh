145dbb0076426ffe1b239e0e5f5e14e5d936dce0
Stefano Belforte <stefano.belforte@cern.ch>
2022-01-26 18:03:18 +0100

    another stupid bug in previous commit

--- Docker/addGH.sh
+++ Docker/addGH.sh
@@ -9,7 +9,7 @@ CRABServerDir=`realpath /data/srv/TaskManager/current/*/cms/crabtaskworker`
 
 # 1. find out which WMCore tag was installed
 export PYTHONPATH=$CRABServerDir/$CRABServerTag/lib/python3.8/site-packages
-WMCoreTag=`python3 -c "from WMCore import __version__; print __version__"`
+WMCoreTag=`python -c "from WMCore import __version__; print __version__"`
 
 # 2. create directories for repositories and clone 
 mkdir /data/repos
