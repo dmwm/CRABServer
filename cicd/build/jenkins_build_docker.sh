e1b11e7b5c72e9b08782f76641e75d2709755894
Thanayut Seethongchuen <=>
2022-02-23 14:55:37 +0100

    check DOCKER_CONFIG before relogin

--- cicd/build/jenkins_build_docker.sh
+++ cicd/build/jenkins_build_docker.sh
@@ -32,16 +32,19 @@ docker build . -t registry.cern.ch/cmscrab/crabtaskworker:${RELEASE_TAG} --netwo
 docker push registry.cern.ch/cmscrab/crabtaskworker:${RELEASE_TAG}
 docker rmi registry.cern.ch/cmscrab/crabtaskworker:${RELEASE_TAG}
 
-##build and push crabserver image
-#cd $WORKSPACE
-#git clone https://github.com/dmwm/CMSKubernetes.git
-#cd CMSKubernetes/docker/
-#
-##get HG version tag from comp.crab_${BRANCH} repo, e.g. HG2201a-cde79778caecdc06e9b316b5530c1da5
-#HGVERSION=$(curl -s "http://cmsrep.cern.ch/cmssw/repos/comp.crab_${BRANCH}/slc7_amd64_gcc630/latest/RPMS.json" | grep -oP 'HG\d{4}(.*)(?=":)' | head -1)
-#sed -i.bak -e "/REPO=\"comp*/c\REPO=\"comp.crab_${BRANCH}\"" -e "s/VER=HG.*/VER=$HGVERSION/g" -- crabserver/install.sh
-#echo "(DEBUG) diff dmwm/CMSKubernetes/docker/crabserver/install.sh"
-#diff -u crabserver/install.sh.bak crabserver/install.sh || true
-#echo "(DEBUG) end"
-#
+#build and push crabserver image
+cd $WORKSPACE
+git clone https://github.com/dmwm/CMSKubernetes.git
+cd CMSKubernetes/docker/
+
+#get HG version tag from comp.crab_${BRANCH} repo, e.g. HG2201a-cde79778caecdc06e9b316b5530c1da5
+HGVERSION=$(curl -s "http://cmsrep.cern.ch/cmssw/repos/comp.crab_${BRANCH}/slc7_amd64_gcc630/latest/RPMS.json" | grep -oP 'HG\d{4}(.*)(?=":)' | head -1)
+sed -i.bak -e "/REPO=\"comp*/c\REPO=\"comp.crab_${BRANCH}\"" -e "s/VER=HG.*/VER=$HGVERSION/g" -- crabserver/install.sh
+echo "(DEBUG) diff dmwm/CMSKubernetes/docker/crabserver/install.sh"
+diff -u crabserver/install.sh.bak crabserver/install.sh || true
+echo "(DEBUG) end"
+
+echo $DOCKER_CONFIG
+# relogin to using cmsweb robot account
+#docker login registry.cern.ch --username $HARBOR_CMSWEB_USERNAME --password-stdin <<< $HARBOR_CMSWEB_PASSWORD
 #CMSK8STAG=${RELEASE_TAG} ./build.sh "crabserver"
