7103882699e57d1a4d87b9485b4d39bf672f1c0d
Dario Mapelli <mapelli.dario@gmail.com>
2022-03-01 14:52:40 +0100

    make python3 compatible with master - no merge conflict (#7103)

--- Docker/addGH.sh
+++ Docker/addGH.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # add GitHub repositories for CRABServer and WMCore
 
-# TW_VERSION is set by Dockerfile
-CRABServerTag=$TW_VERSION
-
 # 0. locate the crabserver directory created by install.sh
+# something like: /data/srv/TaskManager/py3.220105-efe7bf851559ee417e352b26e70998cd/slc7_amd64_gcc630/cms/crabtaskworker
 CRABServerDir=`realpath /data/srv/TaskManager/current/*/cms/crabtaskworker`
 
 # 1. find out which WMCore tag was installed
-export PYTHONPATH=$CRABServerDir/$CRABServerTag/lib/python2.7/site-packages
-WMCoreTag=`python -c "from WMCore import __version__; print __version__"`
+export PYTHONPATH=$CRABServerDir/$RPM_RELEASETAG_HASH/lib/python3.8/site-packages
+WMCoreGHTag=$(grep __version__ $CRABServerDir/$RPM_RELEASETAG_HASH/lib/python*/site-packages/WMCore/__init__.py | cut -f2 -d"=" |  tr -d " '\"")
 
 # 2. create directories for repositories and clone 
 mkdir /data/repos
@@ -19,18 +17,20 @@ git clone https://github.com/dmwm/WMCore.git
 
 # 3. checkout the installed tags and add ptrs to developers repos
 cd CRABServer
-git checkout $CRABServerTag
+git checkout $RELEASE_TAG
 git remote add stefano https://github.com/belforte/CRABServer.git
 git remote add daina https://github.com/ddaina/CRABServer.git
 git remote add mapellidario https://github.com/mapellidario/CRABServer.git
 git remote add diego https://github.com/dciangot/CRABServer.git
+git remote add wa https://github.com/novicecpp/CRABServer.git
 cd ..
 cd WMCore
-git checkout $WMCoreTag
+git checkout $WMCoreGHTag
 git remote add stefano https://github.com/belforte/WMCore.git
 git remote add daina https://github.com/ddaina/WMCore.git
 git remote add mapellidario https://github.com/mapellidario/WMCore.git
 git remote add diego https://github.com/dciangot/WMCore.git
+git remote add wa https://github.com/novicecpp/CRABServer.git
 
 # 4. create .gitconfig
 cat > ~/.gitconfig << EOF
