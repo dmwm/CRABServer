35ab935daad7a2152a3cb158e7dc8f1ebaef681e
Stefano Belforte <stefano.belforte@cern.ch>
2022-11-09 16:38:48 +0100

    typo in DBSDataDiscovery test

--- src/python/TaskWorker/Actions/DBSDataDiscovery.py
+++ src/python/TaskWorker/Actions/DBSDataDiscovery.py
@@ -549,9 +549,9 @@ if __name__ == '__main__':
     else:
         config.TaskWorker.cmscert = '/data/certs/servicecert.pem'
     if "X509_USER_KEY" in os.environ:
-        config.TaskWorker.cmscert = os.environ["X509_USER_KEY"]
+        config.TaskWorker.cmskey = os.environ["X509_USER_KEY"]
     else:
-        config.TaskWorker.cmscert = '/data/certs/servicekey.pem'
+        config.TaskWorker.cmskey = '/data/certs/servicekey.pem'
 
     config.TaskWorker.envForCMSWEB = newX509env(X509_USER_CERT=config.TaskWorker.cmscert,
                                                 X509_USER_KEY=config.TaskWorker.cmskey)
