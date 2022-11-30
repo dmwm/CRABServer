d1fc2b2595496817441927baa71e0bbe49cd5537
Dario Mapelli <mapelli.dario@gmail.com>
2022-11-30 15:05:06 +0100

    jobwrapper - fixing bug in calling cmsset_default (#7491)

--- scripts/submit_env.sh
+++ scripts/submit_env.sh
@@ -37,31 +37,30 @@ setup_local_env () {
 setup_cmsset() {
     ### source the CMSSW stuff using either OSG or LCG style entry env. or CVMFS
     echo "======== CMS environment load starting at $(TZ=GMT date) ========"
-    CMSSET_DEFAULT_PATH=""
     if [ -f "$VO_CMS_SW_DIR"/cmsset_default.sh ]
     then  #   LCG style --
         echo "WN with a LCG style environment, thus using VO_CMS_SW_DIR=$VO_CMS_SW_DIR"
-        CMSSET_DEFAULT_PATH=$VO_CMS_SW_DIR/cmsset_default.sh
+        . $VO_CMS_SW_DIR/cmsset_default.sh
     elif [ -f "$OSG_APP"/cmssoft/cms/cmsset_default.sh ]
     then  #   OSG style --
         echo "WN with an OSG style environment, thus using OSG_APP=$OSG_APP"
-        CMSSET_DEFAULT_PATH=$OSG_APP/cmssoft/cms/cmsset_default.sh CMSSW_3_3_2
+        . $OSG_APP/cmssoft/cms/cmsset_default.sh CMSSW_3_3_2
     elif [ -f "$CVMFS"/cms.cern.ch/cmsset_default.sh ]
     then
         echo "WN with CVMFS environment, thus using CVMFS=$CVMFS"
-        CMSSET_DEFAULT_PATH=$CVMFS/cms.cern.ch/cmsset_default.sh
+        . $CVMFS/cms.cern.ch/cmsset_default.sh
     elif [ -f /cvmfs/cms.cern.ch/cmsset_default.sh ]
     then  # ok, lets call it CVMFS then
-        CVMFS=/cvmfs/cms.cern.ch
+        export CVMFS=/cvmfs/cms.cern.ch
         echo "WN missing VO_CMS_SW_DIR/OSG_APP/CVMFS environment variable, forcing it to CVMFS=$CVMFS"
-        CMSSET_DEFAULT_PATH=$CVMFS/cmsset_default.sh
+        . $CVMFS/cmsset_default.sh
     else
         echo "Error during job bootstrap: VO_CMS_SW_DIR, OSG_APP, CVMFS or /cvmfs were not found." >&2
         echo "  Because of this, we can't load CMSSW. Not good." >&2
         exit 11003
     fi
-    . $CMSSET_DEFAULT_PATH
-    echo -e "========  CMS environment load finished at $(TZ=GMT date) ========\n"
+    echo "CRAB job bootstrap: thinks it found the correct CMSSW setup script"
+    echo -e "======== CMS environment load finished at $(TZ=GMT date) ========\n"
 }
 
 setup_python_comp() {
