dmwm/CRABServer

commit b1d1dd2be11116b506aed6c0410553025537131a

Use python3 for every command inside the jobwrapper (#7510)
* jobwrapper - force python3 when it can be ambiguous

* gWMS-CMSRunAnalysis.sh - print date when running condor_chirp

scripts/task_process/task_proc_wrapper.sh
@@ -13,7 +13,7 @@ function manage_transfers {
     log "Running transfers.py"
 
     if [[ -f task_process/transfers.txt ]]; then
-        DEST_LFN=`python -c 'import sys, json; print json.loads( open("task_process/transfers.txt").readlines()[0] )["destination_lfn"]' `
+        DEST_LFN=`python3 -c 'import sys, json; print json.loads( open("task_process/transfers.txt").readlines()[0] )["destination_lfn"]' `
 
         if [[ $DEST_LFN =~ ^/store/user/rucio/* ]]; then
         timeout 15m env PYTHONPATH=$PYTHONPATH:$RucioPy3 python3 task_process/RUCIO_Transfers.py

scripts/CMSRunAnalysis.sh
@@ -14,7 +14,7 @@ sigterm() {
   ls -lnh
   if [ ! -e logCMSSWSaved.txt ];
   then
-    python -c "import CMSRunAnalysis; logCMSSW()"
+    python3 -c "import CMSRunAnalysis; CMSRunAnalysis.logCMSSW()"
   fi
 }
 

scripts/gWMS-CMSRunAnalysis.sh
@@ -44,6 +44,7 @@ function chirp_exit_code {
     #and is successfully loaded. It is taken from the EXIT_STATUS variable
     #instead (which contains the short exit coce)
 
+    echo "======== Figuring out long exit code of the job for condor_chirp at $(TZ=GMT date)========"
     #check if the command condor_chirp exists
     command -v condor_chirp > /dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -51,7 +52,6 @@ function chirp_exit_code {
         return
     fi
     #check if exitCode is in there and report it
-    echo "======== Figuring out long exit code of the job for condor_chirp ========"
     #any error or exception will be printed to stderr and cause $? <> 0
     LONG_EXIT_CODE=$(python << END
 from __future__ import print_function

commit 199530ac7ac16548842725c183bcdd0dc5e27e8f

in python3 print is a function
fix bug introduced by  https://github.com/dmwm/CRABServer/commit/b1d1dd2be11116b506aed6c0410553025537131a

scripts/task_process/task_proc_wrapper.sh
@@ -13,7 +13,7 @@ function manage_transfers {
     log "Running transfers.py"
 
     if [[ -f task_process/transfers.txt ]]; then
-        DEST_LFN=`python3 -c 'import sys, json; print json.loads( open("task_process/transfers.txt").readlines()[0] )["destination_lfn"]' `
+        DEST_LFN=`python3 -c 'import sys, json; print (json.loads( open("task_process/transfers.txt").readlines()[0] )["destination_lfn"])' `
 
         if [[ $DEST_LFN =~ ^/store/user/rucio/* ]]; then
         timeout 15m env PYTHONPATH=$PYTHONPATH:$RucioPy3 python3 task_process/RUCIO_Transfers.py