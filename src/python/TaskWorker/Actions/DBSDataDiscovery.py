f601c4b61cd2df2a4e8d0bf77f665e8a34c786e5
Stefano Belforte <stefano.belforte@cern.ch>
2023-04-28 19:11:58 +0200

    correct accounting of tape locations. Fix #7589 (#7619)
    * correct accounting of tape locations. Fix #7589
    
    * proper handling of set elements!

--- src/python/TaskWorker/Actions/DBSDataDiscovery.py
+++ src/python/TaskWorker/Actions/DBSDataDiscovery.py
@@ -151,7 +151,7 @@ class DBSDataDiscovery(DataDiscovery):
                 if 'T1_RU_JINR_Tape' in tapeLocations and len(tapeLocations) > 1:
                     tapeLocations.remove('T1_RU_JINR_Tape')  # JINR tape data are often duplicated
                 if len(tapeLocations) == 1:
-                    if 'US' in tapeLocations:
+                    if 'US' in list(tapeLocations)[0]:
                         ALL_RSES += "&(country=US|country=BR)"
                     else:
                         ALL_RSES += "\country=US\country=BR"  # Rucio wants the set complement operator \
@@ -453,11 +453,11 @@ class DBSDataDiscovery(DataDiscovery):
             secondaryBlocksWithLocation = secondaryLocationsMap.copy().keys()
 
         # take note of where tapes are
-        tapeLocations = []
+        tapeLocations = set()
         for block, locations in locationsMap.items():
             for rse in locations:
                 if 'Tape' in rse:
-                    tapeLocations.append(rse)
+                    tapeLocations.add(rse)
         # then filter out TAPE locations
         self.keepOnlyDiskRSEs(locationsMap)
 
