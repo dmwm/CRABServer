04d05e72cfe35c41e0d73ba366c1447cfb5935af
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2023-05-12 15:46:50 +0200

    RUCIO_Transfer refactoring: Boostrap refactoring and add building task's dataset (#7587)
    * first draft
    
    * Fix anchor script and add comment
    
    * adjust code according to Stefano's comments
    
    * fix comment
    
    ---------
    
    Co-authored-by: Thanayut Seethongchuen <=>

---
+++ src/python/ASO/Rucio/Main.py
@@ -0,0 +1,70 @@
+import logging
+from argparse import ArgumentParser
+
+import ASO.Rucio.config as config
+from ASO.Rucio.RunTransfer import RunTransfer
+from ASO.Rucio.exception import RucioTransferException
+
+def initLogger():
+    logger = logging.getLogger('RucioTransfer')
+    logger.setLevel(logging.DEBUG)
+    hldr = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+    hldr.setFormatter(formatter)
+    logger.addHandler(hldr)
+
+
+def main():
+    """
+    This main function is mean to called by RUCIO_Transfers.py script.
+    Arguments are process here and only for run integration test or
+    run process directly from this file.
+    """
+    opt = ArgumentParser(usage=__doc__)
+    opt.add_argument("--force-publishname", dest="force_publishname", default=None, type=str,
+                     help="use provided output dataset name instead of output")
+    opt.add_argument("--force-last-line", dest="force_last_line", default=None, type=int,
+                     help="")
+    opt.add_argument("--last-line-path", dest="last_line_path",
+                     default='task_process/transfers/last_transfer.txt',
+                     help="")
+    opt.add_argument("--transfer-txt-path", dest="transfers_txt_path",
+                     default='task_process/transfers.txt',
+                     help="")
+    opt.add_argument("--rest-info-path", dest="rest_info_path",
+                     default='task_process/RestInfoForFileTransfers.json',
+                     help="")
+    opt.add_argument("--container-ruleid-path", dest="container_ruleid_path",
+                     default='task_process/transfers/container_ruleid.txt',
+                     help="")
+    opts = opt.parse_args()
+
+    # Put args to config module to share variable across process.
+    # NOTE: For unittest, manually instantiate new one with argparse.Namespace
+    # class before execute test.
+    config.args = opts
+
+    # init logger
+    initLogger()
+    logger = logging.getLogger('RucioTransfer')
+
+
+    # Execute RunTransfer.algorithm().
+    # Exception handling here is for debugging purpose. If
+    # RucioTransferException was raise, it mean some condition is
+    # not meet and we want to fail (fast) this process.  But if
+    # Exception is raise mean something gone wrong with our code and
+    # need to investigate.
+    logger.info("executing RunTransfer")
+    try:
+        logger.info('executing RunTransfer')
+        run = RunTransfer()
+        run.algorithm()
+    except RucioTransferException as ex:
+        raise ex
+    except Exception as ex:
+        raise Exception("Unexpected error during main") from ex
+    logger.info('transfer completed')
+
+if __name__ == "__main__":
+    main()
