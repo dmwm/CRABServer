fb4b769737d309acc3bbb67fce72d0cc5bec5389
Stefano Belforte <stefano.belforte@cern.ch>
2023-06-30 18:00:25 +0200

    expand user tar files in /srv . Correct fix for #7661

--- scripts/CMSRunAnalysis.py
+++ scripts/CMSRunAnalysis.py
@@ -383,10 +383,17 @@ def parseArgs():
     return opts
 
 def prepSandbox(opts):
-    print("==== Sandbox untarring STARTING at %s ====" % time.asctime(time.gmtime()))
+    print("==== Sandbox untarring in job's top dir STARTING at %s ====" % time.asctime(time.gmtime()))
 
     #The user sandbox.tar.gz has to be unpacked no matter what (even in DEBUG mode)
     print(subprocess.getoutput('tar xfm %s' % opts.archiveJob))
+    # if the sandbox contains tar files, expand them
+    files = subprocess.getoutput(f"tar tf {opts.archiveJob}").split('\n')
+    for file in files:
+        if ('.tar.' in file) or file.endswith('.tar') or\
+                file.endswith('.tgz') or file.endswith('.tbz'):
+            print(subprocess.getoutput(f"tar xvfm {file}"))
+
     print("==== Sandbox untarring FINISHED at %s ====" % time.asctime(time.gmtime()))
 
     #move the pset in the right place
@@ -413,12 +420,6 @@ def extractUserSandbox(archiveJob, cmsswVersion):
     print(subprocess.getoutput('tar xfm %s ' % os.path.join('..', archiveJob)))
     os.rename('PSet.py', '../PSet.py')
     os.rename('PSet.pkl', '../PSet.pkl')
-    # if the sandbox contains tar files, expand them
-    files = subprocess.getoutput(f"tar tf ../{archiveJob}").split('\n')
-    for file in files:
-        if ('.tar.' in file) or file.endswith('.tar') or\
-                file.endswith('.tgz') or file.endswith('.tbz'):
-            print(subprocess.getoutput(f"tar xfm {file}"))
     os.chdir('..')
 
 def getProv(filename, scram):
