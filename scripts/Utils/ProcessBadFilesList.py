d82c37fd885cff8cbcda031a8c2df8ccdc92823c
Stefano Belforte <stefano.belforte@cern.ch>
2023-09-18 22:50:24 +0200

    initial implementation for #7883

--- scripts/Utils/ProcessBadFilesList.py
+++ scripts/Utils/ProcessBadFilesList.py
@@ -5,8 +5,9 @@ ref. https://github.com/dmwm/CRABServer/issues/7548#issuecomment-1714567989
 """
 import os
 import json
-import pprint
+import datetime
 import shutil
+
 
 ERROR_KINDS = [
     'not a ROOT file',
@@ -18,7 +19,7 @@ def main():
     """" """
     # location of files prepared by RetryJobs
     topDir = '/eos/cms/store/temp/user'
-    topDir = '.'  # `for testing locally
+    #topDir = '.'  # `for testing locally
     # topDir = topDir + '/filechecker'  # currently not used`
 
     for subDir in ['corrupted']:  # maybe later on add suspicious files, initially ignore them
@@ -33,13 +34,52 @@ def main():
             result = parse(filePath)
             accumulate(totals, result)
             doneFiles.append(filePath)
-        pprint.pprint(totals)
+        print(totals)
 
         for file in doneFiles:
             shutil.move(file, doneFilesDir)
 
-    # now that we have the totals for this run of the script, update the
-    # daily tally
+        # now that we have the totals for this run of the script, update the
+        # daily tally
+        addToDailyLog(logDir=myDir, totals=totals)
+
+def addToDailyLog(logDir=None, totals=None):
+    """
+    dir is the directory where to place the daily Log in JSON format
+    totals is a dictionary in the format prepared by accumulate()
+    """
+    if not totals:
+        return
+    today = datetime.date.today().strftime('%y%m%d')  # YYMMDD format
+    dailyLog = f"{logDir}/{today}-log.json"
+    if not os.path.exists(dailyLog):
+        logs = {}
+    else:
+        with open(dailyLog, 'r', encoding='utf-8') as fp:
+            logs = json.load(fp)
+            # convert lists to sets to collect unique fields
+            for did in logs.keys():
+                logs[did]['RSEs'] = set(logs[did]['RSEs'])
+                logs[did]['kind'] = set(logs[did]['kind'])
+    for did, info in totals.items():
+        if did in logs:
+            # add to existing info
+            logs[did]['errors'] += info['errors']
+            logs[did]['RSEs'] |= info['RSEs']  # set union
+            logs[did]['kind'] |= info['kind']
+        else:
+            # create new entry
+            logs[did] = totals[did]
+    # persist new log
+    # need to convert set to lists to put in JSON
+    for did in logs.keys():
+        logs[did]['RSEs'] = list(logs[did]['RSEs'])
+        logs[did]['kind'] = list(logs[did]['kind'])
+    newLog = dailyLog + '.NEW'
+    with open(newLog, 'w' , encoding='utf-8') as fp:
+        json.dump(logs, fp)
+    shutil.move(newLog, dailyLog)
+
 
 def parse(file=None):
     """
@@ -49,7 +89,7 @@ def parse(file=None):
     with open(file, 'r', encoding='utf8') as fp:
         info = json.load(fp)
     result = {}
-    result['errorKind'] = None
+    result['errorKind'] = ''
     result['DID'] = info['DID']
     result['RSE'] = info['RSE']
     errorLine = info['message'][1]
@@ -61,17 +101,21 @@ def parse(file=None):
 
 def accumulate(totals=None, result=None):
     """
-    totals is a dictionary with a single key (the DID) pointing to a dictionary of into
-    {'DID': { 'numnerOfErrors': count of how many tims a job failed on this
-               'RSEs' : [ a list (set) of the RSE's were corruption was detected (sites where jobs ran) ]
+    totals is a dictionary with a single key (the DID) pointing to a dictionary of info
+    {'DID': { 'numberOfErrors': count of how many tims a job failed on this
+              'RSEs' : ( a set listing the RSE's were corruption was detected (sites where jobs ran) ]
+              'kind' : ( a set listing the error kinds found for this did, hopefully just 1 !! )
             }, ... }
     """
     did = result['DID']
     rse = result['RSE']
-    if not did in totals:
-        totals[did] = {'errors': 0, 'RSEs': set()}
+    kind = result['errorKind']
+    print("kind= ", kind)
+    if did not in totals:
+        totals[did] = {'errors': 0, 'RSEs': set(), 'kind': set()}
     totals[did]['errors'] += 1
     totals[did]['RSEs'].add(rse)
-
+    totals[did]['kind'].add(kind)
+    print(totals[did]['kind'])
 
 main()
