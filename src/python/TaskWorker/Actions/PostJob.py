951e4bf7e34dd0a2e79fe95b37424cd3b8e42112
Thanayut Seethongchuen <=>
2023-11-16 16:28:39 +0100

    add 'type' back to transfers dict for retry job

--- src/python/TaskWorker/Actions/PostJob.py
+++ src/python/TaskWorker/Actions/PostJob.py
@@ -956,6 +956,13 @@ class ASOServerJob(object):
                 msg += " Transfer submission failed."
                 msg += "\n%s" % (str(hte.headers))
                 returnMsg['error'] = msg
+            # add/remove some keys from newDoc to has the same schema as `if not self.found_doc_in_db` is True.
+            # add
+            newDoc['type'] = doc['type']
+            # remove
+            newDoc.pop('subresource', None)
+            newDoc.pop('transfer_retry_count', None) # we never use this, even in FTS ASO
+
             # Previous post resets asoworker to NULL. This is not good, so we set it again
             # using a different API to update the transfersDB record
             updateDoc = {}
