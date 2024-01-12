733f1206bea56e8b47f7a5839545c344c2b5adec
Stefano Belforte <stefano.belforte@cern.ch>
2024-01-12 15:00:41 +0100

    Update TaskSubmissionStatusTracking.sh

--- test/container/testingScripts/TaskSubmissionStatusTracking.sh
+++ test/container/testingScripts/TaskSubmissionStatusTracking.sh
@@ -6,4 +6,4 @@ python_cmd="python3"
 CMSSW_Major=$(echo $CMSSW_VERSION | cut -d_ -f2)  # e.g. from CMSSW_13_1_x this is "13"
 [ $CMSSW_Major -lt 12 ] && python_cmd="python2"
 
-exec $python_cmd ${WORK_DIR}/CRABServer/test/container/testinScripts/statusTracking.py
+exec $python_cmd ${WORK_DIR}/CRABServer/test/container/testingScripts/statusTracking.py
