f8d412d3d925c079900abcc9257f991f1ebee2e7
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-02-16 11:51:19 +0100

    fix Stefano's comments
    - `lib/python/site-packages` instead of `lib/python/site-packages`.
       Still need to be this structure because of data files discovery
       in CRAB REST image
    - use cut instead of bash array

--- cicd/crabtaskworker_pypi/start.sh
+++ cicd/crabtaskworker_pypi/start.sh
@@ -4,7 +4,7 @@
 TASKWORKER_HOME=/data/srv/TaskManager
 CONFIG=$TASKWORKER_HOME/cfg/TaskWorkerConfig.py
 # CRABTASKWORKER_ROOT is a mandatory variable. Use for getting data directory in `DagmanCreator.getLocation()`
-export CRABTASKWORKER_ROOT=/data/srv/current/lib/python3.8/site-packages/
+export CRABTASKWORKER_ROOT=/data/srv/current/lib/python/site-packages/
 # export PYTHONPATH of app
 export PYTHONPATH=$CRABTASKWORKER_ROOT:$PYTHONPATH
 python3 $CRABTASKWORKER_ROOT/TaskWorker/MasterWorker.py --config ${CONFIG} --logDebug &
