b21bd3625257ea8806b561d70c4e78dd5ed9b145
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-02-29 11:17:52 +0100

    refuse to start when data_files change for taskworker

--- cicd/crabtaskworker_pypi/start.sh
+++ cicd/crabtaskworker_pypi/start.sh
@@ -24,16 +24,23 @@ shift $((OPTIND-1))
 if ! [[ -v MODE ]]; then
   echo "Please set how you want to start crabserver (add -c or -g option)." && helpFunction
 fi
-
+markmodify_path=/data/srv/current/data_files_modified
 case $MODE in
     current)
         # current mode: run current instance
+        # Refuse to start when data_files directry is modified.
+        if [[ -f ${markmodify_path} ]]; then
+            echo "data_files directory has been modifed."
+            echo "Refuse to start with -c option."
+            exit 1
+        fi
         APP_PATH=/data/srv/current/lib/python/site-packages
         ;;
     fromGH)
         # private mode: run private instance from GH
         APP_PATH=/data/repos/CRABServer/src/python:/data/repos/WMCore/src/python
         # update runtime (create TaskManagerRun.tar.gz from source)
+        touch ${markmodify_path}
         ./new_updateTMRuntime.sh
         ;;
     *) echo "Unimplemented mode: $MODE\n"; helpFunction ;;
