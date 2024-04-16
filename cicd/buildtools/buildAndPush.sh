dd69d508a00dd4414876dd6459a30586ffeff64e
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-04-16 20:42:18 +0200

    fix typo in tag name

--- cicd/buildtools/buildAndPush.sh
+++ cicd/buildtools/buildAndPush.sh
@@ -8,8 +8,8 @@ pushd "${SCRIPT_DIR}"
 # also tag today, as backup
 # assign today something like 240101
 today=$(date '+%g%m%d')
-docker build -t registry.cern.ch/cmscrab/buildtools:latast -f Dockerfile .
-docker tag registry.cern.ch/cmscrab/buildtools:latast registry.cern.ch/cmscrab/buildtools:"${today}"
+docker build -t registry.cern.ch/cmscrab/buildtools:latest -f Dockerfile .
+docker tag registry.cern.ch/cmscrab/buildtools:latest registry.cern.ch/cmscrab/buildtools:"${today}"
 docker push registry.cern.ch/cmscrab/buildtools:latest
 docker push registry.cern.ch/cmscrab/buildtools:"${today}"
 
