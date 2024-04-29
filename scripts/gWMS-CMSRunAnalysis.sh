d6e742cc67097e8817b6f6bbf2e9782098d2bb4c
Stefano Belforte <stefano.belforte@cern.ch>
2024-04-29 18:18:05 +0200

    use hyphen (-) as separator in FRONTIER_ID

--- scripts/gWMS-CMSRunAnalysis.sh
+++ scripts/gWMS-CMSRunAnalysis.sh
@@ -195,7 +195,7 @@ then
     export CRAB_Retry=`grep '^CRAB_Retry =' $_CONDOR_JOB_AD | tail -1 | tr -d '"' | awk '{print $NF;}'`
     export CRAB_Reqname=`grep '^CRAB_Reqname =' $_CONDOR_JOB_AD | tail -1 | tr -d '"' | awk '{print $NF;}'`
     # let Frontier know where access will come from
-    export FRONTIER_ID="CRAB $CRAB_Reqname"
+    export FRONTIER_ID="CRAB-${CRAB_Reqname}"
     JOB_CMSSite=`grep '^JOB_CMSSite =' $_CONDOR_JOB_AD | tr -d '"' | tail -1 | awk '{print $NF;}'`
     if [ "X$CRAB_Id" = "X" ];
     then
