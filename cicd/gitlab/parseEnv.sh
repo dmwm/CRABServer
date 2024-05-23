8fa68f53e04b3ea63abb92503f38a77fea38d0c6
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-05-23 08:39:03 +0200

    fix RELEASE_NAME

--- cicd/gitlab/parseEnv.sh
+++ cicd/gitlab/parseEnv.sh
@@ -4,8 +4,7 @@
 # - If match regexp `^pypi-(<env1>|<env2>|...)-.*`, set ENV_NAME to the string
 #   in group. Valide env are preprod/test2/test11/test2
 #   - Allow override ENV_NAME (from push option or WebUI)
-# - If match release tag (e.g., v3.240501), set ENV_NAME to preprod, canoverrid
-#   - Allow override RELEASE_NAME (from push option or WebUI)
+# - If match release tag (e.g., v3.240501), set ENV_NAME to preprod.
 
 set -euo pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -19,16 +18,13 @@ if [[ $TAG =~ $VALIDATE_DEV_TAG ]]; then # Do not quote regexp variable here
     IFS='-' read -ra TMPSTR <<< "${TAG}"
     ENV_NAME=${ENV_NAME:-${TMPSTR[1]}}
 elif [[ $TAG =~ $VALIDATE_RELEASE_TAG ]]; then
-    ENV_NAME=${ENV_NAME:-test12}
-    RELEASE_NAME="${RELEASE_NAME:-}-stable"
+    ENV_NAME=${ENV_NAME:-preprod}
 else
     >&2 echo "fail to parse env from string: $TAG"
     exit 1
 fi
 
 echo "Use env: ${ENV_NAME}"
-echo "Release tag: ${RELEASE_NAME:-}"
 cp "${SCRIPT_DIR}/env/${ENV_NAME}" .env
-echo "RELEASE_NAME=${RELEASE_NAME:-}" >> .env
 echo ".env content:"
 cat .env
