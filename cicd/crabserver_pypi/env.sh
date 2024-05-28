5007661c56953306460ed603ee26dc73a52dae34
Dario Mapelli <mapelli.dario@gmail.com>
2024-05-28 10:34:39 +0200

    pypi - TW/Pub and crabserver - added env.sh script

---
+++ cicd/crabserver_pypi/env.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+helpFunction() {
+    echo -e "Usage example: ./start.sh -c | -g [-d]"
+    echo -e "\t-c start current crabserver instance"
+    echo -e "\t-g start crabserver instance from GitHub repo"
+    echo -e "\t-d start crabserver in debug mode. Option can be combined with -c or -g"
+    exit 1
+}
+
+DEBUG=''
+MODE=''
+while getopts ":dDcCgGhH" o; do
+    case "${o}" in
+        h|H) helpFunction ;;
+        g|G) MODE="fromGH" ;;
+        c|C) MODE="current" ;;
+        d|D) DEBUG=true ;;
+        * ) echo "Unimplemented option: -$OPTARG"; helpFunction ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if ! [[ -v MODE ]]; then
+  echo "Please set how you want to start crabserver (add -c or -g option)." && helpFunction
+fi
+
+case $MODE in
+    current)
+        # current mode: run current instance
+        PYTHONPATH=/data/srv/current/lib/python/site-packages:${PYTHONPATH:-}
+        ;;
+    fromGH)
+        # private mode: run private instance from GH
+        PYTHONPATH=/data/repos/WMCore/src/python:/data/repos/CRABServer/src/python:${PYTHONPATH:-}
+        ;;
+    *) echo "Unimplemented mode: $MODE\n"; helpFunction ;;
+esac
+
+# passing DEBUG and PYTHONPATH  to ./manage.sh scripts
+export DEBUG
+export PYTHONPATH
+
--- cicd/crabtaskworker_pypi/Publisher/start.sh
+++ cicd/crabtaskworker_pypi/Publisher/start.sh
@@ -8,39 +8,6 @@
 set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-helpFunction() {
-    grep "^##H" "${0}" | sed -r "s/##H(| )//g"
-}
+source ${SCRIPT_DIR}/env.sh "$@"
 
-DEBUG=''
-MODE=''
-while getopts ":dDcCgGhH" o; do
-    case "${o}" in
-        h|H) helpFunction ;;
-        g|G) MODE="fromGH" ;;
-        c|C) MODE="current" ;;
-        d|D) DEBUG=true ;;
-        * ) echo "Unimplemented option: -$OPTARG"; helpFunction; exit 1 ;;
-    esac
-done
-shift $((OPTIND-1))
-
-if ! [[ -v MODE ]]; then
-  echo "Please set how you want to start crabserver (add -c or -g option)." && helpFunction
-fi
-case $MODE in
-    current)
-        # current mode: run current instance
-        PYTHONPATH=/data/srv/current/lib/python/site-packages:${PYTHONPATH:-}
-        ;;
-    fromGH)
-        # private mode: run private instance from GH
-        PYTHONPATH=/data/repos/CRABServer/src/python:/data/repos/WMCore/src/python:${PYTHONPATH:-}
-        ;;
-    *) echo "Unimplemented mode: $MODE"; exit 1;;
-esac
-
-# export APP_PATH and DEBUG to ./manage.sh
-export PYTHONPATH
-export DEBUG
 "${SCRIPT_DIR}/manage.sh" start
