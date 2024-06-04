1a30cdd2c3d70a662436c437c48f4b05c65974b6
Stefano Belforte <stefano.belforte@cern.ch>
2024-06-04 15:23:09 +0200

    run Jenkins tests on crab-sched-903

--- test/statusTrackingTasks/HC-splitByLumi.py
+++ test/statusTrackingTasks/HC-splitByLumi.py
@@ -36,6 +36,6 @@ config.Site.blacklist = ['T2_ES_IFCA']
 config.Site.storageSite = 'T2_CH_CERN'
  
 config.section_("Debug")
-config.Debug.scheddName = 'crab3@crab-sched-901.cern.ch'
+config.Debug.scheddName = 'crab3@crab-sched-903.cern.ch'
 
 
