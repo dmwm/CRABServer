4601c138b6cd377b6f5e6f6662b713e131ef16ec
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-06-19 12:50:09 +0200

    fix FORCE_RELEASE

--- cicd/gitlab/parseEnv.sh
+++ cicd/gitlab/parseEnv.sh
@@ -3,8 +3,8 @@
 # Parse deployment env from tag.
 # - If match regexp `^pypi-(<env1>|<env2>|...)-.*`, set ENV_NAME to the string
 #   in group. Valide env are preprod/test2/test11/test2
-#   - Allow override ENV_NAME (from push option or WebUI)
 # - If match release tag (e.g., v3.240501), set ENV_NAME to preprod.
+# Also allow override ENV_NAME (from push option or WebUI)
 
 set -euo pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -12,12 +12,12 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TAG="${1}"
 
 # validate tag
-VALIDATE_DEV_TAG='^pypi-(preprod|test2|test11|test12)-.*'
-VALIDATE_RELEASE_TAG='^v3\.[0-9]{6}.*'
-if [[ $TAG =~ $VALIDATE_DEV_TAG ]]; then # Do not quote regexp variable here
+REGEX_DEV_TAG='^pypi-(preprod|test2|test11|test12)-.*'
+REGEX_RELEASE_TAG='^v3\.[0-9]{6}.*'
+if [[ $TAG =~ $REGEX_DEV_TAG ]]; then # Do not quote regexp variable here
     IFS='-' read -ra TMPSTR <<< "${TAG}"
     ENV_NAME=${ENV_NAME:-${TMPSTR[1]}}
-elif [[ $TAG =~ $VALIDATE_RELEASE_TAG ]]; then
+elif [[ $TAG =~ $REGEX_RELEASE_TAG ]]; then
     ENV_NAME=${ENV_NAME:-preprod}
 else
     >&2 echo "fail to parse env from string: $TAG"
