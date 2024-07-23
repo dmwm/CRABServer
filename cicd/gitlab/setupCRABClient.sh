c0083fd0b151978c78fab7c25750fadce2eefa5d
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-07-23 13:30:58 +0200

    [CI] support CRABClient GH
    Note that ServerUtility.py always come from commit of triggered pipeline

--- cicd/gitlab/setupCRABClient.sh
+++ cicd/gitlab/setupCRABClient.sh
@@ -6,10 +6,10 @@ set -euo pipefail
 
 #Script can be used to setup CRABClient:
 # 1. dev - CRABClient from Intergration Build (IB);
-# 2. GH - CRABClient from CRABClient GH repository master branch. This option requires to set which CRABServer tag to use;
+# 2. GH - CRABClient from CRABClient GH repository master branch.
 # 3. prod - production CRABClient from cvmfs;
-# Variables ${SCRAM_ARCH}, ${CMSSW_release}, ${CRABServer_tag}, ${CRABClient_version}
-# comes from Jenkins job CRABServer_ExecuteTests configuration.
+# Variables ${SCRAM_ARCH}, ${CMSSW_release}, ${CRABClient_version}
+# comes from caller/CI variables
 
 # ignore all bash fail from script from /cvmfs/cms-ib.cern.ch
 set +euo pipefail
@@ -37,51 +37,36 @@ if echo ${CMSSW_release} | grep -q CMSSW_7; then
   fi
 fi
 
-#cd ${WORK_DIR}
-#[ ! -d 'CRABServer' ] && git clone git@github.com:dmwm/CRABServer
-
 case $CRABClient_version in
-  #dev)
-  #  source /cvmfs/cms-ib.cern.ch/latest/common/crab-setup.sh dev
-  #  alias crab='crab-dev'
-  #  ;;
-  #GH)
-  #  set -euo pipefail
-  #  #cd CRABServer; git checkout ${CRABServer_tag}; cd ..
-  #  rm -rf CRABClient
-  #  git clone https://github.com/${CRABCLIENT_FORK}/CRABClient CRABClient -b $CRABCLIENT_TAG
-  #  cp src/python/ServerUtilities.py CRABClient/src/python/
-  #  cp src/python/RESTInteractions.py CRABClient/src/python/
-  #  #$ghprbPullId is used for PR testing. If this variable is set, that means
-  #  #we need to run test against specific commit
-  #  #if [ ! -z "$ghprbPullId" ]; then
-  #  #	cd CRABClient
-  #  #	git fetch origin pull/${ghprbPullId}/merge:PR_MERGE
-  #  #	export COMMIT=`git rev-parse "PR_MERGE^{commit}"`
-  #  #	git checkout -f ${COMMIT}
-  #  #	cd ..
-  #  #fi
-  #  cd ${WORK_DIR}
-  #  GitDir=${WORK_DIR}
-  #
-  #  MY_CRAB=${GitDir}/CRABClient
-  #
-  #  # install the fake WMCore dependency for CRABClient, taking inspiration from
-  #  # https://github.com/cms-sw/cmsdist/blob/b38a4b3339f12706513917153a2ec6cdcb23741c/crab-build.file#L37
-  #  mkdir -p ${GitDir}/WMCore/src/python/WMCore
-  #  touch ${GitDir}/WMCore/src/python/__init__.py
-  #  touch ${GitDir}/WMCore/src/python/WMCore/__init__.py
-  #  cp ${GitDir}/CRABClient/src/python/CRABClient/WMCoreConfiguration.py ${GitDir}/WMCore/src/python/WMCore/Configuration.py
-  #
-  #  export PYTHONPATH=${MY_CRAB}/src/python:${PYTHONPATH:-}
-  #  export PYTHONPATH=${GitDir}/WMCore/src/python:$PYTHONPATH
-  #
-  #  export PATH=${MY_CRAB}/bin:$PATH
-  #  set +euo pipefail
-  #  source ${MY_CRAB}/etc/crab-bash-completion.sh
-  #  ;;
+  dev)
+    set +euo pipefail
+    source /cvmfs/cms-ib.cern.ch/latest/common/crab-setup.sh dev
+    set -euo pipefail
+    alias crab='crab-dev'
+    ;;
+  GH)
+    # TODO: specific fork/commit of crabclient repo still not support
+    MY_CRAB=${PWD}/CRABClient
+    rm -rf CRABClient
+    git clone https://github.com/dmwm/CRABClient ${MY_CRAB} -b master
+    cp ${ROOT_DIR}/src/python/ServerUtilities.py ${MY_CRAB}/src/python/
+    cp ${ROOT_DIR}/src/python/RESTInteractions.py ${MY_CRAB}/src/python/
+    # install the fake WMCore dependency for CRABClient, taking inspiration from
+    # https://github.com/cms-sw/cmsdist/blob/b38a4b3339f12706513917153a2ec6cdcb23741c/crab-build.file#L37
+    mkdir -p ${MY_CRAB}/WMCore/src/python/WMCore
+    touch ${MY_CRAB}/WMCore/src/python/__init__.py
+    touch ${MY_CRAB}/WMCore/src/python/WMCore/__init__.py
+    cp ${MY_CRAB}/src/python/CRABClient/WMCoreConfiguration.py ${MY_CRAB}/WMCore/src/python/WMCore/Configuration.py
+
+    export PYTHONPATH=${MY_CRAB}/src/python:${PYTHONPATH:-}
+    export PYTHONPATH=${MY_CRAB}/WMCore/src/python:$PYTHONPATH
+    export PATH=${MY_CRAB}/bin:$PATH
+    source ${MY_CRAB}/etc/crab-bash-completion.sh
+    ;;
   prod)
+    set +euo pipefail
 	source /cvmfs/cms.cern.ch/common/crab-setup.sh prod
+    set -euo pipefail
 esac
 
 #cd "${CURRENT_DIR}"
