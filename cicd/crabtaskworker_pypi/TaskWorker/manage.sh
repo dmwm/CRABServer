9633efbc43b6aba3ea0a7055cf16a6b1dd3a6096
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-07-29 18:52:24 +0200

    first version

--- cicd/crabtaskworker_pypi/TaskWorker/manage.sh
+++ cicd/crabtaskworker_pypi/TaskWorker/manage.sh
@@ -2,20 +2,6 @@
 
 # Start the TaskWorker service.
 
-##H Usage: manage.sh start -c/-g -d
-##H
-##H Available actions:
-##H   help        show this help
-##H   version     get current version of the service
-##H   restart     (re)start the service
-##H   start       (re)start the service
-##H   stop        stop the service
-##H   status      show pid
-##H   env         eval
-##H
-##H This script needs following environment variables for start action:
-##H   - DEBUG:      if `true`, setup debug mode environment.
-##H   - PYTHONPATH: inherit from ./start.sh
 
 set -euo pipefail
 if [[ -n ${TRACE+x} ]]; then
@@ -24,30 +10,31 @@ if [[ -n ${TRACE+x} ]]; then
 fi
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-MODE="current"
-DEBUG="f"
-ACTION=""
+source $SCRIPT_DIR/source.sh
 
-while [[ $OPTIND -le "$#" ]]; do
-    if getopts ":hgcd" opt; then
-        if [[ ${ACTION} != "start" ]]; then
-            helpFunction; exit 1
-        fi
+ACTION=""
+MODE=""
+DEBUG="f"
+
+# first args always ACTION
+ACTION=${!OPTIND}
+((OPTIND++))
+if [[ $ACTION = "start" ]]; then
+    while getopts ":hgcd" opt; do
         case "${opt}" in
-            h )
-                helpFunction; exit 0
-                ;;
             g )
                 if [[ -n "$MODE" ]]; then
-                   helpFunction
-                   exit 1
+                    echo "Error: only one mode support"
+                    helpFunction
+                    exit 1
                 fi
                 MODE="fromGH"
                 ;;
             c )
                 if [[ -n "$MODE" ]]; then
-                   helpFunction
-                   exit 1
+                    echo "Error: only one mode support"
+                    helpFunction
+                    exit 1
                 fi
                 MODE="current"
                 ;;
@@ -59,14 +46,12 @@ while [[ $OPTIND -le "$#" ]]; do
                 exit 1
                 ;;
         esac
-    elif [[ -n $ACTION ]]; then
-         helpFunction; exit 1
-    else
-        ACTION="${!OPTIND}"
-        ((OPTIND++))
+    done
+    if [[ -z $MODE ]]; then
+        echo "Error: starting mode not are not provided (add -c or -g option)." && helpFunction;
+        exit 1
     fi
-done
-
+fi
 # Main routine, perform action requested on command line.
 case ${ACTION:-help} in
     start | restart )
@@ -84,10 +69,7 @@ case ${ACTION:-help} in
         ;;
 
     env )
-        echo "export CONFIG=$CONFIG"
-        echo "export APP_PATH=$APP_PATH"
-        echo "export PYTHONPATH=$PYTHONPATH"
-        echo "export CRABTASKWORKER_ROOT=$CRABTASKWORKER_ROOT"
+        env_eval
         ;;
     * )
         echo "$0: unknown action '$1', please try '$0 help' or documentation." 1>&2
