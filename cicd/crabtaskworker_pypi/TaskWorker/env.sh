9cc5208831cf2cffb26511594c8039e9d036410f
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-08-06 15:17:51 +0200

    env

--- cicd/crabtaskworker_pypi/TaskWorker/env.sh
+++ cicd/crabtaskworker_pypi/TaskWorker/env.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-set -euo pipefail
-if [[ -n ${TRACE+x} ]]; then
-    set -x
-    export TRACE
-fi
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 eval "$("${SCRIPT_DIR}"/manage.sh env)"
