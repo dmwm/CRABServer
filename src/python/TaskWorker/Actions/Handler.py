1044f49395face9f74dd515c9ec1cbfabe25b997
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-08-09 14:10:21 +0200

    WIP: purge dry run

--- src/python/TaskWorker/Actions/Handler.py
+++ src/python/TaskWorker/Actions/Handler.py
@@ -198,10 +198,7 @@ def handleNewTask(resthost, dbInstance, config, task, procnum, *args, **kwargs):
         handler.addWork(MakeFakeFileSet(config=config, crabserver=crabserver, procnum=procnum))
     handler.addWork(Splitter(config=config, crabserver=crabserver, procnum=procnum))
     handler.addWork(DagmanCreator(config=config, crabserver=crabserver, procnum=procnum, rucioClient=rucioClient))
-    if task['tm_dry_run'] == 'T':
-        handler.addWork(DryRunUploader(config=config, crabserver=crabserver, procnum=procnum))
-    else:
-        handler.addWork(DagmanSubmitter(config=config, crabserver=crabserver, procnum=procnum))
+    handler.addWork(DagmanSubmitter(config=config, crabserver=crabserver, procnum=procnum))
 
     return handler.actionWork(args, kwargs)
 
