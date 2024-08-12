c0315b4624d57ea9b708df16d070ea67b327c7ee
Stefano Belforte <stefano.belforte@cern.ch>
2024-08-12 15:57:02 +0200

    do not use deprecated schedd.transacion. Fix #8597 (#8610)

--- src/python/TaskWorker/Actions/PostJob.py
+++ src/python/TaskWorker/Actions/PostJob.py
@@ -2907,22 +2907,21 @@ class PostJob():
         msg += " ClassAd attributes to set are: %s" % (str(params))
         self.logger.info(msg)
 
-        limit = 5
+        limit = 5  # we retry these actions in case schedd is busy and refuses to edit the job
         counter = 0
         while counter < limit:
             try:
                 counter += 1
                 msg = "attempt %d out of %d" % (counter, limit)
                 self.logger.info("       -----> Started %s -----", msg)
-                with self.schedd.transaction(htcondor.TransactionFlags.NonDurable):
-                    for param in params:
-                        self.schedd.edit([self.dag_jobid], param, str(params[param]))
-                    self.schedd.edit([self.dag_jobid], 'CRAB_PostJobLastUpdate', str(time.time()))
-                    # Once ClassAd state attributes have been updated, let HTCondor remove the job from the queue
-                    self.schedd.edit([self.dag_jobid], "LeaveJobInQueue", classad.ExprTree("false"))
-                    self.logger.info("       -----> Finished %s -----", msg)
-                    self.logger.info("====== Finished to update job ClassAd.")
-                    break
+                for param in params:
+                    self.schedd.edit([self.dag_jobid], param, str(params[param]))
+                self.schedd.edit([self.dag_jobid], 'CRAB_PostJobLastUpdate', str(time.time()))
+                # Once ClassAd state attributes have been updated, let HTCondor remove the job from the queue
+                self.schedd.edit([self.dag_jobid], "LeaveJobInQueue", classad.ExprTree("false"))
+                self.logger.info("       -----> Finished %s -----", msg)
+                self.logger.info("====== Finished to update job ClassAd.")
+                break
             except Exception:
                 self.logger.exception("Exception in setting job ClassAd attributes:")
 
