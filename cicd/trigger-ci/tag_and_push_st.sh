9a553787c5cd30945d3ff6711d771d79cf0c6dcb
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-08-23 10:13:03 +0200

    change ci variables to trigger pipeline and add examples
    also remove RELEASE_NAME variable to make pipeline simpler

---
+++ cicd/trigger-ci/tag_and_push_st.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -euo pipefail
+
+ENV=test2
+# if ./env  file exist, source it
+[[ -f ./env ]] && source ./env
+echo ${ENV}
+TAG=pypi-${ENV}-$(date +"%s")
+git tag $TAG
+git push gitlab $TAG -o ci.variable="SUBMIT_STATUS_TRACKING=t"
