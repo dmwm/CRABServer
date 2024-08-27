432a4e3ed3477623d4f88a9c4247e491cf8f3ce7
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-08-27 12:13:32 +0200

    rename FORCE_RELEASE to ONLY_BUILD_RELEASE

--- cicd/trigger-ci/tag_and_push_only_build_release.sh
+++ cicd/trigger-ci/tag_and_push_only_build_release.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 TAG=v3.240000.test-$(date +"%s")
 git tag $TAG
 #git push gitlab $TAG
-git push gitlab $TAG -o ci.variable="FORCE_RELEASE=t"
+git push gitlab $TAG -o ci.variable="ONLY_BUILD_RELEASE=t"
