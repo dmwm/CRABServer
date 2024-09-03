de3c9b76f2318e0483252215374f0a0e3a67d109
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-09-03 13:19:50 +0200

    grammar

--- cicd/crabtaskworker_pypi/TaskWorker/manage.sh
+++ cicd/crabtaskworker_pypi/TaskWorker/manage.sh
@@ -11,7 +11,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # sanity check
 if [[ -z ${COMMAND+x} || -z ${MODE+x} || -z ${DEBUG+x} || -z ${SERVICE+x} ]]; then
-    >&2 echo "All envvars are not set!."
+    >&2 echo "Error: Not all envvars are set!"
     exit 1
 fi
 
