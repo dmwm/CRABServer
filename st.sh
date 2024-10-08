9e5c0f5591d0e5fb76688ca788ff82d5f1becce5
Vijay Chakravarty <vijaychakravarty9@gmail.com>
2024-10-08 09:19:11 +0200

    Delete st.sh

--- st.sh
+++
@@ -1,7 +0,0 @@
-#source .env
-export X509_USER_PROXY=/tmp/x509up_u150799
-export CRABClient_version=prod  # from .env
-export REST_Instance=test1  # from .env
-export CMSSW_release=CMSSW_13_0_2
-export Client_Validation_Suite=true
-bash -x cicd/gitlab/clientValidation.sh
