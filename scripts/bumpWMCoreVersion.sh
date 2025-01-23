f608ab39cfb2fb9e59809698da28600731fa2a56
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2025-01-23 14:48:29 +0100

    add bumping WMCore version script.

---
+++ scripts/bumpWMCoreVersion.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+# How to use script:
+#  - bumping to latest version > ./scripts/bumpWMCoreVersion.sh
+#  - bumping to specific version > ./scripts/bumpWMCoreVersion.sh 2.3.7.6
+
+if [ -z "$1" ]
+  then
+    WMCORE_RELEASE_TAG=$(curl https://api.github.com/repos/dmwm/wmcore/releases/latest -s | jq .tag_name -r)
+    echo -e "The latest WMCore stable release is $WMCORE_RELEASE_TAG\n"
+else
+    WMCORE_RELEASE_TAG=$1
+    echo -e "The bumping WMCore version to $WMCORE_RELEASE_TAG\n"
+fi
+
+if [[ "$PWD" =~ CRABServer ]]; then
+  [[ $PWD =~ (.*)/CRABServer.* ]]
+  BASE_WDIR=${BASH_REMATCH[1]}
+  WDIR=${BASE_WDIR}/CRABServer
+
+  sed -i -E "s/\(.*\/dmwm\/WMCore\/blob\/\)\(.*\)\(\/requirements.txt$\)/\1$WMCORE_RELEASE_TAG\3/" ${WDIR}/cicd/crabserver_pypi/requirements.txt
+  sed -i -E "s/\(.*\/dmwm\/WMCore\/blob\/\)\(.*\)\(\/requirements.txt$\)/\1$WMCORE_RELEASE_TAG\3/" ${WDIR}/cicd/crabtaskworker_pypi/requirements.txt
+  sed -i -E "s/\(https:\/\/github.com\/dmwm\/WMCore \)\(.*\)/\1$WMCORE_RELEASE_TAG/" ${WDIR}/cicd/crabserver_pypi/wmcore_requirements.txt
+  git diff ${WDIR}/cicd/crabserver_pypi/requirements.txt \
+    ${WDIR}/cicd/crabtaskworker_pypi/requirements.txt \
+    ${WDIR}/cicd/crabserver_pypi/wmcore_requirements.txt
+  echo -e "\n ^^^ If change made above looks ok then commit changes and bump crabserver tag to force rebuilding image, otherwise:
+  > git restore cicd/crabserver_pypi/requirements.txt cicd/crabserver_pypi/wmcore_requirements.txt cicd/crabtaskworker_pypi/requirements.txt "
+else
+  echo -e "Please run this script in the CRABServer code base directory."
+fi
+
