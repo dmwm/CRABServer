e30d65317c344fd1c3d406b82dadfe4407f361ab
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2025-01-23 18:02:17 +0100

    fixes typos, move to cicd dir instead.

--- cicd/scripts/bumpWMCoreVersion.sh
+++ cicd/scripts/bumpWMCoreVersion.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 # How to use script:
-#  - bumping to latest version > ./scripts/bumpWMCoreVersion.sh
-#  - bumping to specific version > ./scripts/bumpWMCoreVersion.sh 2.3.7.6
+#  - bumping to latest version > ./cicd/scripts/bumpWMCoreVersion.sh
+#  - bumping to specific version > ./cicd/scripts/bumpWMCoreVersion.sh 2.3.7.6
 
 if [ -z "$1" ]
   then
@@ -9,7 +9,7 @@ if [ -z "$1" ]
     echo -e "The latest WMCore stable release is $WMCORE_RELEASE_TAG\n"
 else
     WMCORE_RELEASE_TAG=$1
-    echo -e "The bumping WMCore version to $WMCORE_RELEASE_TAG\n"
+    echo -e "Bumping WMCore version to $WMCORE_RELEASE_TAG\n"
 fi
 
 if [[ "$PWD" =~ CRABServer ]]; then
@@ -17,15 +17,15 @@ if [[ "$PWD" =~ CRABServer ]]; then
   BASE_WDIR=${BASH_REMATCH[1]}
   WDIR=${BASE_WDIR}/CRABServer
 
-  sed -i -E "s/\(.*\/dmwm\/WMCore\/blob\/\)\(.*\)\(\/requirements.txt$\)/\1$WMCORE_RELEASE_TAG\3/" ${WDIR}/cicd/crabserver_pypi/requirements.txt
-  sed -i -E "s/\(.*\/dmwm\/WMCore\/blob\/\)\(.*\)\(\/requirements.txt$\)/\1$WMCORE_RELEASE_TAG\3/" ${WDIR}/cicd/crabtaskworker_pypi/requirements.txt
-  sed -i -E "s/\(https:\/\/github.com\/dmwm\/WMCore \)\(.*\)/\1$WMCORE_RELEASE_TAG/" ${WDIR}/cicd/crabserver_pypi/wmcore_requirements.txt
+  sed -i -e "s/\(.*\/dmwm\/WMCore\/blob\/\)\(.*\)\(\/requirements.txt$\)/\1$WMCORE_RELEASE_TAG\3/" ${WDIR}/cicd/crabserver_pypi/requirements.txt
+  sed -i -e "s/\(.*\/dmwm\/WMCore\/blob\/\)\(.*\)\(\/requirements.txt$\)/\1$WMCORE_RELEASE_TAG\3/" ${WDIR}/cicd/crabtaskworker_pypi/requirements.txt
+  sed -i -e "s/\(https:\/\/github.com\/dmwm\/WMCore \)\(.*\)/\1$WMCORE_RELEASE_TAG/" ${WDIR}/cicd/crabserver_pypi/wmcore_requirements.txt
   git diff ${WDIR}/cicd/crabserver_pypi/requirements.txt \
     ${WDIR}/cicd/crabtaskworker_pypi/requirements.txt \
     ${WDIR}/cicd/crabserver_pypi/wmcore_requirements.txt
   echo -e "\n ^^^ If change made above looks ok then commit changes and bump crabserver tag to force rebuilding image, otherwise:
   > git restore cicd/crabserver_pypi/requirements.txt cicd/crabserver_pypi/wmcore_requirements.txt cicd/crabtaskworker_pypi/requirements.txt "
 else
-  echo -e "Please run this script in the CRABServer code base directory."
+  echo -e "Please run this script inside CRABServer codebase directory."
 fi
 
