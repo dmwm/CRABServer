4d20ac0a5734e4ee85b176a94f417c64ab734373
Vijay Chakravarty <vijaychakravarty9@gmail.com>
2025-04-08 11:48:12 +0200

    Fix #8952

--- scripts/dag_bootstrap_startup.sh
+++ scripts/dag_bootstrap_startup.sh
@@ -175,15 +175,6 @@ else
     exit 1
 fi
 
-# Decide if this task will use enable REUSE flag for FTS ASO jobs
-# Do it here so that decision stays for task lifetime, even if schedd configuratoion
-# is changed at some point
-if [ -f /etc/use_fts_reuse ] ;
-then
-    echo "Found file /etc/use_fts_reuse. Set this task to enable REUSE flag in FTS"
-    touch USE_FTS_REUSE
-fi
-
 export _CONDOR_DAGMAN_LOG=$PWD/$1.dagman.out
 export _CONDOR_DAGMAN_GENERATE_SUBDAG_SUBMITS=False
 export _CONDOR_MAX_DAGMAN_LOG=0
