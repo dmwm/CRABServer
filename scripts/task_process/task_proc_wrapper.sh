658226a08c421da5674e3e97104c0808c4f78d70
Stefano Belforte <stefano.belforte@cern.ch>
2025-04-16 17:02:42 +0200

    make cache_status_new the default, rename previous one to _old

--- scripts/task_process/task_proc_wrapper.sh
+++ scripts/task_process/task_proc_wrapper.sh
@@ -21,8 +21,8 @@ function report_difference {
 function cache_status {
     log "Running cache_status.py"
     python3 task_process/cache_status.py
-    log "Running cache_status_new.py"
-    python3 task_process/cache_status_new.py
+    log "Running cache_status_old.py"
+    python3 task_process/cache_status_old.py
     log "Comparing.."
     compare_status
 }
