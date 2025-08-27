0a511d587ac8a570a1e58b21843bb13f91ebc8bb
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2025-08-28 00:32:09 +0200

    atomic update crabserver image to sidecar repo script, ci-bot for automatic bumping for testing in test14.

---
+++ cicd/scripts/updateCRABServerImageTag.sh
@@ -0,0 +1,33 @@
+set -euo pipefail
+DEPLOY_ENV="${DEPLOY_ENV:?DEPLOY_ENV is required}"
+NEW_IMAGE_TAG="${NEW_IMAGE_TAG:?NEW_IMAGE_TAG is required}"
+DESIRED_IMAGE="${DESIRED_IMAGE:=registry.cern.ch/cmscrab/crabserver:$NEW_IMAGE_TAG}"
+
+UPSTREAM_IAC_REPO_URL="${UPSTREAM_IAC_REPO_URL:=https://github.com/sinonkt/CMSKubernetes}"
+UPSTREAM_IAC_BRANCH="${UPSTREAM_IAC_BRANCH:=migrate-to-argocd}"
+GITLAB_SIDECAR_REPO_URL="${GITLAB_SIDECAR_REPO_URL:=gitlab.cern.ch/kphornsi/crab-iac}"
+GITLAB_SIDECAR_REPO_BRANCH="${GITLAB_SIDECAR_REPO_BRANCH:=master}"
+GITLAB_SIDECAR_REPO_PAT="${GITLAB_SIDECAR_REPO_PAT:?GITLAB_SIDECAR_REPO_PAT is required}"
+CI_BOT_USER_NAME="${CI_BOT_USER_NAME:=crabserver-ci-bot}"
+CI_BOT_USER_EMAIL="${CI_BOT_USER_NAME:=krittin.phornsiricharoenphant@cern.ch}"
+
+LATEST_COMMIT_SHA=$(git ls-remote "$UPSTREAM_IAC_REPO_URL" "$UPSTREAM_IAC_BRANCH" | awk '{print $1}')
+
+WORKDIR="$(mktemp -d)"
+trap 'rm -rf "$WORKDIR"' EXIT
+
+git clone --branch ${GITLAB_SIDECAR_REPO_BRANCH} \
+  "https://oauth2:${GITLAB_SIDECAR_REPO_PAT}@${GITLAB_SIDECAR_REPO_URL}" \
+  $WORKDIR/crab-iac
+
+DESIRED_IMAGE=$DESIRED_IMAGE yq -i '
+  (.spec.template.spec.containers[] | select(.name=="crabserver").image)
+  = strenv(DESIRED_IMAGE)
+' $WORKDIR/crab-iac/argocd/apps/crab/crabserver/overlays/$DEPLOY_ENV/patches/image.yaml
+
+pushd $WORKDIR/crab-iac
+git config user.name  "$CI_BOT_USER_NAME"
+git config user.email "$CI_BOT_USER_EMAIL"
+git commit -am "bump ${DEPLOY_ENV} crabserver image -> ${NEW_IMAGE_TAG}, bump upstream base -> ${LATEST_COMMIT_SHA}"
+git push -u origin $GITLAB_SIDECAR_REPO_BRANCH
+popd
