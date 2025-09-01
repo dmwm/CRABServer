1d234fcb6e859e85e3a5cd693f7545a01c5856d8
Stefano Belforte <stefano.belforte@cern.ch>
2025-09-01 17:17:54 +0200

    useless if. We only use S3 for cache

--- src/python/CRABInterface/RESTUserWorkflow.py
+++ src/python/CRABInterface/RESTUserWorkflow.py
@@ -246,7 +246,7 @@ class RESTUserWorkflow(RESTEntity):
                 validate_real("totalunits", param, safe, optional=True)
             validate_str("cachefilename", param, safe, RX_CACHENAME, optional=False)
             validate_str("debugfilename", param, safe, RX_CACHENAME, optional=True)
-            validate_str("cacheurl", param, safe, RX_CACHEURL, optional=False)
+            validate_str("cacheurl", param, safe, RX_CACHEURL, optional=True)
             validate_str("lfn", param, safe, RX_LFN, optional=True)
             self._checkOutLFN(safe.kwargs, username)
             validate_strlist("addoutputfiles", param, safe, RX_ADDFILE, custom_err="Incorrect 'JobType.outputFiles' parameter. " \
