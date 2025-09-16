7362013e8276eba45a410e8c56a046d1c4b9b099
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2025-09-16 15:47:34 +0200

    add adjust to updateImageTag to helm-style and vendir sync method, omit preprod for now.

--- cicd/scripts/updateCRABServerImageTag.sh
+++ cicd/scripts/updateCRABServerImageTag.sh
@@ -5,7 +5,7 @@ DESIRED_IMAGE="${DESIRED_IMAGE:=registry.cern.ch/cmscrab/crabserver:$NEW_IMAGE_T
 
 UPSTREAM_IAC_REPO_URL="${UPSTREAM_IAC_REPO_URL:=https://github.com/sinonkt/CMSKubernetes}"
 UPSTREAM_IAC_BRANCH="${UPSTREAM_IAC_BRANCH:=migrate-to-argocd}"
-GITLAB_SIDECAR_REPO_URL="${GITLAB_SIDECAR_REPO_URL:=gitlab.cern.ch/crab3/crab-iac-overlays-sidecar}"
+GITLAB_SIDECAR_REPO_URL="${GITLAB_SIDECAR_REPO_URL:=gitlab.cern.ch/crab3/crab-k8s-overlays}"
 GITLAB_SIDECAR_REPO_BRANCH="${GITLAB_SIDECAR_REPO_BRANCH:=master}"
 GITLAB_SIDECAR_REPO_PAT="${GITLAB_SIDECAR_REPO_PAT:?GITLAB_SIDECAR_REPO_PAT is required}"
 CI_BOT_USER_NAME="${CI_BOT_USER_NAME:=crabserver-ci-bot}"
@@ -18,30 +18,23 @@ trap 'rm -rf "$WORKDIR"' EXIT
 
 git clone --branch ${GITLAB_SIDECAR_REPO_BRANCH} \
   "https://oauth2:${GITLAB_SIDECAR_REPO_PAT}@${GITLAB_SIDECAR_REPO_URL}" \
-  $WORKDIR/crab-iac
+  $WORKDIR/crab-k8s-overlays
 
-# Automatically bumped to latest base.
-yq -y -i --arg LATEST_COMMIT_SHA "$LATEST_COMMIT_SHA" '
-  .resources[0] |=
-    ( if test("^https://raw\\.githubusercontent\\.com/")
-      then (split("/") | .[5] = $LATEST_COMMIT_SHA | join("/"))
-      elif test("^https://github\\.com/.+\\.git//")
-      then sub("([?&]ref=)[^&]*"; "?ref=" + $LATEST_COMMIT_SHA)
-      else .
-      end )
-' $WORKDIR/crab-iac/argocd/apps/crab/crabserver/overlays/$DEPLOY_ENV/kustomization.yaml
+# Workaround, automatically bump chart via vendoring until we have clear helm chart registry. then we bumped chart version instead.
+yq -y -i --arg LATEST_COMMIT_SHA "$LATEST_COMMIT_SHA" --arg UPSTREAM_IAC_REPO_URL "$UPSTREAM_IAC_REPO_URL" $'
+  .directories[0].contents[0] 
+  |= ( if has("git") and .git.url == "'"$UPSTREAM_IAC_REPO_URL"'" 
+       then .git.ref = "'"$LATEST_COMMIT_SHA"'" 
+       else . end )
+' $WORKDIR/crab-k8s-overlays/vendir.yml
 
 # Automatically bump image regarding DevOperator desired image.
-yq -i -y --arg DESIRED_IMAGE "$DESIRED_IMAGE" '
-  .spec.template.spec.containers |=
-    map(if .name=="crabserver"
-        then .image = $DESIRED_IMAGE
-        else . end)
-' $WORKDIR/crab-iac/argocd/apps/crab/crabserver/overlays/$DEPLOY_ENV/patches/image.yaml
+yq -i -y --arg DESIRED_IMAGE "$DESIRED_IMAGE" '.image.tag = $DESIRED_IMAGE' $WORKDIR/crab-k8s-overlays/helm/crabserver/values-$DEPLOY_ENV.yaml
 
-pushd $WORKDIR/crab-iac
+pushd $WORKDIR/crab-k8s-overlays
+vendir sync
 git config user.name  "$CI_BOT_USER_NAME"
 git config user.email "$CI_BOT_USER_EMAIL"
-git commit -am "bumped ${DEPLOY_ENV}'s {crabserver:${NEW_IMAGE_TAG}|base:${LATEST_COMMIT_SHA}}"
+git commit -am "bumped ${DEPLOY_ENV}'s crabserver:${NEW_IMAGE_TAG}, chart:${LATEST_COMMIT_SHA}"
 git push -u origin $GITLAB_SIDECAR_REPO_BRANCH
 popd
