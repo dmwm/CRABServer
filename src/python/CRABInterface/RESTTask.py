000f7b7f7517fc001aad70e3eec3fdd42ad287f2
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2025-12-02 15:29:55 +0700

    remove deps on centralconfig for retrieving Schedd's proxied url.

--- src/python/CRABInterface/RESTTask.py
+++ src/python/CRABInterface/RESTTask.py
@@ -233,28 +233,20 @@ class RESTTask(RESTEntity):
             yield "None"
             return
 
-        #=============================================================================
-        # scheddObj is a dictionary composed like this (see the value of htcondorSchedds):
-        # "htcondorSchedds": {
-        #  "crab3-5@vocms059.cern.ch": {
-        #      "proxiedurl": "https://cmsweb.cern.ch/scheddmon/5"
-        #  },
-        #  ...
-        # }
-        # so that they have a "proxied URL" to be used in case the schedd is
-        # behind a firewall.
-        #=============================================================================
-        scheddsObj = self.centralcfg.centralconfig['backend-urls'].get('htcondorSchedds', {})
-        self.logger.info("ScheddObj for task %s is: %s\nSchedd used for submission %s", workflow, scheddsObj, row.schedd)
-        #be careful that htcondorSchedds could be a list (backward compatibility). We might want to remove this in the future
-        if row.schedd in list(scheddsObj) and isinstance(scheddsObj, dict):
-            self.logger.debug("Found schedd %s", row.schedd)
-            proxiedurlbase = scheddsObj[row.schedd].get('proxiedurl')
-            self.logger.debug("Proxied url base is %s", proxiedurlbase)
-            if proxiedurlbase:
-                yield proxiedurlbase + suffix
-        else:
-            self.logger.info("Could not determine proxied url for task %s", workflow)
+        try:
+            # Examples:
+            # row.schedd values: crab3-5@vocms059.cern.ch, crab3@crab-preprod-scd03.cern.ch
+            # valid proxiedUrls: https://cmsweb.cern.ch/scheddmon/059, https://cmsweb.cern.ch/scheddmon/crab-preprod-scd03
+            matched = re.match(r"^[^@]*@([^.]+)\.cern\.ch$", row.schedd)
+            if not matched:
+                raise ValueError("ScheddID is not in a valid pattern.")
+            normalizedScheddId = matched.group(1).replace("vocms", "")
+            proxiedUrlBase="https://cmsweb.cern.ch/scheddmon"
+            proxiedUrl=f"{proxiedUrlBase}/{normalizedScheddId}{suffix}"
+            self.logger.info("ProxiedUrl for task %s is: %s\nSchedd used for submission %s", workflow, proxiedUrl, row.schedd)
+            yield proxiedUrl
+        except Exception:
+            self.logger.info("Could not determine ProxiedUrl for task %s", workflow)
 
     def counttasksbystatus(self, **kwargs):
         """Retrieves all jobs of the specified user with the specified status
