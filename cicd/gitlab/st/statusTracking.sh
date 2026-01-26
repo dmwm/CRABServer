e25c6a4f406d41bb5424657af5c2799bc110245e
Vijay Chakravarty <vijaychakravarty9@gmail.com>
2026-01-26 14:14:11 +0100

    try with python2 vs 3

--- cicd/gitlab/st/statusTracking.sh
+++ cicd/gitlab/st/statusTracking.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "(DEBUG) X509_USER_PROXY=${X509_USER_PROXY}"
 echo "(DEBUG) ROOT_DIR=${ROOT_DIR}"
 echo "(DEBUG) WORK_DIR=${WORK_DIR}"
+echo "(DEBUG) CMSSW_release=${CMSSW_release}"
 
 source "${ROOT_DIR}"/cicd/gitlab/setupCRABClient.sh
-python3 "${SCRIPT_DIR}"/statusTracking.py
+CM=${CMSSW_release#CMSSW_}; $([ "${CM%%_*}" -le 11 ] && echo python2 || echo python3) "${SCRIPT_DIR}/statusTracking.py"
