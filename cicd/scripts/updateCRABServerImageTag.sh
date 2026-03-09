8801610e68e44dd229784a47f8a548c2163faa69
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2026-03-09 21:25:54 +0100

    private deploy key with protection-rules instead of PAT approach.

--- cicd/scripts/updateCRABServerImageTag.sh
+++ cicd/scripts/updateCRABServerImageTag.sh
@@ -2,28 +2,38 @@ set -euo pipefail
 DEPLOY_ENV="${DEPLOY_ENV:?DEPLOY_ENV is required}"
 NEW_IMAGE_TAG="${NEW_IMAGE_TAG:?NEW_IMAGE_TAG is required}"
 
-UPSTREAM_IAC_REPO_URL="${UPSTREAM_IAC_REPO_URL:=github.com/nausikt/CMSKubernetes}"
+UPSTREAM_IAC_REPO="${UPSTREAM_IAC_REPO:=nausikt/CMSKubernetes}"
 UPSTREAM_IAC_BRANCH="${UPSTREAM_IAC_BRANCH:=crab}"
-UPSTREAM_IAC_REPO_PAT="${UPSTREAM_IAC_REPO_PAT:?UPSTREAM_IAC_REPO_PAT is required}"
 CI_BOT_USER_NAME="${CI_BOT_USER_NAME:=crab-gitlab-ci}"
-CI_BOT_USER_EMAIL="${CI_BOT_USER_NAME:=krittin.phornsiricharoenphant@cern.ch}"
+CI_BOT_USER_EMAIL="${CI_BOT_USER_EMAIL:=krittin.phornsiricharoenphant@cern.ch}"
 CI_COMMIT_AUTHOR="${CI_COMMIT_AUTHOR:=crab-gitlab-ci}"
 
-LATEST_COMMIT_SHA=$(git ls-remote "$UPSTREAM_IAC_REPO_URL" "$UPSTREAM_IAC_BRANCH" | awk '{print $1}')
+# Setup SSH deploy key
+mkdir -p ~/.ssh
+echo "${UPSTREAM_IAC_REPO_DEPLOY_PRIVATE_PKI_BASE64}" | base64 -d | tr -d '\r' > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
+
+# Get latest SHA
+LATEST_COMMIT_SHA=$(git ls-remote \
+  "git@github.com:${UPSTREAM_IAC_REPO}.git" \
+  "$UPSTREAM_IAC_BRANCH" | awk '{print $1}')
 
 WORKDIR="$(mktemp -d)"
-trap 'rm -rf "$WORKDIR"' EXIT
+trap 'rm -rf "$WORKDIR" ~/.ssh/id_ed25519' EXIT     # cleanup on exit
 
 git clone --branch ${UPSTREAM_IAC_BRANCH} \
-  "https://${UPSTREAM_IAC_REPO_PAT}@${UPSTREAM_IAC_REPO_URL}" \
+  "git@github.com:${UPSTREAM_IAC_REPO}" \
   $WORKDIR/CMSKubernetes
 
 # Automatically bump image regarding DevOperator desired image.
-yq -i -y --arg NEW_IMAGE_TAG "$NEW_IMAGE_TAG" '.image.tag = $NEW_IMAGE_TAG' $WORKDIR/CMSKubernetes/helm/crabserver/values-$DEPLOY_ENV.yaml
+yq -i -y --arg NEW_IMAGE_TAG "$NEW_IMAGE_TAG" \
+  '.image.tag = $NEW_IMAGE_TAG' \
+  "$WORKDIR/CMSKubernetes/helm/crabserver/values-${DEPLOY_ENV}.yaml"
 
-pushd $WORKDIR/CMSKubernetes
+pushd "$WORKDIR/CMSKubernetes"
 git config user.name  "$CI_BOT_USER_NAME"
 git config user.email "$CI_BOT_USER_EMAIL"
-git commit -am "deploy(${DEPLOY_ENV}): crabserver=${NEW_IMAGE_TAG} chart=${LATEST_COMMIT_SHA:0:7} by ${CI_COMMIT_AUTHOR}"
+git commit -am "deploy(${DEPLOY_ENV}): crabserver=${NEW_IMAGE_TAG} commit=${LATEST_COMMIT_SHA:0:7} by ${CI_COMMIT_AUTHOR}"
 git push -u origin $UPSTREAM_IAC_BRANCH
 popd
